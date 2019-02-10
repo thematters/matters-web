a12cc5a5d8a6ca123e35e490f4b0821e864f9cea
RoberTu <robertu0717@gmail.com>
2019-02-10 13:55:17 +0800

    Add CI

---
+++ bin/eb-deploy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+APP_DEVELOP="matters-client-web"
+ENV_DEVELOP="matters-client-web-develop"
+REGION_DEVELOP="ap-southeast-1"
+
+APP_STAGING="matters-client-web"
+ENV_STAGING="matters-client-web-stage"
+REGION_STAGING="ap-southeast-1"
+
+APP_PRODUCTION="matters-client-web"
+ENV_PRODUCTION="matters-client-web-prod"
+REGION_PRODUCTION="ap-southeast-1"
+
+if [[ $1 == 'develop' ]]
+then
+    echo "Deploying to development environment..."
+    printf '\n\n' | eb init $APP_DEVELOP --region $REGION_DEVELOP
+    eb deploy $ENV_DEVELOP
+elif [[ $1 == 'staging' ]]
+then
+    echo "Deploying to staging environment..."
+    printf '\n\n' | eb init $APP_STAGING --region $REGION_STAGING
+    eb deploy $ENV_STAGING
+elif [[ $1 == 'prod' ]]
+then
+    echo "Deploying to production environment..."
+    printf '\n\n' | eb init $APP_PRODUCTION --region $REGION_PRODUCTION
+    eb deploy $ENV_PRODUCTION
+else
+    echo "Usage: bin/eb-deploy.sh [develop|staging|prod]"
+fi
+
