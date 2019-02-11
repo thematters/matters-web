e6f7e85e8da43b4192fa3a2df4a390b772097ccd
RoberTu <robertu0717@gmail.com>
2019-02-11 20:47:13 +0800

    Upgrade to Next.js 8 👏

--- components/GlobalHeader/index.tsx
+++ components/GlobalHeader/index.tsx
@@ -5,18 +5,21 @@ import IconLogo from '~/static/icons/logo.svg?sprite'
 import { PATHS } from '~/common/enums'
 import styles from './styles.css'
 
+const Logo = () => (
+  <Link href={PATHS.HOMEPAGE.fs} as={PATHS.HOMEPAGE.url}>
+    <a>
+      <IconLogo height="20px" />
+    </a>
+  </Link>
+)
+
 export const GlobalHeader = () => (
   <header>
     <div className="l-row">
       <div className="container">
         <section>
-          <Link href={PATHS.HOMEPAGE.fs} as={PATHS.HOMEPAGE.url}>
-            <a>
-              <IconLogo height="20px" />
-            </a>
-          </Link>
+          <Logo />
         </section>
-
         <section />
       </div>
     </div>
