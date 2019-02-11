1964621667d37830b97fa63dead72839d0f2f6a1
RoberTu <robertu0717@gmail.com>
2019-02-11 13:33:07 +0800

    Update container width of 1024 viewport; Disable babel cache in dev enviroment;

--- postcss.config.js
+++ postcss.config.js
@@ -4,7 +4,14 @@ module.exports = {
     require('postcss-conditionals'),
     require('lost'),
     require('postcss-preset-env')({
-      stage: 0
+      stage: 0,
+      importFrom: [
+        './common/styles/variables/breakpoints.css',
+        './common/styles/variables/colors.css',
+        './common/styles/variables/sizing.css',
+        './common/styles/variables/spacing.css',
+        './common/styles/variables/typography.css'
+      ]
     })
   ]
 }
