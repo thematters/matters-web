023de2877989f29b3c9b66fef9937e5c81a0fa52
Zeck Li <devformatters@gmail.com>
2019-02-12 21:40:30 +0800

    Formatting

--- components/ToastHolder/Toast/index.tsx
+++ components/ToastHolder/Toast/index.tsx
@@ -1,17 +1,36 @@
 // External modules
 import classNames from 'classnames'
-import { useState, useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 // Internal modules
 import { emitter } from '~/common/services/event'
 import { Icon } from '~/components'
-import IconClose from '~/static/icons/close.svg'
 import IconCloseWhite from '~/static/icons/close-white.svg'
+import IconClose from '~/static/icons/close.svg'
 import styles from './styles.css'
 
+/**
+ * Toast is a component for presenting pop-up message. Don't manually
+ * mount component, use emitter instead.
+ *
+ * Usage:
+ *
+ * // To create a toast
+ * import { emitter } from '~/common/services/event'
+ *
+ * emitter.emit('addToast', {
+ *   color: 'green',
+ *   header: '',
+ *   content: '',
+ *   closeButton: true,
+ *   fixed: false,
+ *   duration: 3000
+ * })
+ *
+ */
 const second = 1000
 
-type Props = {
+interface Props {
   id: string
   color: 'green' | 'grey' | 'red' | 'white'
   header?: any
@@ -24,28 +43,39 @@ type Props = {
 }
 
 export const Toast: React.SFC<Props> = ({
-  id, color, header, content, closeButton, button, fixed, duration, remove
+  id,
+  color,
+  header,
+  content,
+  closeButton,
+  button,
+  fixed,
+  duration,
+  remove
 }) => {
-
   const mainClass = classNames({
-    'toast': true,
+    toast: true,
     [color]: !!color
   })
 
   const contentClass = classNames({
-    'content': true,
-    'opaque': header && content
+    content: true,
+    opaque: header && content
   })
 
   const iconCloseSrc = color === 'white' ? IconClose : IconCloseWhite
 
   const iconCloseStyle = { cursor: 'pointer' }
 
+  const removeToast = () => {
+    emitter.emit('removeToast', { id })
+  }
+
   useEffect(() => {
     if (fixed !== true) {
       setTimeout(() => {
-        emitter.emit('removeToast', { id })
-      }, duration || 5 * second)
+        removeToast()
+      }, duration || 6 * second)
     }
   })
 
@@ -53,18 +83,17 @@ export const Toast: React.SFC<Props> = ({
     <>
       <div className={mainClass}>
         <div>
-          { header && <div className="header">{header}{id}</div> }
-          { content && <div className={contentClass}>{content}</div> }
+          {header && <div className="header">{header}</div>}
+          {content && <div className={contentClass}>{content}</div>}
         </div>
-        {
-          closeButton &&
-            <Icon
-              src={iconCloseSrc}
-              style={iconCloseStyle}
-              onClick={() => {emitter.emit('removeToast', { id })}}
-            />
-        }
-        { !closeButton && customButton && customButton }
+        {closeButton && (
+          <Icon
+            src={iconCloseSrc}
+            style={iconCloseStyle}
+            onClick={removeToast}
+          />
+        )}
+        {!closeButton && customButton && customButton}
       </div>
       <style jsx>{styles}</style>
     </>
--- components/ToastHolder/index.tsx
+++ components/ToastHolder/index.tsx
@@ -1,40 +1,40 @@
 // External modules
 import classNames from 'classnames'
 import { filter } from 'lodash'
-import { useState, useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 // Internal modules
 import { emitter } from '~/common/services/event'
-import { Toast } from './Toast'
 import styles from './styles.css'
+import { Toast } from './Toast'
 
 /**
- * ToastHolder is a container for managing Toast component.
+ * ToastHolder is a container for managing Toast component. Use emitter to create
+ * and remove a Toast component.
  *
  * Usage:
  *
  * ```jsx
  *
- * // Place it ontop page.
+ * // Place it on top page.
  * <ToastHolder classes={['some', 'classes']} />
  *
  * ```
  */
 const prefix: string = 'toast-'
 
-type Props = {
+interface Props {
   classes?: string[]
 }
 
 export const ToastHolder: React.FC<Props> = ({
   classes = ['l-col-4', 'l-col-md-5', 'l-col-lg-8']
 }) => {
-
   const [toasts, setToasts] = useState<any[]>([])
 
   const mainClass = classNames(...classes)
 
-  const add = (payload: {[key: string]: any}) => {
+  const add = (payload: { [key: string]: any }) => {
     if (!payload || Object.keys(payload).length === 0) {
       return false
     }
@@ -62,11 +62,9 @@ export const ToastHolder: React.FC<Props> = ({
     <>
       <div className="l-row toast-holder">
         <div className={mainClass}>
-          {
-            toasts.map(
-              toast => <Toast key={toast.id} {...toast} remove={remove} />
-            )
-          }
+          {toasts.map(toast => (
+            <Toast key={toast.id} {...toast} remove={remove} />
+          ))}
         </div>
       </div>
       <style jsx>{styles}</style>
