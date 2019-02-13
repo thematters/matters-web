32fc5552016ebca86a893bc1ad0bbb8ff18b7163
Zeck Li <devformatters@gmail.com>
2019-02-13 21:20:32 +0800

    Add MessageHolder and Message components

---
+++ components/MessageHolder/index.tsx
@@ -0,0 +1,69 @@
+// External modules
+import classNames from 'classnames'
+import { filter } from 'lodash'
+import { FC, useEffect, useState } from 'react'
+
+// Internal modules
+import { useEventListener } from '~/components'
+import { Message } from './Message'
+import styles from './styles.css'
+
+/**
+ * MessageHolder is a container for managing Message component. Use event to create
+ * and remove a Message component.
+ *
+ * Usage:
+ *
+ * ```jsx
+ *
+ * // Place it on top page.
+ * <MessageHolder classes={['some', 'classes']} />
+ *
+ * ```
+ */
+const prefix: string = 'message-'
+
+interface Props {
+  classes?: string[]
+}
+
+export const MessageHolder: FC<Props> = ({
+  classes = []
+}) => {
+  const [messages, setMessages] = useState<any[]>([])
+
+  const mainClass = classNames(...classes)
+
+  const add = (payload: { [key: string]: any }) => {
+    if (!payload || Object.keys(payload).length === 0) {
+      return false
+    }
+    setMessages(prev => [{ id: `${prefix}${Date.now()}`, ...payload }, ...prev])
+  }
+
+  const remove = (payload: { id: string }) => {
+    const { id } = payload
+    if (!id || !id.startsWith(prefix)) {
+      return false
+    }
+    setMessages(prev => filter(prev, message => message.id !== id))
+  }
+
+  useEventListener('addMessage', add)
+
+  useEventListener('removeMessage', remove)
+
+  return (
+    <>
+      <div className="l-row message-holder">
+        <div className={mainClass}>
+          {messages.map(message => (
+            <Message key={message.id} {...message} remove={remove} />
+          ))}
+        </div>
+      </div>
+      <style jsx>{styles}</style>
+    </>
+  )
+}
+
