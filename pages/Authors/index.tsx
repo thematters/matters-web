4498833ad1a50ac1300d8a9d5c9b5ee2f39897d5
RoberTu <robertu0717@gmail.com>
2019-02-18 22:09:24 +0800

    Add AllAuthors page

--- pages/Authors/index.tsx
+++ pages/Authors/index.tsx
@@ -1,14 +1,113 @@
-import { Placeholder } from '~/components'
+import gql from 'graphql-tag'
+import _get from 'lodash/get'
+import { Query, QueryResult } from 'react-apollo'
 
-export default () => (
+import {
+  Footer,
+  InfiniteScroll,
+  PageHeader,
+  Spinner,
+  Translate,
+  UserDigest
+} from '~/components'
+
+import { mergeConnections } from '~/common/utils'
+import { AllAuthors } from './__generated__/AllAuthors'
+import styles from './styles.css'
+
+const ALL_AUTHORS = gql`
+  query AllAuthors($cursor: String) {
+    viewer {
+      id
+      recommendation {
+        authors(input: { first: 10, after: $cursor }) {
+          pageInfo {
+            startCursor
+            endCursor
+            hasNextPage
+          }
+          edges {
+            cursor
+            node {
+              ...UserDigestFullDescUser
+            }
+          }
+        }
+      }
+    }
+  }
+  ${UserDigest.FullDesc.fragments.user}
+`
+
+const Authors = () => (
   <main className="l-row">
     <article className="l-col-4 l-col-md-5 l-col-lg-8">
-      <h1>Authors</h1>
-      <Placeholder.ArticleDigestList />
+      <PageHeader
+        pageTitle={
+          <Translate
+            translations={{ zh_hant: '全部作者', zh_hans: '全部作者' }}
+          />
+        }
+      />
+
+      <section>
+        <Query query={ALL_AUTHORS}>
+          {({
+            data,
+            loading,
+            error,
+            fetchMore
+          }: QueryResult & { data: AllAuthors }) => {
+            if (loading) {
+              return <Spinner />
+            }
+
+            if (error) {
+              return <span>{JSON.stringify(error)}</span> // TODO
+            }
+
+            const connectionPath = 'viewer.recommendation.authors'
+            const { edges, pageInfo } = _get(data, connectionPath)
+            const loadMore = () =>
+              fetchMore({
+                variables: {
+                  cursor: pageInfo.endCursor
+                },
+                updateQuery: (previousResult, { fetchMoreResult }) =>
+                  mergeConnections({
+                    oldData: previousResult,
+                    newData: fetchMoreResult,
+                    path: connectionPath
+                  })
+              })
+
+            return (
+              <InfiniteScroll
+                hasNextPage={pageInfo.hasNextPage}
+                loadMore={loadMore}
+                loading={loading}
+                loader={<Spinner />}
+              >
+                <ul>
+                  {edges.map(({ node, cursor }: { node: any; cursor: any }) => (
+                    <li key={cursor}>
+                      <UserDigest.FullDesc user={node} />
+                    </li>
+                  ))}
+                </ul>
+              </InfiniteScroll>
+            )
+          }}
+        </Query>
+      </section>
     </article>
 
     <aside className="l-col-4 l-col-md-3 l-col-lg-4">
-      <Placeholder.Sidebar />
+      <Footer />
     </aside>
+
+    <style jsx>{styles}</style>
   </main>
 )
+
+export default Authors
