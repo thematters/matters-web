0a8cb185247ea446569d3ddfae65fac8c459a0f1
Guo Liu <liuguomailbox@gmail.com>
2019-02-19 12:20:05 -0800

    add snapshot examples

--- components/__test__/Avatar.test.tsx
+++ components/__test__/Avatar.test.tsx
@@ -3,7 +3,7 @@ import { render } from 'react-testing-library'
 
 import { Avatar } from '../Avatar'
 
-test('Button default style match snapshot', () => {
+test('Avatar default style match snapshot', () => {
   const { container } = render(<Avatar />)
   expect(container.firstChild).toMatchSnapshot()
 })
