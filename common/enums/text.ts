470f328e2a56367d5f3fd4f9d885fe3fb032aa78
Zeck Li <devformatters@gmail.com>
2019-02-21 03:09:36 +0800

    Add Text constants

---
+++ common/enums/text.ts
@@ -0,0 +1,35 @@
+export const TEXT = {
+
+  zh_hant: {
+    login: '登入',
+    register: '註冊',
+    previousStep: '上一步',
+    nextStep: '下一步',
+    enterEmail: '請輸入電子信箱',
+    enterRegisteredEmail: '請輸入你的註冊電子信箱',
+    invalidEmail: '電子信箱格式有誤',
+    enterPassword: '請輸入密碼',
+    forgetPassword: '忘記密碼',
+    hasNoAccount: '沒有帳號',
+    verificationCode: '驗證碼',
+    sendVerificationCode: '發送驗證碼',
+    required: '必填欄位',
+  },
+
+  zh_hans: {
+    login: '登入',
+    register: '注册',
+    previousStep: '上一步',
+    nextStep: '下一步',
+    enterEmail: '请输入邮箱',
+    enterRegisteredEmail: '请输入你的注册邮箱',
+    invalidEmail: '邮箱格式有误',
+    enterPassword: '请输入密码',
+    forgetPassword: '忘记密码',
+    hasNoAccount: '没有帐号',
+    verificationCode: '驗證碼',
+    sendVerificationCode: '发送验证码',
+    required: '必填栏位',
+  }
+
+}
