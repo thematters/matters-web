2fb5d1c332b67e345c53748a52476bf57296799d
RoberTu <robertu0717@gmail.com>
2019-02-24 19:50:46 +0800

    Add extra info of draft

--- components/DraftDigest/FeedDigest/index.tsx
+++ components/DraftDigest/FeedDigest/index.tsx
@@ -2,9 +2,10 @@ import classNames from 'classnames'
 import gql from 'graphql-tag'
 import Link from 'next/link'
 
-import { toPath } from '~/common/utils'
-import { Title } from '~/components'
+import { Icon, TextIcon, Title, Translate } from '~/components'
 
+import { toPath } from '~/common/utils'
+import ICON_HELP from '~/static/icons/help.svg?sprite'
 import { FeedDigestDraft } from './__generated__/FeedDigestDraft'
 import styles from './styles.css'
 
@@ -20,6 +21,14 @@ const fragments = {
     }
   `
 }
+
+const IconHelp = () => (
+  <Icon
+    id={ICON_HELP.id}
+    viewBox={ICON_HELP.viewBox}
+    style={{ width: 14, height: 14 }}
+  />
+)
 
 const FeedDigest = ({ draft }: { draft: FeedDigestDraft }) => {
   const { id, title, summary, scheduledAt, createdAt, publishState } = draft
@@ -45,6 +54,7 @@ const FeedDigest = ({ draft }: { draft: FeedDigestDraft }) => {
             </a>
           </Link>
         </div>
+
         <div className="description">
           <Link {...path}>
             <a>
@@ -53,6 +63,43 @@ const FeedDigest = ({ draft }: { draft: FeedDigestDraft }) => {
           </Link>
         </div>
       </div>
+
+      {publishState === 'pending' && (
+        <div className="extra">
+          <p>
+            <TextIcon icon={<IconHelp />}>
+              <Translate
+                zh_hant="上鏈後不可修改和刪除"
+                zh_hans="上链后不可修改和删除"
+              />
+            </TextIcon>
+          </p>
+          <button type="button" onClick={() => alert('revert')}>
+            <Translate zh_hant="撤銷" zh_hans="撤销" />
+          </button>
+        </div>
+      )}
+
+      {publishState === 'error' && (
+        <div className="extra">
+          <p>
+            <TextIcon icon={<IconHelp />}>
+              <Translate
+                zh_hant="文章已存至草稿箱，請檢查網絡後重試。"
+                zh_hans="文章已存至草稿箱，请检查网络后重试。"
+              />
+            </TextIcon>
+          </p>
+          <div>
+            <button type="button" onClick={() => alert('cancel')}>
+              <Translate zh_hant="取消" zh_hans="取消" />
+            </button>
+            <button type="button" onClick={() => alert('retry')}>
+              <Translate zh_hant="重試" zh_hans="重试" />
+            </button>
+          </div>
+        </div>
+      )}
 
       <style jsx>{styles}</style>
     </section>
