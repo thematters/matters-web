eb09e131af3f57fe954e30dc3dd14556c77b1f40
Zeck Li <devformatters@gmail.com>
2019-02-26 18:30:47 +0800

    Extract password form as component

--- components/Form/index.tsx
+++ components/Form/index.tsx
@@ -1,7 +1,11 @@
 import Input from './Input'
 import LoginForm from './LoginForm'
+import ResetCodeForm from './ResetCodeForm'
+import ResetForm from './ResetForm'
 
 export const Form = {
   Input,
-  LoginForm
+  LoginForm,
+  ResetCodeForm,
+  ResetForm
 }
