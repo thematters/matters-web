f84ce84ea9511002e1c7203fa850e1a41567deaf
Zeck Li <devformatters@gmail.com>
2019-03-05 21:35:40 +0800

    Extract resuable component

--- components/Modal/UserNameModal/index.tsx
+++ components/Modal/UserNameModal/index.tsx
@@ -3,6 +3,7 @@ import { FC, useContext, useState } from 'react'
 import { Button } from '~/components/Button'
 import { UserNameChangeConfirmForm } from '~/components/Form/UserNameChangeForm'
 import { LanguageContext } from '~/components/Language'
+import ModalComplete from '~/components/Modal/Complete'
 import ModalContent from '~/components/Modal/Content'
 
 import { translate } from '~/common/utils'
@@ -34,62 +35,52 @@ const UserNameModal: FC<ModalInstanceProps> = ({ close }) => {
 
   const confirmCallback = () => setStep('complete')
 
-  const Ask = () => (
-    <>
-      <ModalContent>
-        {translate({
-          zh_hant: '您的 Matters ID 僅能永久修改一次，確定要繼續嗎？',
-          zh_hans: '您的 Matters ID 仅能永久修改一次，确定要继续吗？',
-          lang
-        })}
-      </ModalContent>
-      <div className="ask-buttons">
-        <Button
-          type="button"
-          bgColor="transparent"
-          size="xlarge"
-          onClick={close}
-        >
-          {translate({ zh_hant: '取消', zh_hans: '取消', lang })}
-        </Button>
-        <Button
-          type="button"
-          bgColor="transparent"
-          className="u-link-green"
-          size="xlarge"
-          onClick={askCallback}
-        >
-          {translate({ zh_hant: '確定', zh_hans: '确定', lang })}
-        </Button>
-      </div>
-      <style jsx>{styles}</style>
-    </>
-  )
-
-  const Complete = () => (
-    <>
-      <div className="complete">
-        <div className="message">
-          {translate({
-            zh_hant: 'Matters ID 修改成功',
-            zh_hans: 'Matters ID 修改成功',
-            lang
-          })}
-        </div>
-      </div>
-      <style jsx>{styles}</style>
-    </>
-  )
-
   return (
     <>
-      {step === 'ask' && <Ask />}
+      {step === 'ask' && (
+        <>
+          <ModalContent>
+            {translate({
+              zh_hant: '您的 Matters ID 僅能永久修改一次，確定要繼續嗎？',
+              zh_hans: '您的 Matters ID 仅能永久修改一次，确定要继续吗？',
+              lang
+            })}
+          </ModalContent>
+          <div className="ask-buttons">
+            <Button
+              type="button"
+              bgColor="transparent"
+              size="xlarge"
+              onClick={close}
+            >
+              {translate({ zh_hant: '取消', zh_hans: '取消', lang })}
+            </Button>
+            <Button
+              type="button"
+              bgColor="transparent"
+              className="u-link-green"
+              size="xlarge"
+              onClick={askCallback}
+            >
+              {translate({ zh_hant: '確定', zh_hans: '确定', lang })}
+            </Button>
+          </div>
+        </>
+      )}
       {step !== 'ask' && (
         <ModalContent>
           {step === 'confirm' && (
             <UserNameChangeConfirmForm submitCallback={confirmCallback} />
           )}
-          {step === 'complete' && <Complete />}
+          {step === 'complete' && (
+            <ModalComplete
+              message={translate({
+                zh_hant: 'Matters ID 修改成功',
+                zh_hans: 'Matters ID 修改成功',
+                lang
+              })}
+            />
+          )}
         </ModalContent>
       )}
       <style jsx>{styles}</style>
