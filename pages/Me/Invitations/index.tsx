ac2039ba03aa6576b3b1e69958c56ddd1f45884b
RoberTu <robertu0717@gmail.com>
2019-03-11 17:02:10 +0800

    Empty holder for invitations page

--- pages/Me/Invitations/index.tsx
+++ pages/Me/Invitations/index.tsx
@@ -1,16 +1,22 @@
-import { Placeholder } from '~/components'
+import { Empty } from '~/components/Empty'
+import { Footer } from '~/components/Footer'
+import { Translate } from '~/components/Language'
 import { Protected } from '~/components/Protected'
+
+import ICON_EMPTY_COMMENT from '~/static/images/illustration-empty-comment.svg?url'
 
 export default () => (
   <Protected>
     <main className="l-row">
       <article className="l-col-4 l-col-md-5 l-col-lg-8">
-        <h1>Invitations</h1>
-        <Placeholder.ArticleDigestList />
+        <Empty
+          icon={<img src={ICON_EMPTY_COMMENT} />}
+          description={<Translate zh_hant="即將上線" zh_hans="即将上线" />}
+        />
       </article>
 
       <aside className="l-col-4 l-col-md-3 l-col-lg-4">
-        <Placeholder.Sidebar />
+        <Footer />
       </aside>
     </main>
   </Protected>
