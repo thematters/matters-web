47cb3c3bdca0b29067de761dad8bd04c34101d95
RoberTu <robertu0717@gmail.com>
2019-03-13 16:02:21 +0800

    Disable edit userName if "viewer.info.userNameEditable" is false; Enhance ModalComplete style

--- components/Modal/UserNameModal/index.tsx
+++ components/Modal/UserNameModal/index.tsx
@@ -1,12 +1,10 @@
-import { FC, useContext, useState } from 'react'
+import { FC, useState } from 'react'
 
 import { Button } from '~/components/Button'
 import { UserNameChangeConfirmForm } from '~/components/Form/UserNameChangeForm'
-import { LanguageContext, Translate } from '~/components/Language'
+import { Translate } from '~/components/Language'
 import { Modal } from '~/components/Modal'
 import ModalComplete from '~/components/Modal/Complete'
-
-import { translate } from '~/common/utils'
 
 import styles from './styles.css'
 
@@ -24,8 +22,6 @@ import styles from './styles.css'
 type Step = 'ask' | 'confirm' | 'complete'
 
 const UserNameModal: FC<ModalInstanceProps> = ({ close }) => {
-  const { lang } = useContext(LanguageContext)
-
   const [step, setStep] = useState<Step>('ask')
 
   const askCallback = (event: any) => {
@@ -40,11 +36,10 @@ const UserNameModal: FC<ModalInstanceProps> = ({ close }) => {
       {step === 'ask' && (
         <>
           <Modal.Content>
-            {translate({
-              zh_hant: '您的 Matters ID 僅能永久修改一次，確定要繼續嗎？',
-              zh_hans: '您的 Matters ID 仅能永久修改一次，确定要继续吗？',
-              lang
-            })}
+            <Translate
+              zh_hant="您的 Matters ID 僅能永久修改一次，確定要繼續嗎？"
+              zh_hans="您的 Matters ID 仅能永久修改一次，确定要继续吗？"
+            />
           </Modal.Content>
           <div className="ask-buttons">
             <Button
@@ -53,7 +48,7 @@ const UserNameModal: FC<ModalInstanceProps> = ({ close }) => {
               size="xlarge"
               onClick={close}
             >
-              {translate({ zh_hant: '取消', zh_hans: '取消', lang })}
+              <Translate zh_hant="取消" zh_hans="取消" />
             </Button>
             <Button
               type="button"
@@ -62,7 +57,7 @@ const UserNameModal: FC<ModalInstanceProps> = ({ close }) => {
               size="xlarge"
               onClick={askCallback}
             >
-              {translate({ zh_hant: '確定', zh_hans: '确定', lang })}
+              <Translate zh_hant="確定" zh_hans="确定" />
             </Button>
           </div>
         </>
