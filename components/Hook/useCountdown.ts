67ea5e3b5a9b843e01d41e09f0985277606d71c1
RoberTu <robertu0717@gmail.com>
2019-03-17 01:22:04 +0800

    Add reusable "useCountdown" custom hook

---
+++ components/Hook/useCountdown.ts
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+
+import { leftPad } from '~/common/utils'
+
+import { useInterval } from './useInterval'
+
+const formatTimeLeft = (diff: number) => {
+  const mins = Math.floor((diff / (60 * 1000)) * 1)
+  const secs = Math.floor(((diff % (60 * 1000)) / 1000) * 1)
+  return { mins, secs }
+}
+
+interface UseCountdownProps {
+  timeLeft: number
+  step?: number
+}
+
+export const useCountdown = ({ timeLeft, step = 1000 }: UseCountdownProps) => {
+  const [countdown, setCountdown] = useState({ timeLeft, step })
+  const formatted = formatTimeLeft(countdown.timeLeft)
+
+  useInterval(() => {
+    if (countdown.timeLeft > 0) {
+      setCountdown({
+        ...countdown,
+        timeLeft: Math.max(countdown.timeLeft - 1000, 0)
+      })
+    }
+
+    if (timeLeft < 0) {
+      setCountdown({
+        ...countdown,
+        timeLeft: 0
+      })
+    }
+  }, countdown.step)
+
+  return {
+    countdown,
+    setCountdown: (props: UseCountdownProps) =>
+      setCountdown({
+        timeLeft: props.timeLeft,
+        step: props.step || countdown.step
+      }),
+    formattedTimeLeft: {
+      mmss: `${leftPad(formatted.mins, 2, 0)}:${leftPad(formatted.secs, 2, 0)}`,
+      ss: `${leftPad(formatted.secs, 2, 0)}`
+    }
+  }
+}
--- pages/Me/DraftDetail/PublishState/PendingState.tsx
+++ pages/Me/DraftDetail/PublishState/PendingState.tsx
@@ -1,26 +1,19 @@
-import { useState } from 'react'
 import { Query } from 'react-apollo'
 
-import { Translate, useInterval } from '~/components'
+import { Translate } from '~/components'
 import RecallButton from '~/components/DraftDigest/Components/RecallButton'
 import { PublishStateDraft } from '~/components/GQL/fragments/__generated__/PublishStateDraft'
 import DRAFT_PUBLISH_STATE from '~/components/GQL/queries/draftPublishState'
+import { useCountdown } from '~/components/Hook'
 import { Toast } from '~/components/Toast'
-
-import { countDownToTime, leftPad } from '~/common/utils'
 
 const PendingState = ({ draft }: { draft: PublishStateDraft }) => {
   const scheduledAt = draft.scheduledAt
-  const timeLeft = Date.parse(scheduledAt) - Date.now()
+  const {
+    countdown: { timeLeft },
+    formattedTimeLeft
+  } = useCountdown({ timeLeft: Date.parse(scheduledAt) - Date.now() })
   const isPublishing = !scheduledAt || !timeLeft || timeLeft <= 0
-
-  const [left, setLeft] = useState(timeLeft || 0)
-  useInterval(() => {
-    if (left > 0) {
-      setLeft(Math.max(left - 1000, 0))
-    }
-  }, 1000)
-  const leftFormatted = countDownToTime(left)
 
   return (
     <Query
@@ -28,7 +21,7 @@ const PendingState = ({ draft }: { draft: PublishStateDraft }) => {
       query={DRAFT_PUBLISH_STATE}
       pollInterval={1000 * 5}
       errorPolicy="none"
-      skip={!process.browser}
+      skip={!process.browser || !isPublishing}
     >
       {() => (
         <Toast
@@ -38,21 +31,8 @@ const PendingState = ({ draft }: { draft: PublishStateDraft }) => {
               <Translate zh_hant="正在發佈" zh_hans="正在发布" />
             ) : (
               <Translate
-                zh_hant={({ l }) =>
-                  `正在等待發佈 (${leftPad(l.mins, 2, 0)}:${leftPad(
-                    l.secs,
-                    2,
-                    0
-                  )})`
-                }
-                zh_hans={({ l }) =>
-                  `正在等待发布 (${leftPad(l.mins, 2, 0)}:${leftPad(
-                    l.secs,
-                    2,
-                    0
-                  )})`
-                }
-                data={{ l: leftFormatted }}
+                zh_hant={`正在等待發佈 (${formattedTimeLeft.mmss})`}
+                zh_hans={`正在等待发布 (${formattedTimeLeft.mmss})`}
               />
             )
           }
