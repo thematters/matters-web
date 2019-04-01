4d32dfb14b70c3a0c55b1095d53150b6cc1b063a
RoberTu <robertu0717@gmail.com>
2019-04-01 23:35:00 +0800

    Remove console.log

--- components/Editor/utils/lineBreakMatcher.ts
+++ components/Editor/utils/lineBreakMatcher.ts
@@ -3,8 +3,6 @@ import { Quill } from 'react-quill'
 const Delta = Quill.import('delta')
 
 const lineBreakMatcher = (node: HTMLElement, delta: any) => {
-  console.log('...', node, node.classList.contains('inline'))
-
   return node.classList.contains('inline')
     ? new Delta().insert({
         smartBreak: false
