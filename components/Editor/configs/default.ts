b9d5a35b53a96f4955fd5e98354fc2ccf0fe8296
RoberTu <robertu0717@gmail.com>
2019-04-03 10:06:01 +0800

    Remove "enter" keyboard bindings to fix cursor incorrect selection issue

--- components/Editor/configs/default.ts
+++ components/Editor/configs/default.ts
@@ -1,12 +1,8 @@
-import { Quill } from 'react-quill'
-
 import { KEYCODES } from '~/common/enums'
 
 import '../blots'
 import '../modules/mention'
 import lineBreakMatcher from '../utils/lineBreakMatcher'
-
-const Parchment = Quill.import('parchment')
 
 export const modules = {
   toolbar: [
@@ -26,55 +22,7 @@ export const modules = {
           return false
         }
       },
-      handleEnter: {
-        key: KEYCODES.enter,
-        handler(range: any, context: any) {
-          // @ts-ignore
-          const quill = this.quill
-          if (range.length > 0) {
-            // Remove characters in selected range
-            // So we do not trigger `text-change`
-            quill.scroll.deleteAt(range.index, range.length)
-          }
 
-          const lineFormats = Object.keys(context.format).reduce(
-            (lf: any, format) => {
-              if (
-                Parchment.query(format, Parchment.Scope.BLOCK) &&
-                !Array.isArray(context.format[format])
-              ) {
-                lf[format] = context.format[format]
-              }
-              return lf
-            },
-            {}
-          ) as any
-          const previousChar = quill.getText(range.index - 1, 1)
-
-          // Earlier scroll.deleteAt might have messed up our selection,
-          // so insertText's built in selection preservation is not reliable
-          quill.insertText(range.index, '\n', lineFormats, 'user')
-
-          if (previousChar === '' || previousChar === '\n') {
-            quill.setSelection(range.index + 2, 'silent')
-          } else {
-            quill.setSelection(range.index + 1, 'silent')
-          }
-
-          Object.keys(context.format).forEach(name => {
-            if (lineFormats[name] != null) {
-              return
-            }
-            if (Array.isArray(context.format[name])) {
-              return
-            }
-            if (name === 'link') {
-              return
-            }
-            quill.format(name, context.format[name], 'user')
-          })
-        }
-      },
       linebreak: {
         key: KEYCODES.enter,
         shiftKey: true,
