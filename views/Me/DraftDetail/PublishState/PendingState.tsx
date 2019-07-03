723c0e4dd4b28eb2936c79aa3038afe60c039fb8
RoberTu <robertu0717@gmail.com>
2019-07-03 12:17:46 +0800

    Update deps

--- views/Me/DraftDetail/PublishState/PendingState.tsx
+++ views/Me/DraftDetail/PublishState/PendingState.tsx
@@ -36,12 +36,8 @@ const PendingState = ({ draft }: { draft: PublishStateDraft }) => {
               />
             ) : (
               <Translate
-                zh_hant={`${TEXT.zh_hant.waitingForPublish} (${
-                  formattedTimeLeft.mmss
-                })`}
-                zh_hans={`${TEXT.zh_hans.waitingForPublish} (${
-                  formattedTimeLeft.mmss
-                })`}
+                zh_hant={`${TEXT.zh_hant.waitingForPublish} (${formattedTimeLeft.mmss})`}
+                zh_hans={`${TEXT.zh_hans.waitingForPublish} (${formattedTimeLeft.mmss})`}
               />
             )
           }
