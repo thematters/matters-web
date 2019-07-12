b5720d6ee1052cd56ea5b3708e7a6a1820e94e09
Guo Liu <liuguomailbox@gmail.com>
2019-07-12 15:30:53 -0700

    update immerse mode trigger

--- components/Hook/useImmersiveMode.ts
+++ components/Hook/useImmersiveMode.ts
@@ -2,11 +2,19 @@ import { useEffect } from 'react'
 
 import { dom } from '~/common/utils'
 
+// create a "sticky" effect on upper and lower anchors
+const upperAnchor = 400
+const lowerAnchor = -300
+
 export const useImmersiveMode = (hookSelector?: string) => {
+  let topOffset = 0
+  let previousLocation = 0
+  let currentLocation = 0
+
   let lastKnownScrollY = 0
   let currentScrollY = 0
+
   let ticking = false
-  let topOffset = 0
 
   const onScroll = () => {
     currentScrollY = window.pageYOffset
@@ -18,15 +26,28 @@ export const useImmersiveMode = (hookSelector?: string) => {
     }
     ticking = true
   }
+
   const update = () => {
-    if (currentScrollY < lastKnownScrollY) {
-      document.documentElement.classList.remove('immersive-mode')
-    } else if (
-      currentScrollY > lastKnownScrollY &&
+    previousLocation = currentLocation
+
+    const delta = currentScrollY - lastKnownScrollY
+    currentLocation =
       currentScrollY > topOffset
-    ) {
-      document.documentElement.classList.add('immersive-mode')
+        ? delta * previousLocation > 0
+          ? previousLocation > 0
+            ? upperAnchor
+            : lowerAnchor
+          : previousLocation + delta
+        : lowerAnchor
+
+    if (currentLocation * previousLocation <= 0) {
+      if (currentLocation < 0) {
+        document.documentElement.classList.remove('immersive-mode')
+      } else if (currentLocation > 0) {
+        document.documentElement.classList.add('immersive-mode')
+      }
     }
+
     lastKnownScrollY = currentScrollY
     ticking = false
   }
