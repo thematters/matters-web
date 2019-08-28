c9ea2323029f4022b7f30a0b07a5663f6c4b80b8
RoberTu <robertu0717@gmail.com>
2019-08-28 23:26:11 +0800

    Add "setupLikeCoin" step to signup modal

--- components/Modal/SignUpModal/index.tsx
+++ components/Modal/SignUpModal/index.tsx
@@ -4,6 +4,7 @@ import SignUpComplete from '~/components/Form/SignUpComplete'
 import { SignUpInitForm, SignUpProfileForm } from '~/components/Form/SignUpForm'
 import { LanguageContext } from '~/components/Language'
 import { Modal } from '~/components/Modal'
+import SetupLikeCoin from '~/components/SetupLikeCoin'
 
 import { TEXT } from '~/common/enums'
 import { translate } from '~/common/utils'
@@ -19,12 +20,12 @@ import { translate } from '~/common/utils'
  *
  */
 
-type Step = 'signUp' | 'profile' | 'complete'
+type Step = 'signUp' | 'profile' | 'setupLikeCoin' | 'complete'
 
 const SignUpModal: FC<ModalInstanceProps> = ({ closeable, setCloseable }) => {
   const { lang } = useContext(LanguageContext)
 
-  const [step, setStep] = useState<Step>('signUp')
+  const [step, setStep] = useState<Step>('setupLikeCoin')
   const data = {
     signUp: {
       title: translate({
@@ -40,6 +41,13 @@ const SignUpModal: FC<ModalInstanceProps> = ({ closeable, setCloseable }) => {
         lang
       })
     },
+    setupLikeCoin: {
+      title: translate({
+        zh_hant: TEXT.zh_hant.setupLikeCoin,
+        zh_hans: TEXT.zh_hans.setupLikeCoin,
+        lang
+      })
+    },
     complete: {
       title: translate({
         zh_hant: TEXT.zh_hant.registerSuccess,
@@ -49,24 +57,31 @@ const SignUpModal: FC<ModalInstanceProps> = ({ closeable, setCloseable }) => {
     }
   }
 
-  const signUpCallback = ({ email, codeId, password }: any) => {
-    setCloseable(false)
-    setStep('profile')
-  }
-
-  const profileCallback = () => {
-    setStep('complete')
-  }
-
   return (
     <>
       <Modal.Header title={data[step].title} closeable={closeable} />
 
       {step === 'signUp' && (
-        <SignUpInitForm purpose="modal" submitCallback={signUpCallback} />
+        <SignUpInitForm
+          purpose="modal"
+          submitCallback={() => {
+            setCloseable(false)
+            setStep('profile')
+          }}
+        />
       )}
       {step === 'profile' && (
-        <SignUpProfileForm purpose="modal" submitCallback={profileCallback} />
+        <SignUpProfileForm
+          purpose="modal"
+          submitCallback={() => setStep('setupLikeCoin')}
+        />
+      )}
+      {step === 'setupLikeCoin' && (
+        <SetupLikeCoin
+          submitCallback={() => {
+            setStep('complete')
+          }}
+        />
       )}
       {step === 'complete' && <SignUpComplete />}
     </>
