9494611945ffc1a7c2a60b62e4f004ed97fcfa3b
RoberTu <robertu0717@gmail.com>
2019-08-29 16:00:10 +0800

    Add 	`<OAuth.Box>` and used in OAuth success & failure pages

---
+++ components/OAuth/Box/index.tsx
@@ -0,0 +1,23 @@
+import { Avatar } from '~/components/Avatar'
+
+import styles from './styles.css'
+
+interface Props {
+  avatar: string
+  title: React.ReactNode | string
+}
+
+export const Box: React.FC<Props> = ({ avatar, title, children }) => {
+  return (
+    <section className="container l-col-4 l-col-sm-6 l-offset-sm-1 l-col-md-4 l-offset-md-2 l-col-lg-6 l-offset-lg-3">
+      <header>
+        <Avatar size="large" src={avatar} />
+        <h1>{title}</h1>
+      </header>
+
+      {children}
+
+      <style jsx>{styles}</style>
+    </section>
+  )
+}
