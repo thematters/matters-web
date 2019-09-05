aab7ac1e606b25d0a95e03dca15b3e8f1cd984a5
Zeck Li <devformatters@gmail.com>
2019-09-05 17:10:51 +0800

    Formatting.

--- views/ArticleDetail/Wall/index.tsx
+++ views/ArticleDetail/Wall/index.tsx
@@ -27,34 +27,34 @@ const Wall = ({ show, client }: any) => {
   return (
     <section className={outerClasses}>
       <section className="bg" style={bgStyle}>
-      <section className="l-row">
-        <section className="l-col-4 l-col-md-8 l-col-lg-12">
-          <section className="container">
-            <div className="slogan">
-              <Translate zh_hant="看不過癮？" zh_hans="看不过瘾？" />
-            </div>
-            <div className="desc">
-              <Translate
-                zh_hant="馬上加入全球最高質量華語創作社區，更多精彩文章与讨论等着你。"
-                zh_hans="马上加入全球最高质量华语创作社区，更多精彩文章与讨论等着你。"
-              />
-            </div>
-            <div className="signup">
-              <SignUpButton extraStyle={signUpButtonStyle}>
-                <Translate zh_hant="註冊帳號" zh_hans="注册账号" />
-              </SignUpButton>
-            </div>
-            <div className="close">
-              <button onClick={close}>
-                <Icon
-                  id={ICON_CLOSE.id}
-                  viewBox={ICON_CLOSE.viewBox}
-                  size="small"
+        <section className="l-row">
+          <section className="l-col-4 l-col-md-8 l-col-lg-12">
+            <section className="container">
+              <div className="slogan">
+                <Translate zh_hant="看不過癮？" zh_hans="看不过瘾？" />
+              </div>
+              <div className="desc">
+                <Translate
+                  zh_hant="馬上加入全球最高質量華語創作社區，更多精彩文章与讨论等着你。"
+                  zh_hans="马上加入全球最高质量华语创作社区，更多精彩文章与讨论等着你。"
                 />
-              </button>
-            </div>
+              </div>
+              <div className="signup">
+                <SignUpButton extraStyle={signUpButtonStyle}>
+                  <Translate zh_hant="註冊帳號" zh_hans="注册账号" />
+                </SignUpButton>
+              </div>
+              <div className="close">
+                <button onClick={close}>
+                  <Icon
+                    id={ICON_CLOSE.id}
+                    viewBox={ICON_CLOSE.viewBox}
+                    size="small"
+                  />
+                </button>
+              </div>
+            </section>
           </section>
-        </section>
         </section>
       </section>
       <style jsx>{styles}</style>
