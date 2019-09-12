0c700d33fb5fa7838e5a34f82600855f05bab92d
RoberTu <robertu0717@gmail.com>
2019-09-12 13:51:17 +0800

    Add query for "viewer.status.LIKE"

--- views/Me/Settings/Account/WalletSettings.tsx
+++ views/Me/Settings/Account/WalletSettings.tsx
@@ -1,7 +1,10 @@
+import gql from 'graphql-tag'
 import _get from 'lodash/get'
 import { useContext } from 'react'
+import { QueryResult } from 'react-apollo'
 
 import { Icon, PageHeader, TextIcon, Translate } from '~/components'
+import { Query } from '~/components/GQL'
 import { ViewerContext } from '~/components/Viewer'
 
 import { TEXT } from '~/common/enums'
@@ -9,7 +12,21 @@ import ICON_ARROW_RIGHT_GREEN from '~/static/icons/arrow-right-green.svg?sprite'
 
 import styles from './styles.css'
 
-export default () => {
+const VIEWER_LIKE_INFO = gql`
+  query ViewerLikeInfo {
+    viewer {
+      id
+      status {
+        LIKE {
+          total
+          rateUSD
+        }
+      }
+    }
+  }
+`
+
+const WalletSetting = () => {
   const viewer = useContext(ViewerContext)
 
   return (
@@ -31,32 +48,60 @@ export default () => {
         </div>
       </section>
 
-      <section className="setting-section">
-        <div className="left">
-          <span className="title">
-            <Translate zh_hant="我的創作價值" zh_hans="我的创作价值" />
-          </span>
-          <span>{_get(viewer, 'likerId')}</span>
-        </div>
+      <Query query={VIEWER_LIKE_INFO}>
+        {({ data, loading, error }: QueryResult) => {
+          const LIKE = _get(data, 'viewer.status.LIKE')
 
-        <a href="https://like.co/in" className="u-link-green" target="_blank">
-          <TextIcon
-            icon={
-              <Icon
-                id={ICON_ARROW_RIGHT_GREEN.id}
-                viewBox={ICON_ARROW_RIGHT_GREEN.viewbox}
-                size="small"
-              />
-            }
-            textPlacement="left"
-            weight="medium"
-          >
-            <Translate zh_hant="去 like.co 查看" zh_hans="去 like.co 查看" />
-          </TextIcon>
-        </a>
-      </section>
+          if (loading || error) {
+            return null
+          }
+
+          const USDPrice = (LIKE.rateUSD * LIKE.total).toFixed(2)
+
+          return (
+            <section className="setting-section">
+              <div className="left">
+                <span className="title">
+                  <Translate zh_hant="我的創作價值" zh_hans="我的创作价值" />
+                </span>
+                <span>
+                  {LIKE.total} LikeCoin
+                  {USDPrice && (
+                    <span className="usd-price"> ≈ {USDPrice} USD</span>
+                  )}
+                </span>
+              </div>
+
+              <a
+                href="https://like.co/in"
+                className="u-link-green"
+                target="_blank"
+              >
+                <TextIcon
+                  icon={
+                    <Icon
+                      id={ICON_ARROW_RIGHT_GREEN.id}
+                      viewBox={ICON_ARROW_RIGHT_GREEN.viewbox}
+                      size="small"
+                    />
+                  }
+                  textPlacement="left"
+                  weight="medium"
+                >
+                  <Translate
+                    zh_hant="去 like.co 查看"
+                    zh_hans="去 like.co 查看"
+                  />
+                </TextIcon>
+              </a>
+            </section>
+          )
+        }}
+      </Query>
 
       <style jsx>{styles}</style>
     </section>
   )
 }
+
+export default WalletSetting
