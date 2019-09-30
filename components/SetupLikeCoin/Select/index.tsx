7d9356d9d443572ab5a333fc10dd2476772a3c47
RoberTu <robertu0717@gmail.com>
2019-10-01 00:28:22 +0800

    Fix typo; Fix styles;

--- components/SetupLikeCoin/Select/index.tsx
+++ components/SetupLikeCoin/Select/index.tsx
@@ -47,7 +47,7 @@ const Description = () => (
         spacing="xtight"
         weight="medium"
       >
-        <Translate zh_hans="什么是 Liker ID？" zh_hant="什么是 Liker ID？" />
+        <Translate zh_hant="什麼是 Liker ID？" zh_hans="什么是 Liker ID？" />
       </TextIcon>
     </section>
 
@@ -103,7 +103,7 @@ const Select: React.FC<Props> = ({ startGenerate, startBind, scrollLock }) => {
                   <p>
                     <Translate
                       zh_hant="同意 Matters 帮我创建 Liker ID"
-                      zh_hans="同意 Matters 幫我創建 Liker ID"
+                      zh_hans="同意 Matters 帮我创建 Liker ID"
                     />
                   </p>
                 </section>
