aa8738fcee33e353399968e2f3b9d6bbb90d86df
RoberTu <robertu0717@gmail.com>
2019-10-04 18:34:19 +0800

    Update FAQ

--- views/Misc/FAQ/content.ts
+++ views/Misc/FAQ/content.ts
@@ -1,80 +1,300 @@
 export default {
-  zh_hant: `
-<h3>Matters 是什麼？</h3>
-<p>Matters 正在建立一個基於分佈式技術的內容平台。目標是，令創作者生產的所有數據回歸其個人所有，永久保存；在此基礎上搭建智能合約及加密貨幣體系，讓因內容而產生的經濟價值，盡可能回歸作者，讓參與者獲得持續回報；以此體系，推動公共領域的知識生產。</p>
+  zh_hant: /* html */ `
+<details class="u-content" open>
+  <summary>
+    <h3>Matters 是什麼？</h3>
+  </summary>
+  <p>Matters 正在建立一個基於分佈式技術的內容平台。目標是，令創作者生產的所有數據回歸其個人所有，永久保存；在此基礎上搭建智能合約及加密貨幣體系，讓因內容而產生的經濟價值，盡可能回歸作者，讓參與者獲得持續回報；以此體系，推動公共領域的知識生產。</p>
+</details>
 
-<h3>Matters 為什麼要用分佈式技術？</h3>
-<p>Matters 站上所有作品（不含評論）已正式上載到星際文件系統（InterPlanetary File System，IPFS）的節點上，實現作品內容的分佈式存儲。在傳統的中心化網路社區里，由於創作者的帳戶、密碼、隱私策略文件、生產的作品和產品的衍生數據，都要存儲在社區服務提供商控制的服務器，加之含有版權侵佔條款的社區用戶協議，故服務提供商可以從技術上佔有、修改、刪除、洩露創作者的作品和數據。Matters 採用的分佈式存儲技術，可以實現創作者百分之百擁有自己的作品和數據。採用分佈式存儲后，作為服務提供商的 Matters，以及其他任意的分佈式節點，從技術上無法侵害創作者的任何權益，為保護創作者的版權、隱私權和數據權利不受第三方侵害奠定基礎。</p>
+<details class="u-content">
+  <summary>
+    <h3>LikeCoin 是什麼？</h3>
+  </summary>
+  <p>LikeCoin Foundation 致力開發出一套基於區塊鏈的基礎設施，讓創作者真正擁有內容，獲得實質回饋，同時內容不被竄改，得以永久保存，既促進衍生轉發，也有助求真核實。 LikeCoin 是 LikeCoin Foundation 推出的加密貨幣。更多資訊，請看<a class="u-link-green" href="https://matters.news/@likecoin/%E5%8C%96%E8%AE%9A%E7%82%BA%E8%B3%9E-%E8%AE%9A%E8%B3%9E%E5%85%AC%E6%B0%91%E6%87%B6%E4%BA%BA%E5%8C%85-zdpuAtWT6a2rjr75JZBzwnvy67vrxw5cBRqwnHbkFehRYHLXx" target="_blank">讚賞公民懶人包</a>。</p>
+</details>
 
-<h3>分佈式技術對我有什麼用？</h3>
-<p>第一，通過 Matters 發表的作品，將在分佈式網路中永久儲存； </p>
-<p>第二，每篇作品都會獲得僅作者可見的、獨一無二的「數字指紋」； </p>
-<p>第三，您可使用任意分佈式公共節點傳播作品。</p>
+<details class="u-content">
+  <summary>
+    <h3>MAT 與 LikeCoin</h3>
+  </summary>
+  <p>2019 年 10 月起，<a href="https://matters.news/@az/%E7%B5%A6%E7%A4%BE%E5%8D%80%E7%9A%84%E4%B8%80%E5%B0%81%E4%BF%A1-matters-%E8%88%87-like-coin%E5%AE%A3%E4%BD%88%E5%90%88%E4%BD%9C-zdpuB3JTzZ8Ch2J9NbPsJ79NKuAJB3ehie3KWf78rimdCefo4" target="_blank" class="u-link-green">Matters 與 LikeCoin 聯手</a>，將 LikeCoin 這一基於寫作者創造力為衡量的加密貨幣引入 Matters 的自由創作和公共討論中，讓平台效應以收入的形式回饋給作者。</p>
+  <p>請持有 MAT 的用戶注意：在東八區時間 2019 年 10 月 15 日 10:00 後，Matters 平台中的所有 MAT 按鍵，將替換為 LikeCoin 讚賞鍵。在此之前已持有 MAT 的用戶，即會獲得以固定匯率自動兌換成的 LikeCoin，兌換匯率為 <b>1 MAT = 10 LikeCoin</b>。</p>
+  <p>2019 年 10 月 15 日 10:00 後註冊的用戶，將不再持有 MAT，直接獲得 LikeCoin。</p>
+</details>
 
-<h3>怎樣把作品發佈至分佈式網絡？</h3>
-<p>當你在編輯器中點擊「發佈」時，會先彈出「作品即將發佈到分佈式網絡」的說明視窗，告知發佈規則，並要求您再次確認「發佈」，或者「暫存草稿箱」回到一般編輯狀態。</p>
-<p>當您再次確認「發佈」後，系統會立即將作品存至草稿箱。此時頁面會自動跳轉到預覽狀態，出現「正在等待發佈」的標籤，作品發佈成功後會存儲在 IPFS 的公共節點之中，不可再修改或刪除。</p>
+<details class="u-content" open>
+  <summary>
+    <h3>如何使用 LikeCoin</h3>
+  </summary>
 
-<h3>作品發佈後想刪除怎麼辦？</h3>
-<p>已成功發佈的作品，理論上會實現永久存儲，無法被刪除和修改。若必要，您可以選擇在 Matters 站上將已發佈的作品「站內隱藏」，隱藏之後，作品在您的列表裡會標記為隱藏狀態，僅作者可見。作品指紋只有您本人可見，是否要繼續傳播公共節點所保存的鏈接，決定權也在您。</p>
+  <details class="u-content">
+    <summary>
+      <h4>我是讀者，怎樣用 LikeCoin 讚賞作品？</h4>
+    </summary>
+    <p>只需點擊 LikeCoin 讚賞鍵為你欣賞的作品拍手，即可化讚為賞。你可以向一篇作品拍手5次，不需要支付任何費用，也不需要向作者轉帳，但你的每一次拍手都會轉化為作者的收入。拍手需要擁有 Liker ID，Matters 會在徵得用戶同意的情況下，為用戶自動開通 Liker ID 。為使用戶體驗更為流暢，此後每次登入 Matters 時會自動登入 Liker ID。如果你已經擁有Liker ID ，可直接與 Matters ID進行綁定。</p>
+  </details>
 
-<h3>作品發佈至分佈式網路，需要成本嗎？</h3>
-<p>將作品存儲至星際文件系統（IPFS），IPFS 會收取存儲費。在測試階段，Matters 將承擔這筆費用。未來，會以內容發佈者所贏得讚賞的一定比例進行支付。</p>
+  <details class="u-content">
+    <summary>
+      <h4>我是作者，讀者拍手不花錢，那我獲得的 LikeCoin，來源於哪裡？</h4>
+    </summary>
+    <p>你所得到的 LikeCoin 分配一部分來自於 LikeCoin Foundation 基金會— 其中推動社群態的資金會用來資助沒有註冊的讀者，讓讀者在拍手不花錢的情況下也能讓作者收到 LikeCoin。另外一部分是來自於用月費支持創作者的讚賞公民另外一部分是來自用月費支持創作者的<a class="u-link-green" href="https://liker.land/civic?from=matters" target="_blank">讚賞公民</a>。</p>
+  </details>
 
-<h3>評論會被永久保存嗎？</h3>
-<p>一篇作品的「評論」不會同步發送至分佈式網路，只保存在 Matters 網站上。評論可以刪除及修改，可以以@的方式與其他用戶互動，可以被作品原作者置頂。</p>
+  <details class="u-content">
+    <summary>
+      <h4>怎樣查看我的 LikeCoin 收入和當前幣值？</h4>
+    </summary>
+    <p>你擁有的 LikeCoin 總額，可以在 <a class="u-link-green" href="https://like.co" target="_blank">https://like.co</a> 進行查閱。你獲得的 LikeCoin 收入，會直接儲存在 Matters 綁定的電子錢包中，回饋金額由你當日收穫的拍手次數決定，並且受到幣值、當日共有多少作者一起分享月費配對基金的影響而浮動。</p>
+  </details>
 
-<h3>使用關聯功能</h3>
-<p>您可以在編輯器中，或者自己作品詳情頁使用「關聯」功能，連結自己或他人的作品。您可以使用「關聯」功能進行回應、連載、撰寫專欄、作品策展，或用於延伸討論和拓展議題等。</p>
+  <details class="u-content">
+    <summary>
+    <h4>如何將我手中的 LikeCoin 變現？</h4>
+    </summary>
+    <p>可以參考用戶的變現操作：<a class="u-link-green" href="https://matters.news/@twinsangel/在bit-asset交易所把-like-coin-兌換成台幣-zdpuAnz4WQ82ZhG6BTPdSysBbZRax4d6tKXQerjNrnUtVfPHR" target="_blank">在 BitAsset 交易所把 LikeCoin 兌換成台幣</a></p>
+  </details>
 
-<h3>註冊社區會員享有全部功能</h3>
-<p>在未登錄狀態下，您可免費閱讀與分享 Matters 站上作品。但如需創作或參與社區互動，請註冊登錄享有全部功能，包括創作、讚賞、評論、關聯、分享、收藏、點踩評論，追蹤他人等。</p>
-<p>Matters 編輯器是一個實時保存、所見即所得的創作工具。在編輯器內，您可以為作品選擇封面、添加標籤，點按「+」更可為作品內嵌多媒體文件（圖片、音頻、視頻）、代碼塊、超連結等。</p>
-
-<h3>檢舉及申訴</h3>
-<p>如果你發現有違反 Matters《社區約章》的情況，可以點擊進入 <a href="https://trello.com/invite/b/oltVvMMi/8321a9c27ee10fbf2b831119a1e3c90c/matters-lab" target="_blank">MattersLab Trello</a> 看板，在相關板塊添加反饋，或發送郵件至 ask@matters.news 進行檢舉，並描述具體情況。被檢舉的信息一旦經站方查實，站方將根據《用戶協議》和《社區約章》條例進行處理。包括但不限於隱藏作品、用戶禁言、禁止互動，甚至凍結帳戶。與此同時，被檢舉的用戶可以進行意見反饋，或發送郵件至 ask@matters.news 向站方申訴。</p>
-
-<h3>更多建議和意見要對 Matters 說</h3>
-<p>若您希望向 Matters 提出建議，或遭遇操作異常，請登入 <a href="https://trello.com/invite/b/oltVvMMi/8321a9c27ee10fbf2b831119a1e3c90c/matters-lab" target="_blank">MattersLab Trello</a> 相關板塊進行反饋，也可以發送郵件至 ask@matters.news，我們會儘快回覆！</p>
-  `,
-  zh_hans: `
-<h3>Matters 是什么？</h3>
-<p>Matters 正在建立一个基于分布式技术的内容平台。目标是，令创作者生产的所有数据回归其个人所有，永久保存；在此基础上搭建智能合约及加密货币体系，让因内容而产生的经济价值，尽可能回归作者，让参与者获得持续回报；以此体系，推动公共领域的知识生产。</p>
-
-<h3>Matters 为什么要用分布式技术？</h3>
-<p>Matters 站上所有作品（不含评论）已正式上载到星际文件系统（InterPlanetary File System，IPFS）的节点上，实现作品内容的分布式存储。在传统的中心化网络社区里，由于创作者的账户、密码、隐私策略文件、生产的作品和产品的衍生数据，都要存储在社区服务提供商控制的服务器，加之含有版权侵佔条款的社区用户协议，故服务提供商可以从技术上占有、修改、删除、泄漏创作者的作品和数据。Matters 采用的分布式存储技术，可以实现创作者百分之百拥有自己的作品和数据。采用分布式存储后，作为服务提供商的 Matters，以及其他任意的分布式节点，从技术上无法侵害创作者的任何权益，为保护创作者的版权、隐私权和数据权利不受第三方侵害奠定基础。</p>
-
-<h3>分布式技术对我有什么用？</h3>
-<p>第一，通过 Matters 发表的作品，将在分布式网络中永久储存； </p>
-<p>第二，每篇作品都会获得仅作者可见的、独一无二的「数字指纹」； </p>
-<p>第三，您可使用任意分布式公共节点传播作品。</p>
-
-<h3>怎样把作品发布至分布式网络？</h3>
-<p>当您在编辑器中点击「发布」时，会先弹出「作品即将发布到分布式网络」的说明视窗，告知发布规则，并要求您再次确认「发布」，或者「暂存草稿箱」回到一般编辑状态。</p>
-<p>当您再次确认「发布」后，系统会立即将作品存至草稿箱。此时页面会自动跳转到预览状态，出现「正在等待发佈」的标签，作品发布成功后会存储在 IPFS 的公共节点之中，不可再修改或删除。</p>
-
-<h3>作品发布后想删除怎么办？</h3>
-<p>已成功发布的作品，理论上会实现永久存储，无法被删除和修改。若必要，您可以选择在 Matters 站上将已发布的作品「站内隐藏」，隐藏之后，作品在您的列表中会标记为隐藏状态，仅作者可见。作品指纹只有您本人可见，是否要继续传播公共节点所保存的链接，决定权也在您。</p>
+  <details class="u-content">
+    <summary>
+      <h4>我如果不想開通 LikeCoin 可以嗎？</h4>
+    </summary>
+    <p>如果你不想使用 LikeCoin，可以在用戶協議更改時，選擇「不同意開通 Liker ID」。在此情況下，你仍然可以使用 Matters 進行閱讀和評論，你的舊作品也可以獲得 LikeCoin。但由於價值標記工具無法啟用，你將無法發表新文章和給出讚賞，也無法對由 MAT 轉換而得、以及舊作持續積累的 LikeCoin 進行管理或者提現。</p>
+  </details>
+</details>
 
 
-<h3>作品发布至分布式网络，需要成本吗？</h3>
-<p>将作品存储至星际文件系统（IPFS），IPFS 会收取存储费。在测试阶段，Matters 将承担这笔费用。未来，会以内容发布者所赢得赞赏的一定比例进行支付。</p>
+<details class="u-content" open>
+  <summary>
+    <h3>分佈式技術</h3>
+  </summary>
 
-<h3>评论会被永久保存吗？</h3>
-<p>一篇作品的「评论」不会同步发送至分布式网络，只保存在 Matters 网站上。评论可以删除及修改，可以以@的方式与其他用户互动，可以被作品原作者置顶。</p>
+  <details class="u-content">
+    <summary>
+      <h4>Matters 為什麼要用分佈式技術？</h4>
+    </summary>
+    <p>Matters 站上所有作品（不含評論）已正式上載到星際文件系統（InterPlanetary File System，IPFS）的節點上，實現作品內容的分佈式存儲。在傳統的中心化網絡社區裡面，由於創作者的帳戶、密碼、隱私策略文件、生產的作品和產品的衍生數據，都要存儲在社區服務提供商控制的服務器，加之含有版權侵佔條款的社區用戶協議，故服務提供商可以從技術上佔有、修改、刪除、洩漏創作者的作品和數據。</p>
+    <p>Matters 採用的分佈式存儲技術，可以實現創作者百分之百擁有自己的作品和數據。採用分佈式存儲後，作為服務提供商的 Matters，以及其他任意的分佈式節點，從技術上無法侵害創作者的任何權益，為保護創作者的版權、隱私權和數據權利不受第三方侵害奠定基礎。</p>
+  </details>
 
-<h3>使用关联功能</h3>
-<p>您可以在编辑器中，或者自己作品详情页使用「关联」功能，连结自己或他人的作品。您可以使用「关联」功能进行回应、连载、撰写专栏、作品策展，或用于延伸讨论和拓展议题等。</p>
+  <details class="u-content">
+    <summary>
+      <h4>分佈式技術對我有什麼用？</h4>
+    </summary>
+    <p>第一，通過 Matters 發表的作品，將在分佈式網絡中永久儲存；</p>
+    <p>第二，每篇作品都會獲得獨一無二的「數字指紋」，未來幫你追溯被動收入；</p>
+    <p>第三，你可使用任意分佈式公共節點傳播作品。</p>
+  </details>
 
-<h3>注册社区会员享有全部功能</h3>
-<p>在未登录状态下，您可免费阅读与分享 Matters 站上作品。但如需创作或参与社区互动，请注册登录享有全部功能。包括创作、赞赏、评论、关联、分享、收藏、点踩评论，追踪他人等。</p>
-<p>Matters 编辑器是一个实时保存、所见即所得的创作工具。在编辑器内，您可以为作品选择封面、添加标签，点按「+」更可为作品内嵌多媒体文件（图片、音频、视频）、代码块、超连结等。</p>
+  <details class="u-content">
+    <summary>
+      <h4>怎樣把作品發佈至分佈式網絡？</h4>
+    </summary>
+    <p>你通過 Matters 發佈成功的作品，都會儲存在分佈式網絡中。</p>
+    <p>創作完成，在編輯器點擊「發佈」時，系統會先跟你確認，你確認之後，頁面會自動跳轉到「正在等待發佈」的預覽狀態，這一狀態時，系統會自動備份作品至草稿箱以避免丟失，作品發佈成功後，會同時存儲在 IPFS 的公共節點之中，不可再修改或刪除。你也可以選擇「暫存草稿箱」，回到一般編輯狀態。</p>
+  </details>
 
-<h3>检举及申诉</h3>
-<p>如果你发现有违反 Matters《社区约章》的情况，可以点击进入 <a href="https://trello.com/invite/b/oltVvMMi/8321a9c27ee10fbf2b831119a1e3c90c/matters-lab" target="_blank">MattersLab Trello</a> 看板，在相关板块添加反馈，或发送邮件至 ask@matters.news 进行举报，并描述具体情况。被举报的信息一旦经站方查实，站方将根据《用户协议》和《社区约章》条例进行处理。包括但不限于隐藏作品、用户禁言、禁止互动，甚至冻结帐户。与此同时，被举报的用户可以进行意见反馈，或发送邮件至 ask@matters.news 向站方申诉。</p>
+  <details class="u-content">
+    <summary>
+    <h4>作品發佈後想刪除怎麼辦？</h4>
+    </summary>
+    <p>已成功發佈的作品，理論上會實現永久存儲，無法被刪除和修改。但存儲是否能被他人可見，主動權在你。你可以選擇在 Matters 站上將已發佈的作品「站內隱藏」，隱藏之後，作品在你的列表裡會標記為隱藏狀態，僅作者可見。作品指紋只有你本人可見，是否要繼續傳播公共節點所保存的鏈接，決定權也在你。</p>
+  </details>
 
-<h3>更多建议和意见要对 Matters 说</h3>
-<p>若您希望向 Matters 提出建议，或遭遇操作异常，请登入 <a href="https://trello.com/invite/b/oltVvMMi/8321a9c27ee10fbf2b831119a1e3c90c/matters-lab" target="_blank">MattersLab Trello</a> 相关板块进行反馈，也可以发送邮件至 ask@matters.news，我们会尽快回复！</p>
+  <details class="u-content">
+    <summary>
+      <h4>作品發佈至分佈式網絡，需要成本嗎？</h4>
+    </summary>
+    <p>將作品存儲至星際文件系統（IPFS），IPFS 會收取存儲費。在測試階段，Matters 將承擔這筆費用。未來，會以內容發佈者所贏得讚賞的一定比例進行支付。</p>
+  </details>
+</details>
 
+<details class="u-content">
+  <summary>
+    <h3>評論會被永久保存嗎？</h3>
+  </summary>
+  <p>一篇作品的「評論」不會同步發送至分佈式網絡，只保存在 Matters 網站上。評論可以刪除及修改，可以以 @ 的方式與其他用戶互動，可以被作品原作者置頂。</p>
+</details>
+
+<details class="u-content">
+  <summary>
+    <h3>使用關聯功能</h3>
+  </summary>
+  <p>你可以在編輯器中，或者自己作品詳情頁使用「關聯」功能，連結自己或他人的作品。你可以使用「關聯」功能進行回應、連載、撰寫專欄、作品策展，或用於延伸討論和拓展議題等。</p>
+</details>
+
+<details class="u-content">
+  <summary>
+    <h3>註冊社區會員享有全部功能</h3>
+  </summary>
+  <p>在未登錄狀態下，你可免費閱讀與分享 Matters 站上作品。但如需創作或參與社區互動，請註冊登錄享有全部功能，包括創作、讚賞、評論、關聯、分享、收藏、點踩評論，追蹤他人等。</p>
+</details>
+
+<details class="u-content">
+  <summary>
+    <h3>檢舉及申訴</h3>
+  </summary>
+  <p>Matters 實行社區自治，當用戶認為出現違反社區共識的行為時，可以發起訴訟，於此同時，被檢舉的用戶同時可以在訴訟貼中進行申辯。當訴訟貼獲得超過一定標準的讚賞人數後，即啟動社區審判。一旦社區審判中，出現「贊同懲罰」的多數意見，即實行懲罰。</p>
+  <p>在第一版的社區自治規則中，懲罰措施由 Matters 團隊決定，並向社區公告。而通過社區自治進行的懲罰案例將作為站方執法的判例，列入社區約章。</p>
+  <p>社區自治具體實行方法，請點擊<a class="u-link-green" href="https://matters.news/@hi176/matters-%E7%A4%BE%E5%8C%BA%E8%87%AA%E6%B2%BB%E5%AE%9E%E9%AA%8C%E5%90%AF%E5%8A%A8-zdpuAtuu2Y33zg5L7WLDonpa9RRsYNXHu7B9kYkZPhfXQSMmQ" target="_blank">這裡</a>查看（自治規則會根據運行情況隨時更新）。</p>
+</details>
+
+
+<details class="u-content">
+  <summary>
+    <h3>更多建議和意見要對 Matters 說</h3>
+  </summary>
+  <p>若你希望向 Matters 提出建議，或遭遇操作異常，請發送郵件至 <a class="u-link-green" href="mailto:ask@matters.news" target="_blank">ask@matters.news</a>，我們會儘快回覆！</p>
+</details>
+`,
+  zh_hans: /* html */ `
+  <details class="u-content" open>
+  <summary>
+    <h3>Matters 是什么？</h3>
+  </summary>
+  <p>Matters 正在建立一个基于分布式技术的内容平台。目标是，令创作者生产的所有数据回归其个人所有，永久保存；在此基础上搭建智能合约及加密货币体系，让因内容而产生的经济价值，尽可能回归作者，让参与者获得持续回报；以此体系，推动公共领域的知识生产。</p>
+</details>
+
+<details class="u-content">
+  <summary>
+    <h3>LikeCoin 是什么？</h3>
+  </summary>
+  <p>LikeCoin Foundation 致力开发出一套基于区块链的基础设施，让创作者真正拥有内容，获得实质回馈，同时内容不被窜改，得以永久保存，既促进衍生转发，也有助求真核实。 LikeCoin 是 LikeCoin Foundation 推出的加密货币。更多资讯，请看<a class="u-link-green" href="https://matters.news/@likecoin/%E5%8C%96%E8%AE%9A%E7%82%BA%E8%B3%9E-%E8%AE%9A%E8%B3%9E%E5%85%AC%E6%B0%91%E6%87%B6%E4%BA%BA%E5%8C%85-zdpuAtWT6a2rjr75JZBzwnvy67vrxw5cBRqwnHbkFehRYHLXx" target="_blank">赞赏公民懒人包</a>。</p>
+</details>
+
+<details class="u-content">
+  <summary>
+    <h3>MAT 与 LikeCoin</h3>
+  </summary>
+  <p>2019 年 10 月起，<a href="https://matters.news/@az/%E7%B5%A6%E7%A4%BE%E5%8D%80%E7%9A%84%E4%B8%80%E5%B0%81%E4%BF%A1-matters-%E8%88%87-like-coin%E5%AE%A3%E4%BD%88%E5%90%88%E4%BD%9C-zdpuB3JTzZ8Ch2J9NbPsJ79NKuAJB3ehie3KWf78rimdCefo4" target="_blank" class="u-link-green">Matters 与 LikeCoin 联手</a>，将 LikeCoin 这一基于写作者创造力为衡量的加密货币引入 Matters 的自由创作和公共讨论中，让平台效应以收入的形式回馈给作者。</p>
+  <p>请持有 MAT 的用户注意：在东八区时间 2019 年 10 月 15 日 10:00 后，Matters 平台中的所有 MAT 按键，将替换为 LikeCoin 赞赏键。在此之前已持有 MAT 的用户，即会获得以固定汇率自动兑换成的 LikeCoin，兑换汇率为 <b>1 MAT = 10 LikeCoin</b>。</p>
+  <p>2019 年 10 月 15 日 10:00 后注册的用户，将不再持有 MAT，直接获得 LikeCoin。</p>
+</details>
+
+<details class="u-content" open>
+  <summary>
+    <h3>如何使用 LikeCoin</h3>
+  </summary>
+
+  <details class="u-content">
+    <summary>
+      <h4>我是读者，怎样用 LikeCoin 赞赏作品？</h4>
+    </summary>
+    <p>只需点击 LikeCoin 赞赏键为你欣赏的作品拍手，即可化赞为赏。你可以向一篇作品拍手5次，不需要支付任何费用，也不需要向作者转帐，但你的每一次拍手都会转化为作者的收入。拍手需要拥有 Liker ID，Matters 会在征得用户同意的情况下，为用户自动开通 Liker ID 。为使用户体验更为流畅，此后每次登入 Matters 时会自动登入 Liker ID。如果你已经拥有Liker ID ，可直接与 Matters ID进行绑定。</p>
+  </details>
+
+  <details class="u-content">
+    <summary>
+      <h4>我是作者，读者拍手不花钱，那我获得的 LikeCoin，来源于哪里？</h4>
+    </summary>
+    <p>你所得到的 LikeCoin 分配一部分来自于 LikeCoin Foundation 基金会— 其中推动社群态的资金会用来资助没有注册的读者，让读者在拍手不花钱的情况下也能让作者收到 LikeCoin。另外一部分是来自于用月费支持创作者的赞赏公民另外一部分是来自用月费支持创作者的<a class="u-link-green" href="https://liker.land/civic?from=matters" target="_blank">赞赏公民</a>。</p>
+  </details>
+
+  <details class="u-content">
+    <summary>
+      <h4>怎样查看我的 LikeCoin 收入和当前币值？</h4>
+    </summary>
+    <p>你拥有的 LikeCoin 总额，可以在 <a class="u-link-green" href="https://like.co" target="_blank">https://like.co</a> 进行查阅。你获得的 LikeCoin 收入，会直接储存在 Matters 绑定的电子钱包中，回馈金额由你当日收获的拍手次数决定，并且受到币值、当日共有多少作者一起分享月费配对基金的影响而浮动。</p>
+  </details>
+
+  <details class="u-content">
+    <summary>
+    <h4>如何将我手中的 LikeCoin 变现？</h4>
+    </summary>
+    <p>可以参考用户的变现操作：<a class="u-link-green" href="https://matters.news/@twinsangel/在bit-asset交易所把-like-coin-兑换成台币-zdpuAnz4WQ82ZhG6BTPdSysBbZRax4d6tKXQerjNrnUtVfPHR" target="_blank">在 BitAsset 交易所把 LikeCoin 兑换成台币</a></p>
+  </details>
+
+  <details class="u-content">
+    <summary>
+      <h4>我如果不想开通 LikeCoin 可以吗？</h4>
+    </summary>
+    <p>如果你不想使用 LikeCoin，可以在用户协议更改时，选择「不同意开通 Liker ID」。在此情况下，你仍然可以使用 Matters 进行阅读和评论，你的旧作品也可以获得 LikeCoin。但由于价值标记工具无法启用，你将无法发表新文章和给出赞赏，也无法对由 MAT 转换而得、以及旧作持续积累的 LikeCoin 进行管理或者提现。</p>
+  </details>
+</details>
+
+
+<details class="u-content" open>
+  <summary>
+    <h3>分布式技术</h3>
+  </summary>
+
+  <details class="u-content">
+    <summary>
+      <h4>Matters 为什么要用分布式技术？</h4>
+    </summary>
+    <p>Matters 站上所有作品（不含评论）已正式上载到星际文件系统（InterPlanetary File System，IPFS）的节点上，实现作品内容的分布式存储。在传统的中心化网络社区里面，由于创作者的帐户、密码、隐私策略文件、生产的作品和产品的衍生数据，都要存储在社区服务提供商控制的服务器，加之含有版权侵占条款的社区用户协议，故服务提供商可以从技术上占有、修改、删除、泄漏创作者的作品和数据。</p>
+    <p>Matters 采用的分布式存储技术，可以实现创作者百分之百拥有自己的作品和数据。采用分布式存储后，作为服务提供商的 Matters，以及其他任意的分布式节点，从技术上无法侵害创作者的任何权益，为保护创作者的版权、隐私权和数据权利不受第三方侵害奠定基础。</p>
+  </details>
+
+  <details class="u-content">
+    <summary>
+      <h4>分布式技术对我有什么用？</h4>
+    </summary>
+    <p>第一，通过 Matters 发表的作品，将在分布式网络中永久储存；</p>
+    <p>第二，每篇作品都会获得独一无二的「数字指纹」，未来帮你追溯被动收入；</p>
+    <p>第三，你可使用任意分布式公共节点传播作品。</p>
+  </details>
+
+  <details class="u-content">
+    <summary>
+      <h4>怎样把作品发布至分布式网络？</h4>
+    </summary>
+    <p>你通过 Matters 发布成功的作品，都会储存在分布式网络中。</p>
+    <p>创作完成，在编辑器点击「发布」时，系统会先跟你确认，你确认之后，页面会自动跳转到「正在等待发布」的预览状态，这一状态时，系统会自动备份作品至草稿箱以避免丢失，作品发布成功后，会同时存储在 IPFS 的公共节点之中，不可再修改或删除。你也可以选择「暂存草稿箱」，回到一般编辑状态。</p>
+  </details>
+
+  <details class="u-content">
+    <summary>
+    <h4>作品发布后想删除怎么办？</h4>
+    </summary>
+    <p>已成功发布的作品，理论上会实现永久存储，无法被删除和修改。但存储是否能被他人可见，主动权在你。你可以选择在 Matters 站上将已发布的作品「站内隐藏」，隐藏之后，作品在你的列表里会标记为隐藏状态，仅作者可见。作品指纹只有你本人可见，是否要继续传播公共节点所保存的链接，决定权也在你。</p>
+  </details>
+
+  <details class="u-content">
+    <summary>
+      <h4>作品发布至分布式网络，需要成本吗？</h4>
+    </summary>
+    <p>将作品存储至星际文件系统（IPFS），IPFS 会收取存储费。在测试阶段，Matters 将承担这笔费用。未来，会以内容发布者所赢得赞赏的一定比例进行支付。</p>
+  </details>
+</details>
+
+<details class="u-content">
+  <summary>
+    <h3>评论会被永久保存吗？</h3>
+  </summary>
+  <p>一篇作品的「评论」不会同步发送至分布式网络，只保存在 Matters 网站上。评论可以删除及修改，可以以 @ 的方式与其他用户互动，可以被作品原作者置顶。</p>
+</details>
+
+<details class="u-content">
+  <summary>
+    <h3>使用关联功能</h3>
+  </summary>
+  <p>你可以在编辑器中，或者自己作品详情页使用「关联」功能，连结自己或他人的作品。你可以使用「关联」功能进行回应、连载、撰写专栏、作品策展，或用于延伸讨论和拓展议题等。</p>
+</details>
+
+<details class="u-content">
+  <summary>
+    <h3>注册社区会员享有全部功能</h3>
+  </summary>
+  <p>在未登录状态下，你可免费阅读与分享 Matters 站上作品。但如需创作或参与社区互动，请注册登录享有全部功能，包括创作、赞赏、评论、关联、分享、收藏、点踩评论，追踪他人等。</p>
+</details>
+
+<details class="u-content">
+  <summary>
+    <h3>检举及申诉</h3>
+  </summary>
+  <p>Matters 实行社区自治，当用户认为出现违反社区共识的行为时，可以发起诉讼，于此同时，被检举的用户同时可以在诉讼贴中进行申辩。当诉讼贴获得超过一定标准的赞赏人数后，即启动社区审判。一旦社区审判中，出现「赞同惩罚」的多数意见，即实行惩罚。</p>
+  <p>在第一版的社区自治规则中，惩罚措施由 Matters 团队决定，并向社区公告。而通过社区自治进行的惩罚案例将作为站方执法的判例，列入社区约章。</p>
+  <p>社区自治具体实行方法，请点击<a class="u-link-green" href="https://matters.news/@hi176/matters-%E7%A4%BE%E5%8C%BA%E8%87%AA%E6%B2%BB%E5%AE%9E%E9%AA%8C%E5%90%AF%E5%8A%A8-zdpuAtuu2Y33zg5L7WLDonpa9RRsYNXHu7B9kYkZPhfXQSMmQ" target="_blank">这里</a>查看（自治规则会根据运行情况随时更新）。</p>
+</details>
+
+
+<details class="u-content">
+  <summary>
+    <h3>更多建议和意见要对 Matters 说</h3>
+  </summary>
+  <p>若你希望向 Matters 提出建议，或遭遇操作异常，请发送邮件至 <a class="u-link-green" href="mailto:ask@matters.news" target="_blank">ask@matters.news</a>，我们会尽快回复！</p>
+</details>
   `
 }
