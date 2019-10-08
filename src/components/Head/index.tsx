ecada0790bc55bbc709d23ffca0abd725064073d
RoberTu <robertu0717@gmail.com>
2019-10-08 12:35:10 +0800

    Upgrade Next.js to 9.1 with folder structure enhancment

--- src/components/Head/index.tsx
+++ src/components/Head/index.tsx
@@ -6,11 +6,17 @@ import { useContext } from 'react'
 import { LanguageContext } from '~/components'
 
 import { langConvert, translate } from '~/common/utils'
-import FAVICON_16 from '~/static/favicon-16x16.png?url'
-import FAVICON_32 from '~/static/favicon-32x32.png?url'
-import ICON_192 from '~/static/icon-192x192.png?url'
-import IMAGE_INTRO from '~/static/images/intro.jpg'
-import OPENSEARCH from '~/static/opensearch.xml'
+import IMAGE_FAVICON_16 from '~/static/favicon-16x16.png?url'
+import IMAGE_FAVICON_32 from '~/static/favicon-32x32.png?url'
+import IMAGE_LOGO_192 from '~/static/icon-192x192.png?url'
+import IMAGE_INTRO from '~/static/images/intro.jpg?url'
+import IMAGE_LAUNCH_1125 from '~/static/images/splashscreens/launch-1125x2436.png?url'
+import IMAGE_LAUNCH_1242 from '~/static/images/splashscreens/launch-1242x2148.png?url'
+import IMAGE_LAUNCH_1536 from '~/static/images/splashscreens/launch-1536x2048.png?url'
+import IMAGE_LAUNCH_1668 from '~/static/images/splashscreens/launch-1668x2224.png?url'
+import IMAGE_LAUNCH_2048 from '~/static/images/splashscreens/launch-2048x2732.png?url'
+import IMAGE_LAUNCH_640 from '~/static/images/splashscreens/launch-640x1136.png?url'
+import IMAGE_LAUNCH_750 from '~/static/images/splashscreens/launch-750x1294.png?url'
 
 const {
   publicRuntimeConfig: { ENV, SITE_DOMAIN, FB_APP_ID }
@@ -63,21 +69,21 @@ const BaseHead: React.FC<HeadProps> = props => {
       <link
         rel="icon"
         type="image/png"
-        href={FAVICON_32}
+        href={IMAGE_FAVICON_16}
         sizes="32x32"
         key="favicon-32"
       />
       <link
         rel="icon"
         type="image/png"
-        href={FAVICON_16}
+        href={IMAGE_FAVICON_32}
         sizes="16x16"
         key="favicon-16"
       />
       <link
         rel="search"
         title="Matters"
-        href={OPENSEARCH}
+        href="/static/opensearch.xml"
         type="application/opensearchdescription+xml"
         key="opensearch"
       />
@@ -134,7 +140,11 @@ const BaseHead: React.FC<HeadProps> = props => {
       <meta name="twitter:image" key="twitter:image" content={head.image} />
 
       {/* PWA */}
-      <link rel="apple-touch-icon" key="apple-touch-icon" href={ICON_192} />
+      <link
+        rel="apple-touch-icon"
+        key="apple-touch-icon"
+        href={IMAGE_LOGO_192}
+      />
       <meta name="application-name" key="application-name" content="Matters" />
       <meta name="theme-color" key="theme-color" content="#0d6763" />
       <meta
@@ -166,43 +176,43 @@ const BaseHead: React.FC<HeadProps> = props => {
       <link
         rel="apple-touch-startup-image"
         key="apple-touch-startup-image-640x1136"
-        href="/static/splashscreens/launch-640x1136.png"
+        href={IMAGE_LAUNCH_640}
         media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
       />
       <link
         rel="apple-touch-startup-image"
         key="apple-touch-startup-image-750x1294"
-        href="/static/splashscreens/launch-750x1294.png"
+        href={IMAGE_LAUNCH_750}
         media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
       />
       <link
         rel="apple-touch-startup-image"
         key="apple-touch-startup-image-1242x2148"
-        href="/static/splashscreens/launch-1242x2148.png"
+        href={IMAGE_LAUNCH_1242}
         media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
       />
       <link
         rel="apple-touch-startup-image"
         key="apple-touch-startup-image-1125x2436"
-        href="/static/splashscreens/launch-1125x2436.png"
+        href={IMAGE_LAUNCH_1125}
         media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
       />
       <link
         rel="apple-touch-startup-image"
         key="apple-touch-startup-image-1536x2048"
-        href="/static/splashscreens/launch-1536x2048.png"
+        href={IMAGE_LAUNCH_1536}
         media="(min-device-width: 768px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)"
       />
       <link
         rel="apple-touch-startup-image"
         key="apple-touch-startup-image-1668x2224"
-        href="/static/splashscreens/launch-1668x2224.png"
+        href={IMAGE_LAUNCH_1668}
         media="(min-device-width: 834px) and (max-device-width: 834px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)"
       />
       <link
         rel="apple-touch-startup-image"
         key="apple-touch-startup-image-2048x2732"
-        href="/static/splashscreens/launch-2048x2732.png"
+        href={IMAGE_LAUNCH_2048}
         media="(min-device-width: 1024px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)"
       />
 
