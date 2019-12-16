bd6bed5a4fb17e08b3b4e95d8673fa9c935880dd
Zeck Li <devformatters@gmail.com>
2019-12-16 21:22:38 +0800

    Update shell script.

--- bdd/shells/browser.sh
+++ bdd/shells/browser.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-apt update
-apt install -y build-essential
+# Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt install -y ./google-chrome-stable_current_amd64.deb
--- bdd/shells/init.sh
+++ bdd/shells/init.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-echo fs.inotify.max_user_watches=524288 | tee -a /etc/sysctl.conf && sysctl -p
-cp .env.example .env
-npm run build
-npm run start &
+apg update
+apt install -y build-essential
