8b703e165cefd35c88cebdfd4e6bdec5b163a41c
Zeck Li <devformatters@gmail.com>
2019-12-16 18:09:58 +0800

    Update shell script.

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -22,7 +22,7 @@ module.exports = {
       desiredCapabilities: {
         browserName: 'chrome',
         chromeOptions: {
-          args: ['headless', 'no-sandbox']
+          //args: ['headless', 'no-sandbox']
         }
       }
     },
