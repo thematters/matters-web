161657f5834ca2f0cf6c1ed154714eb62bb8816e
Zeck Li <devformatters@gmail.com>
2019-12-16 21:46:50 +0800

    Update shell script.

--- bdd/shells/browser.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt install -y ./google-chrome-stable_current_amd64.deb
--- bdd/shells/init.sh
+++ bdd/shells/init.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-apg update
+apt update
 apt install -y build-essential
+
+# chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt install -y ./google-chrome-stable_current_amd64.deb
+
+# TODO: firefox
