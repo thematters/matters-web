7c1d1755982b87c4420a4cf818ea95e92ae046ad
Zeck Li <devformatters@gmail.com>
2019-12-16 14:54:22 +0800

    Update shell script.

--- bdd/shells/chrome.sh
+++ bdd/shells/chrome.sh
@@ -3,4 +3,4 @@
 apt update
 apt install -y build-essential
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt install ./google-chrome-stable_current_amd64.deb
+apt install -y ./google-chrome-stable_current_amd64.deb
