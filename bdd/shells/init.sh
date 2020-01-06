93c3f5f3d2bdaefd88b5f32ab96b299e06ad9cec
Zeck Li <devformatters@gmail.com>
2020-01-06 12:24:07 +0800

    Trigger BDD test every commit.

--- bdd/shells/init.sh
+++ bdd/shells/init.sh
@@ -3,6 +3,8 @@
 apt update
 apt install -y build-essential
 
+echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
+
 # chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt install -y ./google-chrome-stable_current_amd64.deb
