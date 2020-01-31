58de953b48b22cdc4476e5f309325b087a659b6e
Zeck Li <devformatters@gmail.com>
2020-01-31 15:37:11 +0800

    Extract tag articles query from components.

--- src/components/GQL/queries/tagArticles.ts
+++ src/components/GQL/queries/tagArticles.ts
@@ -3,11 +3,11 @@ import gql from 'graphql-tag'
 import { ArticleDigest } from '~/components/ArticleDigest'
 
 export default gql`
-  query TagDetailArticles($id: ID!, $after: String) {
+  query TagArticles($id: ID!, $after: String, $selected: Boolean) {
     node(input: { id: $id }) {
       ... on Tag {
         id
-        articles(input: { first: 10, after: $after }) {
+        articles(input: { first: 10, after: $after, selected: $selected }) {
           pageInfo {
             startCursor
             endCursor
