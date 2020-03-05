62cbd2a0924b71acc5e8d8427d044fafc82a3bcf
robertu <robertu0717@gmail.com>
2020-03-05 11:35:19 +0800

    Move back home sides

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,9 +1,22 @@
 import { Layout } from '~/components'
 
+import Authors from './Authors'
 import Feed from './Feed'
+import Icymi from './Icymi'
+import Tags from './Tags'
+import Topics from './Topics'
 
 export default () => (
-  <Layout>
+  <Layout
+    rightSide={
+      <>
+        <Icymi />
+        <Tags />
+        <Topics />
+        <Authors />
+      </>
+    }
+  >
     <Feed />
   </Layout>
 )
