4e8b735b022cc8dfeac7b9b7c13ee0acfef2b965
robertu <robertu0717@gmail.com>
2020-03-19 17:38:44 +0800

    Revise `<Toolbar.Appreciators>`  based on latest UI

--- src/views/ArticleDetail/Toolbar/Appreciators/index.tsx
+++ src/views/ArticleDetail/Toolbar/Appreciators/index.tsx
@@ -15,6 +15,7 @@ const fragments = {
   article: gql`
     fragment AppreciatorsArticle on Article {
       id
+      appreciationsReceivedTotal
       received: appreciationsReceived(input: { first: 4 }) {
         totalCount
         edges {
@@ -38,15 +39,15 @@ const fragments = {
 
 const Appreciators = ({ article }: { article: AppreciatorsArticle }) => {
   const edges = article.received.edges
-  const count = numAbbr(article.received.totalCount)
-  const hasAppreciators = edges && edges.length > 0
+  const totalReceivedCount = article.appreciationsReceivedTotal
+  const appreciatorCount = Math.min(article.received.totalCount, 99)
   const appreciators = [
     ...(edges?.map(({ node }) => node.sender).filter(user => !!user) || []),
     null,
     null,
     null,
     null
-  ].slice(0, 4)
+  ].slice(0, appreciatorCount >= 4 ? 3 : 4)
 
   return (
     <AppreciatorsDialog article={article}>
@@ -58,16 +59,16 @@ const Appreciators = ({ article }: { article: AppreciatorsArticle }) => {
           aria-label={TEXT.zh_hant.viewAppreciators}
           aria-haspopup="true"
         >
-          {hasAppreciators ? (
-            <p className="count highlight">
+          {totalReceivedCount > 0 ? (
+            <p className="received-count highlight">
               <Translate
-                zh_hant={`${count} 人贊賞`}
-                zh_hans={`${count} 人赞赏`}
+                zh_hant={`${numAbbr(totalReceivedCount)} 贊賞`}
+                zh_hans={`${numAbbr(totalReceivedCount)} 赞赏`}
               />
             </p>
           ) : (
-            <p className="count">
-              <Translate zh_hant="還沒有人讚賞" zh_hans="还没有人赞赏" />
+            <p className="received-count">
+              <Translate zh_hant="還沒有讚賞" zh_hans="还没有赞赏" />
             </p>
           )}
 
@@ -80,6 +81,9 @@ const Appreciators = ({ article }: { article: AppreciatorsArticle }) => {
                 key={index}
               />
             ))}
+            {appreciatorCount > 4 && (
+              <span className="appreciator-count">{appreciatorCount}</span>
+            )}
           </section>
 
           <style jsx>{styles}</style>
