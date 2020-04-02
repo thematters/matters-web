526d98adaf4539f8cd2a4770acf06a45ac2a24e5
Guo Liu <liuguomailbox@gmail.com>
2020-04-02 16:22:31 -0700

    add view mode anlytics

--- src/common/enums/analytics.ts
+++ src/common/enums/analytics.ts
@@ -16,6 +16,7 @@ export const ANALYTICS_EVENTS = {
   CLICK_DRAFT: 'click-draft',
   CLICK_FREQUENT_SEARCH: 'click-frequent-search',
   CLICK_SEARCH_HISTORY: 'click-search-history',
+  CLICK_VIEW_MODE: 'click-view-mode',
   COMMENT_EDITOR_CHANGE: 'comment-editor-change',
   ARTICLE_BOTTOM_CROSS: 'article-bottom-cross',
   PUBLISH_ERROR: 'publish-error',
