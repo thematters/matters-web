aa1a089e255ebcd35c85cb653e9a92f18d2ba824
robertu <robertu0717@gmail.com>
2020-04-10 18:26:19 +0800

    Fix zIndex of tooltip in appreciation button; Fix lineHeight of displayName row;

--- src/components/UserProfile/Badges/index.tsx
+++ src/components/UserProfile/Badges/index.tsx
@@ -4,9 +4,14 @@ import { EXTERNAL_LINKS } from '~/common/enums'
 import { ReactComponent as IconCivicLikerBadge } from '~/static/icons/civic-liker-badge.svg'
 import { ReactComponent as IconSeedBadge } from '~/static/icons/early-user-badge.svg'
 
+import styles from './styles.css'
+
 export const SeedBadge = () => (
   <Tooltip content={<Translate zh_hant="種子用戶" zh_hans="种子用户" />}>
-    <span>{withIcon(IconSeedBadge)({})}</span>
+    <span className="seed-badge">
+      {withIcon(IconSeedBadge)({})}
+      <style jsx>{styles}</style>
+    </span>
   </Tooltip>
 )
 
