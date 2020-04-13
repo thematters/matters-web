f05c8cb2e9bd12e9c03eecdc54bc10a914886336
robertu <robertu0717@gmail.com>
2020-04-13 19:53:05 +0800

    Add back links

--- src/views/Me/Settings/Blocked/index.tsx
+++ src/views/Me/Settings/Blocked/index.tsx
@@ -1,11 +1,13 @@
 import { Layout } from '~/components'
 
+import { PATHS } from '~/common/enums'
+
 import SettingsBlocked from './SettingsBlocked'
 
 export default () => (
   <Layout.Main>
     <Layout.Header
-      left={<Layout.Header.BackButton />}
+      left={<Layout.Header.BackButton {...PATHS.ME_SETTINGS} />}
       right={<Layout.Header.Title id="settingsBlock" />}
       marginBottom={0}
     />
