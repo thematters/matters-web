fa324ddb26b544c054692a83142dbd4cf351c341
robertu <robertu0717@gmail.com>
2020-05-07 16:18:02 +0800

    use <ViewContext> instead of another API query

--- src/views/Follow/index.tsx
+++ src/views/Follow/index.tsx
@@ -1,31 +1,18 @@
-import { useQuery } from '@apollo/react-hooks'
 import gql from 'graphql-tag'
 import { useContext, useEffect } from 'react'
 
-import { Layout, Spinner, useResponsive, ViewerContext } from '~/components'
+import { Layout, useResponsive, ViewerContext } from '~/components'
 import { useMutation } from '~/components/GQL'
 import viewerUnreadFolloweeArticles from '~/components/GQL/updates/viewerUnreadFolloweeArticles'
 
 import FollowFeed from './FollowFeed'
 import PickAuthors from './PickAuthors'
 
-import { MeFollow } from './__generated__/MeFollow'
 import { ReadFolloweeArticles } from './__generated__/ReadFolloweeArticles'
 
 const READ_FOLLOWEE_ARTICLES = gql`
   mutation ReadFolloweeArticles {
     logRecord(input: { type: ReadFolloweeArticles })
-  }
-`
-
-const ME_FOLLOW = gql`
-  query MeFollow {
-    viewer {
-      id
-      followees(input: { first: 0 }) {
-        totalCount
-      }
-    }
   }
 `
 
@@ -37,7 +24,6 @@ const BaseFollow = () => {
       update: viewerUnreadFolloweeArticles,
     }
   )
-  const { data, loading } = useQuery<MeFollow>(ME_FOLLOW)
 
   useEffect(() => {
     if (viewer.isAuthed) {
@@ -45,15 +31,7 @@ const BaseFollow = () => {
     }
   }, [])
 
-  if (loading) {
-    return <Spinner />
-  }
-
-  if (!data) {
-    return null
-  }
-
-  const followeeCount = data?.viewer?.followees.totalCount || 0
+  const followeeCount = viewer?.followees.totalCount || 0
 
   if (followeeCount < 5) {
     return <PickAuthors />
