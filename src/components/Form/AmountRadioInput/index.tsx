b9812b4b8548415049243f2b62dc844be6112cf4
Zeck Li <devformatters@gmail.com>
2020-05-07 13:58:13 +0800

    Add form components for PayTo.

---
+++ src/components/Form/AmountRadioInput/index.tsx
@@ -0,0 +1,121 @@
+import VisuallyHidden from '@reach/visually-hidden'
+import classNames from 'classnames'
+
+import { PAYMENT_CURRENCY } from '~/common/enums'
+
+import Field, { FieldProps } from '../Field'
+import styles from './styles.css'
+
+/**
+ *
+ * Usage:
+ *
+ * ```jsx
+ *   <Form.AmountRadioInput
+ *     error="xxx"
+ *     hint="xxx"
+ *     ...other <input> props...
+ *   />
+ * ```
+ *
+ */
+
+type AmountOptionProps = {
+  amount: number
+  isLike: boolean
+  name: string
+} & FieldProps &
+  React.DetailedHTMLProps<
+    React.InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement
+  >
+
+type AmountRadioInputProps = {
+  currency: PAYMENT_CURRENCY
+  isLike: boolean
+  name: string
+} & Omit<FieldProps, 'fieldMsgId'> &
+  React.DetailedHTMLProps<
+    React.InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement
+  >
+
+const amountOptions = {
+  [PAYMENT_CURRENCY.HKD]: [5, 10, 50, 80, 100, 200],
+  [PAYMENT_CURRENCY.LIKE]: [160, 660, 1660],
+}
+
+const AmountOption: React.FC<AmountOptionProps> = ({
+  amount,
+  isLike,
+  name,
+
+  fieldMsgId,
+  value,
+
+  ...inputProps
+}) => {
+  const fieldId = `field-${name}-${amount}`
+  const classes = classNames({
+    amount: true,
+    [isLike ? 'like' : 'hkd']: true,
+    active: value === amount,
+  })
+  return (
+    <section className={classes}>
+      <label htmlFor={fieldId}>
+        {amount}
+        <VisuallyHidden>
+          <input
+            {...inputProps}
+            aria-describedby={fieldMsgId}
+            id={fieldId}
+            name={name}
+            value={amount}
+          />
+        </VisuallyHidden>
+      </label>
+      <style jsx>{styles}</style>
+    </section>
+  )
+}
+
+const AmountRadioInput: React.FC<AmountRadioInputProps> = ({
+  currency,
+  isLike,
+  name,
+
+  hint,
+  error,
+
+  ...inputProps
+}) => {
+  const fieldMsgId = `field-msg-${name}`
+  const options = amountOptions[currency]
+
+  const baseInputProps = {
+    ...inputProps,
+    fieldMsgId,
+    isLike,
+    name,
+    type: 'radio',
+  }
+
+  return (
+    <Field>
+      <Field.Content>
+        <section className="amount-options">
+          {options.map((option) => (
+            <AmountOption {...baseInputProps} key={option} amount={option} />
+          ))}
+        </section>
+      </Field.Content>
+
+      <Field.Footer fieldMsgId={fieldMsgId} hint={hint} error={error} />
+
+      <style jsx>{styles}</style>
+    </Field>
+  )
+}
+
+export default AmountRadioInput
---
+++ src/components/Form/CurrencyRadioInput/index.tsx
@@ -0,0 +1,127 @@
+import VisuallyHidden from '@reach/visually-hidden'
+import classNames from 'classnames'
+
+import { Translate } from '~/components'
+
+import { PAYMENT_CURRENCY } from '~/common/enums'
+
+import Field, { FieldProps } from '../Field'
+import styles from './styles.css'
+
+/**
+ *
+ * Usage:
+ *
+ * ```jsx
+ *   <Form.CurrencyRadioInput
+ *     error="xxx"
+ *     hint="xxx"
+ *     ...other <input> props...
+ *   />
+ * ```
+ *
+ */
+
+type CurrencyOptionProps = {
+  inactive: boolean
+  isLike: boolean
+  name: string
+  optionName: React.ReactNode
+  optionValue: PAYMENT_CURRENCY
+  optionType: 'hkd' | 'like'
+} & FieldProps &
+  React.DetailedHTMLProps<
+    React.InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement
+  >
+
+type CurrencyRadioInputProps = {
+  name: string
+  isLike: boolean
+} & Omit<FieldProps, 'fieldMsgId'> &
+  React.DetailedHTMLProps<
+    React.InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement
+  >
+
+const CurrencyOption: React.FC<CurrencyOptionProps> = ({
+  inactive,
+  isLike,
+  name,
+  optionName,
+  optionValue,
+  optionType,
+
+  fieldMsgId,
+  value,
+
+  ...inputProps
+}) => {
+  const fieldId = `field-${name}-${optionType}`
+  const classes = classNames({
+    currency: true,
+    [isLike ? 'like' : 'hkd']: true,
+    inactive,
+  })
+  return (
+    <section className={classes}>
+      <label htmlFor={fieldId}>
+        {optionName}
+        <VisuallyHidden>
+          <input
+            {...inputProps}
+            aria-describedby={fieldMsgId}
+            id={fieldId}
+            name={name}
+            value={optionValue}
+          />
+        </VisuallyHidden>
+      </label>
+      <style jsx>{styles}</style>
+    </section>
+  )
+}
+
+const CurrencyRadioInput: React.FC<CurrencyRadioInputProps> = ({
+  isLike,
+
+  hint,
+  error,
+
+  ...inputProps
+}) => {
+  const fieldMsgId = `field-msg-${name}`
+  const baseInputProps = {
+    ...inputProps,
+    fieldMsgId,
+    isLike,
+    type: 'radio',
+  }
+
+  return (
+    <Field>
+      <Field.Content>
+        <CurrencyOption
+          {...baseInputProps}
+          inactive={isLike}
+          optionName={<Translate id="hkd" />}
+          optionType="hkd"
+          optionValue={PAYMENT_CURRENCY.HKD}
+        />
+        <CurrencyOption
+          {...baseInputProps}
+          inactive={!isLike}
+          optionName="LikeCoin"
+          optionType="like"
+          optionValue={PAYMENT_CURRENCY.LIKE}
+        />
+      </Field.Content>
+
+      <Field.Footer fieldMsgId={fieldMsgId} hint={hint} error={error} />
+
+      <style jsx>{styles}</style>
+    </Field>
+  )
+}
+
+export default CurrencyRadioInput
