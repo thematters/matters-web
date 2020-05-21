df1f88a5472c1d209af0c30518cbc56cb6b1c8e7
RoberTu <robertu0717@gmail.com>
2020-05-21 15:27:33 +0800

    revise view stripe account login link

--- src/views/Me/Wallet/ViewStripeAccount.tsx
+++ src/views/Me/Wallet/ViewStripeAccount.tsx
@@ -1,9 +1,7 @@
-import { useLazyQuery } from '@apollo/react-hooks'
 import gql from 'graphql-tag'
+import { useQuery } from '@apollo/react-hooks'
 
 import { Form, Icon, TextIcon, Translate } from '~/components'
-
-import { ADD_TOAST } from '~/common/enums'
 
 import { GetStripeLoginUrl } from './__generated__/GetStripeLoginUrl'
 
@@ -21,33 +19,18 @@ const GET_STRIPE_LOGIN_URL = gql`
 `
 
 const Buttons = () => {
-  const [getStripeLoginUrl, { loading }] = useLazyQuery<GetStripeLoginUrl>(
-    GET_STRIPE_LOGIN_URL
-  )
-
-  const getLoginUrl = async () => {
-    try {
-      await getStripeLoginUrl()
-
-      // TODO
-    } catch (e) {
-      window.dispatchEvent(
-        new CustomEvent(ADD_TOAST, {
-          detail: {
-            color: 'red',
-            content: <Translate id="ACTION_FAILED" />,
-          },
-        })
-      )
-    }
-  }
+  const { data, loading } = useQuery<GetStripeLoginUrl>(GET_STRIPE_LOGIN_URL, {
+    skip: !process.browser,
+  })
+  const loginUrl = data?.viewer?.wallet.stripeAccount?.loginUrl
 
   return (
     <Form.List.Item
       title={
         <Translate zh_hant="管理 Stripe 賬戶" zh_hans="管理 Stripe 账户" />
       }
-      onClick={getLoginUrl}
+      htmlHref={loginUrl}
+      htmlTarget="_blank"
       right={
         loading ? (
           <Icon.Spinner color="grey" size="sm" />
