f31b82de7c656ad572a60458fc17be47ff55bfbd
RoberTu <robertu0717@gmail.com>
2020-05-26 14:58:24 +0800

    prefetch loginUrl

--- src/views/Me/Wallet/ViewStripeAccount.tsx
+++ src/views/Me/Wallet/ViewStripeAccount.tsx
@@ -1,6 +1,5 @@
-import { useApolloClient } from '@apollo/react-hooks'
+import { useQuery } from '@apollo/react-hooks'
 import gql from 'graphql-tag'
-import { useState } from 'react'
 
 import { Form, Icon, TextIcon, Translate } from '~/components'
 
@@ -21,38 +20,16 @@ const GET_STRIPE_LOGIN_URL = gql`
 `
 
 const Buttons = () => {
-  const [loading, setLoading] = useState(false)
-
-  /**
-   * Use `useApolloClient` instead of `useLazyQuery`
-   * since `useLazyQuery` return Promsise<void>, and
-   * `window.open` is only triggered by user action.
-   *
-   */
-  const client = useApolloClient()
-  const openStripeDashboard = async () => {
-    setLoading(true)
-
-    try {
-      const { data } = await client.query<GetStripeLoginUrl>({
-        query: GET_STRIPE_LOGIN_URL,
-      })
-      const loginUrl = data?.viewer?.wallet.stripeAccount?.loginUrl
-
-      window.open(loginUrl, '_blank')
-    } catch (e) {
-      console.error(e)
-    }
-
-    setLoading(false)
-  }
+  const { data, loading } = useQuery<GetStripeLoginUrl>(GET_STRIPE_LOGIN_URL)
+  const loginUrl = data?.viewer?.wallet.stripeAccount?.loginUrl
 
   return (
     <Form.List.Item
       title={
         <Translate zh_hant="管理 Stripe 賬戶" zh_hans="管理 Stripe 账户" />
       }
-      onClick={openStripeDashboard}
+      htmlHref={loginUrl}
+      htmlTarget="_blank"
       right={
         loading ? (
           <Icon.Spinner color="grey" size="sm" />
