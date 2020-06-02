0e2256297062494f494d0f71fc926cf466f70794
Zeck Li <devformatters@gmail.com>
2020-06-02 20:25:28 +0800

    Formatting.

--- src/common/utils/agentHash.ts
+++ src/common/utils/agentHash.ts
@@ -11,9 +11,10 @@ export const initAgentHash = () => {
     const stored = window.localStorage.getItem(STORE_KEY_AGENT_HASH)
 
     if (!stored || !stored.startsWith(AGENT_HASH_PREFIX)) {
-      fingerprint.get(components => {
-        const values = components.map(component => component.value)
-        const hash = AGENT_HASH_PREFIX + fingerprint.x64hash128(values.join(''), 31)
+      fingerprint.get((components) => {
+        const values = components.map((component) => component.value)
+        const hash =
+          AGENT_HASH_PREFIX + fingerprint.x64hash128(values.join(''), 31)
         window.localStorage.setItem(STORE_KEY_AGENT_HASH, hash)
       })
     }
