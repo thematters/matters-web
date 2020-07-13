ffcf7bb1500badc15cd5271b27025d86641dca47
RoberTu <robertu0717@gmail.com>
2020-07-13 18:18:04 +0800

    feat(public-private-queries): `<UserFollowers>` & `<UserFollowees>`

---
+++ src/views/User/Followees/gql.ts
@@ -0,0 +1,40 @@
+import gql from 'graphql-tag'
+
+import { UserDigest } from '~/components/UserDigest'
+
+export const USER_FOLLOWEES_PUBLIC = gql`
+  query UserFolloweePublic($userName: String!, $after: String) {
+    user(input: { userName: $userName }) {
+      id
+      displayName
+      followees(input: { first: 20, after: $after }) {
+        pageInfo {
+          startCursor
+          endCursor
+          hasNextPage
+        }
+        edges {
+          cursor
+          node {
+            ...UserDigestRichUserPublic
+            ...UserDigestRichUserPrivate
+          }
+        }
+      }
+    }
+  }
+  ${UserDigest.Rich.fragments.user.public}
+  ${UserDigest.Rich.fragments.user.private}
+`
+
+export const USER_FOLLOWEES_PRIVATE = gql`
+  query UserFolloweePrivate($ids: [ID!]!) {
+    nodes(input: { ids: $ids }) {
+      id
+      ... on User {
+        ...UserDigestRichUserPrivate
+      }
+    }
+  }
+  ${UserDigest.Rich.fragments.user.private}
+`
