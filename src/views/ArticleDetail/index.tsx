f12268038010361886a903ca1e6b8662dac03307
Zeck Li <devformatters@gmail.com>
2020-11-12 02:44:36 +0800

    fix(revision): fix descriptions and bug for revision feature

--- src/views/ArticleDetail/index.tsx
+++ src/views/ArticleDetail/index.tsx
@@ -300,7 +300,21 @@ const ArticleDetail = () => {
 
             <section className="info">
               <section className="left">
-                <DateTime date={article.createdAt} color="grey" />
+                <section className="time">
+                  <span>
+                    <Translate zh_hant="發布於" zh_hans="發布於" />
+                  </span>
+                  <DateTime date={article.createdAt} color="grey" />
+                </section>
+
+                {article.revisedAt && (
+                  <section className="time">
+                    <span>
+                      <Translate zh_hant="修訂於" zh_hans="修訂於" />
+                    </span>
+                    <DateTime date={article.revisedAt} color="grey" />
+                  </section>
+                )}
 
                 <FingerprintButton article={article} />
 
