b7821ec15d89d518036a391ab882beb141ee3e20
Zeck Li <devformatters@gmail.com>
2021-03-24 18:01:44 +0800

    feat(notice): revise notice content of circle invitation

--- src/components/Notice/CircleNotice/CircleInvitationNotice.tsx
+++ src/components/Notice/CircleNotice/CircleInvitationNotice.tsx
@@ -36,13 +36,13 @@ const CircleInvitationNotice = ({
           <Translate
             zh_hant=" 邀請你免費加入圍爐 "
             zh_hans=" 邀请你免费加入围炉 "
-            en="invites you to join circle for"
+            en=" invites you to join circle for "
           />
           {circle?.invitedBy?.freePeriod}
           <Translate
             zh_hant=" 個月，前去免費體驗，與大家談天說地吧。"
             zh_hans=" 个月，前去免费体验，与大家谈天说地吧。"
-            en="months. Let's try it for free."
+            en=" months for free. Let's try it and have fun."
           />
         </NoticeHead>
 
