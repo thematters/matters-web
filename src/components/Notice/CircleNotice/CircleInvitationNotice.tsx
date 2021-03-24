7478f896a6601049fcbd7fd15ea578c9602e524d
Zeck Li <devformatters@gmail.com>
2021-03-24 17:54:10 +0800

    feat(notice): add notice component for circle invitation

---
+++ src/components/Notice/CircleNotice/CircleInvitationNotice.tsx
@@ -0,0 +1,80 @@
+import gql from 'graphql-tag'
+
+import { Translate } from '~/components'
+
+import NoticeActorName from '../NoticeActorName'
+import NoticeCircleCard from '../NoticeCircleCard'
+import NoticeDate from '../NoticeDate'
+import NoticeHead from '../NoticeHead'
+import NoticeTypeIcon from '../NoticeTypeIcon'
+import styles from '../styles.css'
+
+import { CircleInvitationNotice as CircleInvitationNoticeType } from './__generated__/CircleInvitationNotice'
+
+const CircleInvitationNotice = ({
+  notice,
+}: {
+  notice: CircleInvitationNoticeType
+}) => {
+  if (!notice.actors || !notice.circle) {
+    return null
+  }
+
+  const circle = notice.circle
+  const actor = notice.actors[0]
+
+  return (
+    <section className="container">
+      <section className="avatar-wrap">
+        <NoticeTypeIcon type="circle" />
+      </section>
+
+      <section className="content-wrap">
+        <NoticeHead>
+          <Translate zh_hant="恭喜你！" zh_hans="恭喜你！" en="Congrats!" />
+          <NoticeActorName user={actor} />
+          <Translate
+            zh_hant=" 邀請你免費加入圍爐 "
+            zh_hans=" 邀请你免费加入围炉 "
+            en="invites you to join circle for"
+          />
+          {circle?.invitedBy?.freePeriod}
+          <Translate
+            zh_hant=" 個月，前去免費體驗，與大家談天說地吧。"
+            zh_hans=" 个月，前去免费体验，与大家谈天说地吧。"
+            en="months. Let's try it for free."
+          />
+        </NoticeHead>
+
+        <NoticeCircleCard circle={circle} />
+        <NoticeDate notice={notice} />
+      </section>
+      <style jsx>{styles}</style>
+    </section>
+  )
+}
+
+CircleInvitationNotice.fragments = {
+  notice: gql`
+    fragment CircleInvitationNotice on CircleNotice {
+      id
+      ...NoticeDate
+      actors {
+        ...NoticeActorNameUser
+      }
+      circle: target {
+        id
+        invitedBy {
+          id
+          freePeriod
+        }
+        ...NoticeCircleCard
+      }
+    }
+    ${NoticeActorName.fragments.user}
+    ${NoticeCircleCard.fragments.circle}
+    ${NoticeDate.fragments.notice}
+  `,
+}
+
+export default CircleInvitationNotice
---
+++ src/components/Notice/NoticeCircleCard.tsx
@@ -0,0 +1,43 @@
+import gql from 'graphql-tag'
+
+import { CircleDigest } from '~/components'
+
+import styles from './styles.css'
+
+import { NoticeCircleCard as NoticeCircleCardType } from './__generated__/NoticeCircleCard'
+
+const NoticeCircleCard = ({
+  circle,
+}: {
+  circle: NoticeCircleCardType | null
+}) => {
+  if (!circle) {
+    return null
+  }
+
+  return (
+    <section className="sub-content">
+      <CircleDigest.Rich
+        bgColor="grey-lighter"
+        borderRadius="xtight"
+        circle={circle}
+        hasFooter
+        hasOwner
+        hasPrice
+      />
+      <style jsx>{styles}</style>
+    </section>
+  )
+}
+
+NoticeCircleCard.fragments = {
+  circle: gql`
+    fragment NoticeCircleCard on Circle {
+      id
+      ...DigestRichCirclePublic
+    }
+    ${CircleDigest.Rich.fragments.circle.public}
+  `,
+}
+
+export default NoticeCircleCard
