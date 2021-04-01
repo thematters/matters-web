7db09842b3f3a98217b9b854b12780a8970e19f9
robertu <4065233+robertu7@users.noreply.github.com>
2021-04-01 16:31:49 +0800

    feat(misc): revise misc pages

--- src/views/Guide/content.ts
+++ src/views/Guide/content.ts
@@ -8,7 +8,9 @@ export default {
 
 <p><a className="u-link-green" href="https://matters.news/@hi176/matters-%E4%BD%9C%E8%80%85%E5%B7%A5%E5%85%B7%E7%AE%B1-%E6%96%B0%E6%89%8B%E5%BF%85%E7%9C%8B-2020-11-24%E6%9B%B4%E6%96%B0-bafyreidh5ynfz4bn2szfeaapzwi4z4vfoybzdzdtaed7n6u6f7gljopima">Matters 作者工具箱 | 新手必看</a></p>
 
-<p><a className="u-link-green" href="https://matters.news/@hi176/matters-%E6%94%AF%E4%BB%98%E8%88%87%E6%8F%90%E7%8F%BE%E5%8A%9F%E8%83%BD%E6%8C%87%E5%8D%97-2020-08-14%E6%9B%B4%E6%96%B0-bafyreicfgnpk4jamnhkjecq32kdlmhhtp6xrwpkxoalp37wayd4sqmjo54">Matters 支付與提現指南</a></p>
+<p><a className="u-link-green" href="https://matters.news/@hi176/matters-%E6%94%AF%E4%BB%98%E8%88%87%E6%8F%90%E7%8F%BE%E5%8A%9F%E8%83%BD%E6%8C%87%E5%8D%97-2020-08-14%E6%9B%B4%E6%96%B0-bafyreicfgnpk4jamnhkjecq32kdlmhhtp6xrwpkxoalp37wayd4sqmjo54">Matters 支付指南</a></p>
+
+<p><a className="u-link-green" href="https://matters.news/@hi176/matters-%E6%8F%90%E7%8F%BE%E6%8C%87%E5%8D%97-%E8%B7%9F%E8%91%97%E6%AD%A5%E9%A9%9F%E8%B5%B0-%E6%8A%8A%E9%8C%A2%E9%A0%98%E5%88%B0%E4%BD%A0%E7%9A%84%E6%88%B6%E9%A0%AD%E8%A3%A1-bafyreihdpedvodp3pk66blu545oti2yu7tufvrlxrwlevfuk7vei2hgpli">Matters 提現指南</a></p>
 
 <p><a className="u-link-green" href="https://matters.news/@hi176/matters-%E6%A8%99%E7%B1%A4%E5%8A%9F%E8%83%BD%E5%A4%A7%E9%80%B2%E5%8C%96-%E9%96%B1%E8%AE%80%E5%88%86%E9%A1%9E-%E5%80%8B%E4%BA%BA%E5%B0%88%E6%AC%84-%E5%AE%83%E9%83%BD%E8%83%BD%E6%BB%BF%E8%B6%B3%E4%BD%A0-bafyreifdlhuy33h6235jt5vk2k7fyxendykkhgixfvnzr6izu4677xtwui">Matters 標籤功能指南</a></p>
 
@@ -24,7 +26,9 @@ export default {
 
 <p><a className="u-link-green" href="https://matters.news/@hi176/matters-%E4%BD%9C%E8%80%85%E5%B7%A5%E5%85%B7%E7%AE%B1-%E6%96%B0%E6%89%8B%E5%BF%85%E7%9C%8B-2020-11-24%E6%9B%B4%E6%96%B0-bafyreidh5ynfz4bn2szfeaapzwi4z4vfoybzdzdtaed7n6u6f7gljopima">Matters 作者工具箱 | 新手必看</a></p>
 
-<p><a className="u-link-green" href="https://matters.news/@hi176/matters-%E6%94%AF%E4%BB%98%E8%88%87%E6%8F%90%E7%8F%BE%E5%8A%9F%E8%83%BD%E6%8C%87%E5%8D%97-2020-08-14%E6%9B%B4%E6%96%B0-bafyreicfgnpk4jamnhkjecq32kdlmhhtp6xrwpkxoalp37wayd4sqmjo54">Matters 支付与提现指南</a></p>
+<p><a className="u-link-green" href="https://matters.news/@hi176/matters-%E6%94%AF%E4%BB%98%E8%88%87%E6%8F%90%E7%8F%BE%E5%8A%9F%E8%83%BD%E6%8C%87%E5%8D%97-2020-08-14%E6%9B%B4%E6%96%B0-bafyreicfgnpk4jamnhkjecq32kdlmhhtp6xrwpkxoalp37wayd4sqmjo54">Matters 支付指南</a></p>
+
+<p><a className="u-link-green" href="https://matters.news/@hi176/matters-%E6%8F%90%E7%8F%BE%E6%8C%87%E5%8D%97-%E8%B7%9F%E8%91%97%E6%AD%A5%E9%A9%9F%E8%B5%B0-%E6%8A%8A%E9%8C%A2%E9%A0%98%E5%88%B0%E4%BD%A0%E7%9A%84%E6%88%B6%E9%A0%AD%E8%A3%A1-bafyreihdpedvodp3pk66blu545oti2yu7tufvrlxrwlevfuk7vei2hgpli">Matters 提现指南</a></p>
 
 <p><a className="u-link-green" href="https://matters.news/@hi176/matters-%E6%A8%99%E7%B1%A4%E5%8A%9F%E8%83%BD%E5%A4%A7%E9%80%B2%E5%8C%96-%E9%96%B1%E8%AE%80%E5%88%86%E9%A1%9E-%E5%80%8B%E4%BA%BA%E5%B0%88%E6%AC%84-%E5%AE%83%E9%83%BD%E8%83%BD%E6%BB%BF%E8%B6%B3%E4%BD%A0-bafyreifdlhuy33h6235jt5vk2k7fyxendykkhgixfvnzr6izu4677xtwui">Matters 标签功能指南</a></p>
 
@@ -39,9 +43,11 @@ export default {
 
 <p><a className="u-link-green" href="https://matters.news/@hi176/matters-%E4%BD%9C%E8%80%85%E5%B7%A5%E5 %85%B7%E7%AE%B1-%E6%96%B0%E6%89%8B%E5%BF%85%E7%9C%8B-2020-11-24%E6%9B%B4%E6% 96%B0-bafyreidh5ynfz4bn2szfeaapzwi4z4vfoybzdzdtaed7n6u6f7gljopima">Matters Author Toolbox | A must-see for newbies</a></p>
 
-<p><a className="u-link-green" href="https://matters.news/@hi176/matters-%E6%94%AF%E4%BB%98%E8%88%87%E6 %8F%90%E7%8F%BE%E5%8A%9F%E8%83%BD%E6%8C%87%E5%8D%97-2020-08-14%E6%9B%B4%E6%96 %B0-bafyreicfgnpk4jamnhkjecq32kdlmhhtp6xrwpkxoalp37wayd4sqmjo54">Matters payment and withdrawal guide</a></p>
+<p><a className="u-link-green" href="https://matters.news/@hi176/matters-%E6%94%AF%E4%BB%98%E8%88%87%E6 %8F%90%E7%8F%BE%E5%8A%9F%E8%83%BD%E6%8C%87%E5%8D%97-2020-08-14%E6%9B%B4%E6%96 %B0-bafyreicfgnpk4jamnhkjecq32kdlmhhtp6xrwpkxoalp37wayd4sqmjo54">Matters Payment Guide</a></p>
 
-<p><a className="u-link-green" href="https://matters.news/@hi176/matters-%E6%A8%99%E7%B1%A4%E5%8A%9F%E8 %83%BD%E5%A4%A7%E9%80%B2%E5%8C%96-%E9%96%B1%E8%AE%80%E5%88%86%E9%A1%9E-%E5 %80%8B%E4%BA%BA%E5%B0%88%E6%AC%84-%E5%AE%83%E9%83%BD%E8%83%BD%E6%BB%BF%E8% B6%B3%E4%BD%A0-bafyreifdlhuy33h6235jt5vk2k7fyxendykkhgixfvnzr6izu4677xtwui">Matters tag usage guide</a></p>
+<p><a className="u-link-green" href="https://matters.news/@hi176/matters-%E6%8F%90%E7%8F%BE%E6%8C%87%E5%8D%97-%E8%B7%9F%E8%91%97%E6%AD%A5%E9%A9%9F%E8%B5%B0-%E6%8A%8A%E9%8C%A2%E9%A0%98%E5%88%B0%E4%BD%A0%E7%9A%84%E6%88%B6%E9%A0%AD%E8%A3%A1-bafyreihdpedvodp3pk66blu545oti2yu7tufvrlxrwlevfuk7vei2hgpli">Matters Withdraw Guide</a></p>
+
+<p><a className="u-link-green" href="https://matters.news/@hi176/matters-%E6%A8%99%E7%B1%A4%E5%8A%9F%E8 %83%BD%E5%A4%A7%E9%80%B2%E5%8C%96-%E9%96%B1%E8%AE%80%E5%88%86%E9%A1%9E-%E5 %80%8B%E4%BA%BA%E5%B0%88%E6%AC%84-%E5%AE%83%E9%83%BD%E8%83%BD%E6%BB%BF%E8% B6%B3%E4%BD%A0-bafyreifdlhuy33h6235jt5vk2k7fyxendykkhgixfvnzr6izu4677xtwui">Matters Tag Usage Guide</a></p>
 
 
 <p>If you have any other questions or suggestions, please write to <a className="u-link-green" href="mailto:ask@matters.news" target="_blank">ask@matters.news</a>, we will reply as soon as possible! </p>
