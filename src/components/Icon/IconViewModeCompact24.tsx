18b55cdc532a0598c9e065e3e912479acd590551
robertu <4065233+robertu7@users.noreply.github.com>
2021-04-15 18:05:56 +0800

    feat(kill-features): kill view mode

--- src/components/Icon/IconViewModeCompact24.tsx
+++
@@ -1,5 +0,0 @@
-import { ReactComponent as Icon } from '@/public/static/icons/24px/view-mode-compact.svg'
-
-import { withIcon } from './withIcon'
-
-export const IconViewModeCompact24 = withIcon(Icon)
