ea787ac69f5e88b5d09c0321d1c3b4cf360d551f
Zeck Li <devformatters@gmail.com>
2021-07-06 17:48:29 +0800

    fix(dialog): fix redundant new user task event disptached

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,7 +1,7 @@
 import { useQuery } from '@apollo/react-hooks'
 import classNames from 'classnames'
 
-import { Head, OnboardingTasks, SearchBar, useRoute } from '~/components'
+import { Head, OnboardingTasks, SearchBar, useResponsive, useRoute } from '~/components'
 import CLIENT_PREFERENCE from '~/components/GQL/queries/clientPreference'
 
 import FixedMain from './FixedMain'
@@ -59,6 +59,7 @@ const Main: React.FC<MainProps> = ({ aside, bgColor, inEditor, children }) => {
   const isInSearch = isInPath('SEARCH')
   const isInArticleDetail = isInPath('ARTICLE_DETAIL')
   const isInCircle = isPathStartWith('/~', true)
+  const isLargeUp = useResponsive('lg-up')
 
   const { data } = useQuery<ClientPreference>(CLIENT_PREFERENCE, {
     variables: { id: 'local' },
@@ -83,7 +84,7 @@ const Main: React.FC<MainProps> = ({ aside, bgColor, inEditor, children }) => {
       <article className={articleClasses}>
         {children}
 
-        {showOnboardingTasks && (
+        {showOnboardingTasks && !isLargeUp && (
           <section className="u-lg-up-hide">
             <OnboardingTasks.NavBar />
           </section>
@@ -97,7 +98,7 @@ const Main: React.FC<MainProps> = ({ aside, bgColor, inEditor, children }) => {
           </section>
         )}
 
-        {showOnboardingTasks && (
+        {showOnboardingTasks && isLargeUp && (
           <section className="u-lg-down-hide">
             <OnboardingTasks.Widget />
           </section>
