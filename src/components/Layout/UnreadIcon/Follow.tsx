96d9dc9eae4c98f35737b6f724c022d735b9d8f5
robertu <4065233+robertu7@users.noreply.github.com>
2021-07-21 10:49:35 +0000

    feat(following): increase the polling duration of `unreadFollowing`

--- src/components/Layout/UnreadIcon/Follow.tsx
+++ src/components/Layout/UnreadIcon/Follow.tsx
@@ -7,9 +7,7 @@ import {
   IconNavFollowingActive24,
   ViewerContext,
 } from '~/components'
-import UNREAD_FOLLOWEE_ARTICLES from '~/components/GQL/queries/unreadFollowing'
-
-import { POLL_INTERVAL } from '~/common/enums'
+import UNREAD_FOLLOWING from '~/components/GQL/queries/unreadFollowing'
 
 import styles from './styles.css'
 
@@ -21,19 +19,16 @@ interface FollowUnreadIconProps {
 
 const FollowUnreadIcon: React.FC<FollowUnreadIconProps> = ({ active }) => {
   const viewer = useContext(ViewerContext)
-  const { data, startPolling } = useQuery<UnreadFollowing>(
-    UNREAD_FOLLOWEE_ARTICLES,
-    {
-      errorPolicy: 'none',
-      fetchPolicy: 'network-only',
-      skip: !viewer.isAuthed || !process.browser,
-    }
-  )
+  const { data, startPolling } = useQuery<UnreadFollowing>(UNREAD_FOLLOWING, {
+    errorPolicy: 'none',
+    fetchPolicy: 'network-only',
+    skip: !viewer.isAuthed || !process.browser,
+  })
 
   // FIXME: https://github.com/apollographql/apollo-client/issues/3775
   useEffect(() => {
     if (viewer.isAuthed) {
-      startPolling(POLL_INTERVAL)
+      startPolling(1000 * 60) // 60s
     }
   }, [])
 
