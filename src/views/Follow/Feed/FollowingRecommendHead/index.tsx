eda46ce57d24ebd1a66e6ff4f0bb67f13f8adfa2
Zeck Li <devformatters@gmail.com>
2021-08-11 13:46:44 +0800

    feat(follow): update recommendation description in following feed

--- src/views/Follow/Feed/FollowingRecommendHead/index.tsx
+++ src/views/Follow/Feed/FollowingRecommendHead/index.tsx
@@ -8,22 +8,22 @@ const RecommendHead = ({ type }: Props) => {
   const titleMap = {
     article: (
       <Translate
-        zh_hant="你追蹤的人也支持了"
-        zh_hans="你追踪的人也支持了"
+        zh_hant="你追蹤的人支持了"
+        zh_hans="你追踪的人支持了"
         en="Followees also support"
       />
     ),
     circle: (
       <Translate
-        zh_hant="你追蹤的人也加入了"
-        zh_hans="你追踪的人也加入了"
+        zh_hant="你追蹤的人加入了"
+        zh_hans="你追踪的人加入了"
         en="Followees also subscribe"
       />
     ),
     user: (
       <Translate
-        zh_hant="你追蹤的人也關注了"
-        zh_hans="你追踪的人也关注了"
+        zh_hant="你追蹤的人關注了"
+        zh_hans="你追踪的人关注了"
         en="Followees also follow"
       />
     ),
