84cde02be89b6758c341900fbd915b9e02962e40
robertu <4065233+robertu7@users.noreply.github.com>
2022-01-21 16:46:33 +0800

    feat(wallet): use <Form.Field.Footer> as wallet hinter

--- src/components/Forms/WalletAuthForm/Connect.tsx
+++ src/components/Forms/WalletAuthForm/Connect.tsx
@@ -197,18 +197,23 @@ const Connect: React.FC<FormProps> = ({
             en="Connect Wallet"
           />
         }
-        hint={
-          <Translate
-            zh_hans="若要变更地址，请直接操作钱包切換"
-            zh_hant="若要變更地址，請直接操作錢包切換"
-            en="To change, switch it directly on your wallet"
-          />
-        }
       >
         <Form.List.Item title={maskAddress(values.address)} />
 
         {errors.address && (
           <Form.Field.Footer fieldMsgId={fieldMsgId} error={errors.address} />
+        )}
+        {!errors.address && (
+          <Form.Field.Footer
+            fieldMsgId={fieldMsgId}
+            hint={
+              <Translate
+                zh_hans="若要变更地址，请直接操作钱包切換"
+                zh_hant="若要變更地址，請直接操作錢包切換"
+                en="To change, switch it directly on your wallet"
+              />
+            }
+          />
         )}
       </Form.List>
 
