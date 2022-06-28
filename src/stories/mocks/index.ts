cd1de273591a2c29aeae906955e4e4b4f8765f12
49659410+tx0c <>
2022-06-28 20:14:31 +0000

    use interval timer polling iscn publishing status
    since iscn publishing took longer than expected (10-20s), for a total up to 30s;

--- src/stories/mocks/index.ts
+++ src/stories/mocks/index.ts
@@ -92,6 +92,7 @@ export const MOCK_ARTILCE = {
     '其實已經開始兩週了XD,不過最近才想說應該來記錄一下我在火箭隊的日常,順便練一下文筆,也可以讓大家了解一下火箭隊軟體培訓營裡面大概是在做些什麼事情,上課的模式及氣氛是怎樣等等...畢竟我當時得知有這個免費培訓營時,也是網路上各種爬文類似這種免費培訓工程師半年的心得及成',
   author: MOCK_USER,
   createdAt: '2020-12-24T07:29:17.682Z',
+  revisedAt: null,
   appreciationsReceived: {
     __typename: 'AppreciationConnection' as any,
     totalCount: 0,
