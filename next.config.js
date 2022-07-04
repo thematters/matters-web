008bd8513d482c01a7ce90ab96cdb8de0f2e6336
Elk Leung <elkleung@gmail.com>
2022-07-04 13:25:36 +0800

    Revert "改用 VisuallyHidden"
    This reverts commit 323144baa45939748af0268db82bbe16ffc90c9c.

--- next.config.js
+++ next.config.js
@@ -28,9 +28,6 @@ const nextConfig = {
     ignoreDevErrors: false,
   },
   crossOrigin: 'anonymous',
-  experimental: {
-    esmExternals: false,
-  },
 
   webpack(config, { defaultLoaders, isServer }) {
     /**
