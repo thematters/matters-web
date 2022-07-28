7da166e7ce8b1e984d952a84ecab086660b32cda
Jazzy <mingchenliang954@gmail.com>
2022-07-29 00:52:57 +0800

    feat(downvote):leave related query and fragment complete

--- src/components/Comment/FooterActions/index.tsx
+++ src/components/Comment/FooterActions/index.tsx
@@ -143,9 +143,9 @@ const BaseFooterActions = ({
   return (
     <footer
       aria-label={translate({
-        zh_hant: `${comment.upvotes} 點讚、${comment.downvotes} 點踩`,
-        zh_hans: `${comment.upvotes} 点赞、${comment.downvotes} 点踩`,
-        en: `${comment.upvotes} upvotes, ${comment.downvotes} downvotes`,
+        zh_hant: `${comment.upvotes} 點讚`,
+        zh_hans: `${comment.upvotes} 点赞`,
+        en: `${comment.upvotes} upvotes`,
         lang,
       })}
     >
