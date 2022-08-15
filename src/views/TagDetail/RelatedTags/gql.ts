03f3a175b3d2552865820a6041c3db0c208adb8f
Aka jazzy <mingchenliang954@gmail.com>
2022-08-15 23:03:32 +0800

    feat(related-tags): add shuffle button and function into related tags

--- src/views/TagDetail/RelatedTags/gql.ts
+++ src/views/TagDetail/RelatedTags/gql.ts
@@ -3,11 +3,11 @@ import gql from 'graphql-tag'
 import { TagDigest } from '~/components'
 
 export const RELATED_TAGS = gql`
-  query TagDetailRecommended($id: ID!) {
+  query TagDetailRecommended($id: ID!, $random: random_Int_min_0_max_49) {
     node(input: { id: $id }) {
       ... on Tag {
         id
-        recommended(input: { first: 10 }) {
+        recommended(input: { first: 10, filter: { random: $random } }) {
           edges {
             cursor
             node {
