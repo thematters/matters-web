386e7229ac7488583f9cd9d09f5c457d3ffdb52f
Aka jazzy <mingchenliang954@gmail.com>
2022-08-22 20:31:49 +0800

    feat(test): modify test.yml to enable test reporter

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 test('homepage has Playwright in title and get started link linking to the intro page', async ({ page }) => {
   await page.goto('https://playwright.dev/');
