15eafc012b3cea031005a2915876452daad32a2c
bluecloud <96812901+pitb2022@users.noreply.github.com>
2022-08-31 14:31:25 +0800

    update

--- src/views/Settings/Enhance/index.tsx
+++ src/views/Settings/Enhance/index.tsx
@@ -2,11 +2,11 @@ import { Form, Translate } from '~/components'
 
 const Enhance = () => {
   return (
-    <Form.List groupName={<Translate
-      zh_hant="參與開發"
-      zh_hans="参与开发"
-      en="Build Together"
-    />}>
+    <Form.List
+      groupName={
+        <Translate zh_hant="參與開發" zh_hans="参与开发" en="Build Together" />
+      }
+    >
       <Form.List.Item
         title={<Translate id="openCommunity" />}
         href="https://github.com/thematters/developer-resource"
