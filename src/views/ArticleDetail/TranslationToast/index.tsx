eaac349d044c4c9dceac5b9af79536e37518259e
bluecloud <96812901+pitb2022@users.noreply.github.com>
2022-09-06 18:40:38 +0800

    feat: update translation toast

---
+++ src/views/ArticleDetail/TranslationToast/index.tsx
@@ -0,0 +1,29 @@
+import { Translate } from '@/src/components'
+
+import styles from './styles.css'
+
+const TranslationToast = ({
+  onClick,
+}: {
+  onClick: (event?: React.MouseEvent<HTMLElement, MouseEvent>) => any
+}) => {
+  return (
+    <p className="wrapper">
+      <Translate
+        zh_hant="你正在浏览由 Google 翻译的版本，若要阅读原文版本，"
+        zh_hans="你正在瀏覽由 Google 翻譯的版本，若要閱讀原文版本，"
+        en="You're viewing translated content by Google. To read original content,&nbsp;"
+      />
+      <span onClick={onClick} className="switchButton">
+        <Translate
+          zh_hant="点击这里切换。"
+          zh_hans="點擊這裡切換。"
+          en="click here to switch."
+        />
+      </span>
+      <style jsx>{styles}</style>
+    </p>
+  )
+}
+
+export default TranslationToast
