8f2c6ef94429cf93cbfa7f0fde39cf1f2b6d16ba
bluecloud <96812901+pitb2022@users.noreply.github.com>
2022-09-13 20:58:01 +0800

    feat: revise translation toast

--- src/views/ArticleDetail/TranslationToast/index.tsx
+++ src/views/ArticleDetail/TranslationToast/index.tsx
@@ -1,29 +1,60 @@
+import { CONTENT_LANG_TEXT_MAP } from '@/src/common/enums'
+import { toLocale } from '@/src/common/utils'
 import { Translate } from '@/src/components'
 
 import styles from './styles.css'
 
-const TranslationToast = ({
+const Content = ({ language }: { language: string | null }) => {
+  if (language && toLocale(language) !== '') {
+    const targetLang = {
+      zh_hant: '',
+      zh_hans: '',
+      en: '',
+    }
+    Object.entries(CONTENT_LANG_TEXT_MAP.zh_hant).forEach(([k, v]) => {
+      if (k === language) targetLang.zh_hant = v
+    })
+    Object.entries(CONTENT_LANG_TEXT_MAP.zh_hans).forEach(([k, v]) => {
+      if (k === language) targetLang.zh_hans = v
+    })
+    Object.entries(CONTENT_LANG_TEXT_MAP.en).forEach(([k, v]) => {
+      if (k === language) targetLang.en = v
+    })
+    return (
+      <Translate
+        zh_hans={`你正在浏览由 Google 翻译的${targetLang.zh_hans}版本，若要阅读原文版本，`}
+        zh_hant={`你正在瀏覽由 Google 翻譯的${targetLang.zh_hant}版本，若要閱讀原文版本，`}
+        en={`You’re viewing content translated into ${targetLang.en} by Google. To read original content, `}
+      />
+    )
+  }
+  return (
+    <Translate
+      zh_hans="你正在浏览由 Google 翻译的版本，若要阅读原文版本，"
+      zh_hant="你正在瀏覽由 Google 翻譯的版本，若要閱讀原文版本，"
+      en="You’re viewing content translated by Google. To read original content,&nbsp;"
+    />
+  )
+}
+
+const SwitchContent = ({
   onClick,
 }: {
   onClick: (event?: React.MouseEvent<HTMLElement, MouseEvent>) => any
-}) => {
-  return (
-    <p className="wrapper">
-      <Translate
-        zh_hans="你正在浏览由 Google 翻译的版本，若要阅读原文版本，"
-        zh_hant="你正在瀏覽由 Google 翻譯的版本，若要閱讀原文版本，"
-        en="You're viewing translated content by Google. To read original content,&nbsp;"
-      />
-      <span onClick={onClick} className="switchButton">
-        <Translate
-          zh_hans="点击这里切换。"
-          zh_hant="點擊這裡切換。"
-          en="click here to switch."
-        />
-      </span>
-      <style jsx>{styles}</style>
-    </p>
-  )
+}) => (
+  <span onClick={onClick} className="switchButton">
+    <Translate
+      zh_hans="点击这里切换。"
+      zh_hant="點擊這裡切換。"
+      en="click here to switch."
+    />
+    <style jsx>{styles}</style>
+  </span>
+)
+
+const TranslationToast = {
+  Content,
+  SwitchContent,
 }
 
 export default TranslationToast
