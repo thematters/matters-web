35ce7266493d36d9e83ac7e6b349ef220e39b711
bluecloud <96812901+pitb2022@users.noreply.github.com>
2022-09-30 18:54:38 +0800

    feat: revise Wallet

--- src/views/Me/Wallet/index.tsx
+++ src/views/Me/Wallet/index.tsx
@@ -3,11 +3,9 @@ import { useContext } from 'react'
 
 import {
   Form,
-  getErrorCodes,
   Head,
   Layout,
   PullToRefresh,
-  Spacer,
   Spinner,
   ViewerContext,
 } from '~/components'
@@ -18,6 +16,7 @@ import { PAYMENT_MINIMAL_PAYOUT_AMOUNT } from '~/common/enums'
 import { FiatCurrency, LikeCoin } from './Balance'
 import PaymentPassword from './PaymentPassword'
 import PaymentPointer from './PaymentPointer'
+import styles from './styles.css'
 import TotalAssets from './TotalAssets'
 import ViewStripeAccount from './ViewStripeAccount'
 import ViewStripeCustomerPortal from './ViewStripeCustomerPortal'
@@ -27,24 +26,15 @@ import { WalletBalance } from '~/components/GQL/queries/__generated__/WalletBala
 const Wallet = () => {
   const viewer = useContext(ViewerContext)
 
-  const { data, loading, refetch, error } = useQuery<WalletBalance>(
-    WALLET_BALANCE,
-    {
-      fetchPolicy: 'network-only',
-      errorPolicy: 'none',
-      skip: typeof window === 'undefined',
-    }
-  )
+  const { data, loading, refetch } = useQuery<WalletBalance>(WALLET_BALANCE, {
+    fetchPolicy: 'network-only',
+    errorPolicy: 'none',
+    skip: typeof window === 'undefined',
+  })
   const balanceHKD = data?.viewer?.wallet.balance.HKD || 0
   const canPayout = balanceHKD >= PAYMENT_MINIMAL_PAYOUT_AMOUNT.HKD
   const hasStripeAccount = !!data?.viewer?.wallet.stripeAccount?.id
   const hasPaymentPassword = viewer.status?.hasPaymentPassword
-
-  const likerId = viewer.liker.likerId
-  const errorCodes = getErrorCodes(error)
-  const shouldReAuth = errorCodes.some((code) => code === 'OAUTH_TOKEN_INVALID')
-  const liker = data?.viewer?.liker
-  const balanceLike = liker?.total || 0
 
   if (loading) {
     return (
@@ -68,21 +58,24 @@ const Wallet = () => {
       <Head title={{ id: 'myWallet' }} />
 
       <PullToRefresh refresh={refetch}>
-        <Spacer size="xxloose" />
-
-        <Form.List>
-          <TotalAssets />
+        <TotalAssets />
+        <section className="assetsContainer">
           <FiatCurrency
             balanceHKD={balanceHKD}
             canPayout={canPayout}
             hasStripeAccount={hasStripeAccount}
           />
-          {likerId && !shouldReAuth && <LikeCoin balanceLike={balanceLike} />}
+          <hr />
+          <LikeCoin />
+        </section>
+
+        <Form.List>
           {hasPaymentPassword && <PaymentPassword />}
           <ViewStripeCustomerPortal />
           {hasStripeAccount && <ViewStripeAccount />}
           <PaymentPointer />
         </Form.List>
+        <style jsx>{styles}</style>
       </PullToRefresh>
     </Layout.Main>
   )
