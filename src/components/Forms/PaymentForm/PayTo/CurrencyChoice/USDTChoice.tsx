5e81891f2e794b368672df52686efea432607788
robertu <4065233+robertu7@users.noreply.github.com>
2022-10-19 15:00:06 +0800

    fix(donation): disable choose USDT button first if author hasn't ETH address

--- src/components/Forms/PaymentForm/PayTo/CurrencyChoice/USDTChoice.tsx
+++ src/components/Forms/PaymentForm/PayTo/CurrencyChoice/USDTChoice.tsx
@@ -36,104 +36,108 @@ const USDTChoice: React.FC<FormProps> = ({
   const { data: balanceUSDTData } = useBalanceUSDT({})
   const balanceUSDT = parseFloat(balanceUSDTData?.formatted || '0')
 
-  return (
-    <>
-      {address && !!viewer.info.ethAddress && !!recipient.info.ethAddress && (
-        <section
-          role="button"
-          className="item clickable"
-          onClick={() => {
-            switchToSetAmount(CURRENCY.USDT)
-          }}
-        >
-          <TextIcon
-            icon={<IconUSDTActive40 size="xl-m" />}
-            size="md"
-            spacing="xtight"
-          >
-            Tether
-          </TextIcon>
-          <CurrencyFormatter
-            value={formatAmount(balanceUSDT)}
-            currency={CURRENCY.USDT}
-          />
-        </section>
-      )}
+  const curatorAddress = viewer.info.ethAddress
+  const creatorAddress = recipient.info.ethAddress
 
-      {address && !recipient.info.ethAddress && (
-        <section
-          role="button"
-          className="item"
-          onClick={() => {
-            switchToSetAmount(CURRENCY.USDT)
-          }}
+  if (!creatorAddress) {
+    return (
+      <section role="button" className="item">
+        <TextIcon
+          icon={<IconUSDT40 size="xl-m" />}
+          size="md"
+          spacing="xtight"
+          color="grey"
         >
-          <TextIcon
-            icon={<IconUSDT40 size="xl-m" />}
-            size="md"
-            spacing="xtight"
-            color="grey"
-          >
-            Tether
-          </TextIcon>
-          <TextIcon size="md" color="grey">
+          Tether
+        </TextIcon>
+        <TextIcon size="md" color="grey">
+          <Translate
+            zh_hant="作者尚未啟用"
+            zh_hans="作者尚未启用"
+            en="The author has not opened"
+          />
+        </TextIcon>
+
+        <style jsx>{styles}</style>
+      </section>
+    )
+  }
+
+  if (address && !!curatorAddress && !!creatorAddress) {
+    return (
+      <section
+        role="button"
+        className="item clickable"
+        onClick={() => {
+          switchToSetAmount(CURRENCY.USDT)
+        }}
+      >
+        <TextIcon
+          icon={<IconUSDTActive40 size="xl-m" />}
+          size="md"
+          spacing="xtight"
+        >
+          Tether
+        </TextIcon>
+        <CurrencyFormatter
+          value={formatAmount(balanceUSDT)}
+          currency={CURRENCY.USDT}
+        />
+
+        <style jsx>{styles}</style>
+      </section>
+    )
+  }
+
+  return (
+    <section role="button" className="item">
+      <TextIcon
+        icon={<IconUSDT40 size="xl-m" color="grey" />}
+        size="md"
+        spacing="xtight"
+        color="grey"
+      >
+        Tether
+      </TextIcon>
+
+      {!curatorAddress && (
+        <Button
+          spacing={['xxtight', 'base']}
+          borderColor="green"
+          borderRadius="5rem"
+          href={PATHS.ME_SETTINGS_CONNECT_WALLET}
+        >
+          <TextIcon color="green">
             <Translate
-              zh_hant="作者尚未啟用"
-              zh_hans="作者尚未启用"
-              en="The author has not opened"
+              zh_hant="前往設定錢包"
+              zh_hans="前往设定钱包"
+              en="Go to set up wallet"
             />
           </TextIcon>
-        </section>
+        </Button>
       )}
 
-      {!address && (
-        <section role="button" className="item">
-          <TextIcon
-            icon={<IconUSDT40 size="xl-m" color="grey" />}
-            size="md"
-            spacing="xtight"
-            color="grey"
-          >
-            Tether
+      {!!curatorAddress && (
+        <Button
+          spacing={['xxtight', 'base']}
+          borderColor="green"
+          borderRadius="5rem"
+          onClick={() => {
+            switchToWalletSelect()
+          }}
+        >
+          <TextIcon color="green">
+            <Translate
+              zh_hant="連接錢包"
+              zh_hans="连接钱包"
+              en="Connect Wallet"
+            />
           </TextIcon>
-          {!viewer.info.ethAddress && (
-            <Button
-              spacing={['xxtight', 'base']}
-              borderColor="green"
-              borderRadius="5rem"
-              href={PATHS.ME_SETTINGS_CONNECT_WALLET}
-            >
-              <TextIcon color="green">
-                <Translate
-                  zh_hant="前往設定錢包"
-                  zh_hans="前往设定钱包"
-                  en="Go to set up wallet"
-                />
-              </TextIcon>
-            </Button>
-          )}
-          {!!viewer.info.ethAddress && (
-            <Button
-              spacing={['xxtight', 'base']}
-              borderColor="green"
-              borderRadius="5rem"
-              onClick={() => {
-                switchToWalletSelect()
-              }}
-            >
-              <TextIcon color="green">
-                <Translate
-                  zh_hant="連接錢包"
-                  zh_hans="连接钱包"
-                  en="Connect Wallet"
-                />
-              </TextIcon>
-            </Button>
-          )}
-        </section>
+        </Button>
       )}
+
       <style jsx>{styles}</style>
-    </>
+    </section>
   )
 }
 
