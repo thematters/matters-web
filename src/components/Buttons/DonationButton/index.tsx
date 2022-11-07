f27f0421a647be76ae156ca649890de355f0bdeb
bluecloud <96812901+pitb2022@users.noreply.github.com>
2022-11-07 08:59:08 +0800

    style(DonationButton): return early

--- src/components/Buttons/DonationButton/index.tsx
+++ src/components/Buttons/DonationButton/index.tsx
@@ -6,41 +6,39 @@ interface DonationButtonProps {
 }
 
 const DonationButton = ({ supported, onClick }: DonationButtonProps) => {
+  if (supported) {
+    return (
+      <Button
+        size={['10.5rem', '2.5rem']}
+        bgColor="yellow-lighter"
+        borderColor="gold"
+        borderWidth="sm"
+        onClick={() => {
+          if (onClick) {
+            onClick()
+          }
+        }}
+      >
+        <TextIcon icon={<IconDonate24 />} weight="md" color="gold">
+          <Translate id="donationAgain" />
+        </TextIcon>
+      </Button>
+    )
+  }
   return (
-    <>
-      {supported && (
-        <Button
-          size={['10.5rem', '2.5rem']}
-          bgColor="yellow-lighter"
-          borderColor="gold"
-          borderWidth="sm"
-          onClick={() => {
-            if (onClick) {
-              onClick()
-            }
-          }}
-        >
-          <TextIcon icon={<IconDonate24 />} weight="md" color="gold">
-            <Translate id="donationAgain" />
-          </TextIcon>
-        </Button>
-      )}
-      {!supported && (
-        <Button
-          size={['10.5rem', '2.5rem']}
-          bgColor="gold-linear-gradient"
-          onClick={() => {
-            if (onClick) {
-              onClick()
-            }
-          }}
-        >
-          <TextIcon icon={<IconDonate24 />} weight="md" color="white">
-            <Translate id="donation" />
-          </TextIcon>
-        </Button>
-      )}
-    </>
+    <Button
+      size={['10.5rem', '2.5rem']}
+      bgColor="gold-linear-gradient"
+      onClick={() => {
+        if (onClick) {
+          onClick()
+        }
+      }}
+    >
+      <TextIcon icon={<IconDonate24 />} weight="md" color="white">
+        <Translate id="donation" />
+      </TextIcon>
+    </Button>
   )
 }
 
