8502c79c97e28ba4e927cb5ee71cd880c1e289a7
Aka jazzy <mingchenliang954@gmail.com>
2022-11-09 16:07:42 +0800

    fix(support-setting): fix ui style; textarea allow null string input

--- src/components/UserProfile/DropdownActions/EditProfileDialog/Content.tsx
+++ src/components/UserProfile/DropdownActions/EditProfileDialog/Content.tsx
@@ -189,7 +189,7 @@ const EditProfileDialogContent: React.FC<FormProps> = ({
           id: 'enterDisplayName',
           lang,
         })}
-        hint={<Translate id="hintDisplayName" />}
+        hint={<Translate id="supportSettingHint" />}
         value={values.displayName}
         error={touched.displayName && errors.displayName}
         onBlur={handleBlur}
@@ -204,7 +204,7 @@ const EditProfileDialogContent: React.FC<FormProps> = ({
           id: 'enterUserDescription',
           lang,
         })}
-        hint={<Translate id="hintDescription" />}
+        hint={<Translate id="supportSettingHint" />}
         value={values.description}
         error={touched.description && errors.description}
         onBlur={handleBlur}
