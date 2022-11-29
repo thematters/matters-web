6a15988cb36cb4845586d0df40a25e29062ac9d5
robertu <4065233+robertu7@users.noreply.github.com>
2022-11-29 19:45:25 +0800

    feat(test): break test.yml into test_build.yml and test_e2e.yml

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,13 +12,13 @@ import { devices, type PlaywrightTestConfig } from '@playwright/test'
 const config: PlaywrightTestConfig = {
   testDir: './tests',
   /* Maximum time one test can run for. */
-  timeout: 80 * 1000,
+  timeout: 30 * 1000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
      */
-    timeout: 5000,
+    timeout: 3000,
   },
   /* Run tests in files in parallel */
   fullyParallel: true,
