e4116310b052e092e363a91a1c9f573c24d596d5
bluecloud <96812901+pitb2022@users.noreply.github.com>
2022-12-01 11:48:47 +0800

    fix(Transaction): fix Subscription icon error

--- src/components/Empty/EmptyTransactionSubscription.tsx
+++ src/components/Empty/EmptyTransactionSubscription.tsx
@@ -1,8 +1,8 @@
-import { Empty, IconWallet24, Translate } from '~/components'
+import { Empty, IconCircle24, Translate } from '~/components'
 
 export const EmptyTransactionSubscription = () => (
   <Empty
-    icon={<IconWallet24 size="xxl" />}
+    icon={<IconCircle24 size="xxl" />}
     description={
       <Translate
         zh_hant="還沒有訂閱記錄"
