d8008956c286aff6d7b20a03e39d39bee0f496fb
bluecloud <96812901+pitb2022@users.noreply.github.com>
2022-12-01 17:20:55 +0800

    feat(Storybook): add <TagDigest.Concise> example

---
+++ src/stories/components/TagDigest/Concise.stories.tsx
@@ -0,0 +1,24 @@
+import { MockedProvider } from '@apollo/react-testing'
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+import React from 'react'
+
+import { TagDigest } from '~/components'
+
+import { MOCK_TAG } from '../../mocks'
+
+export default {
+  title: 'Components/TagDigest/Concise',
+  component: TagDigest.Concise,
+} as ComponentMeta<typeof TagDigest.Concise>
+
+const Template: ComponentStory<typeof TagDigest.Concise> = (args) => (
+  <MockedProvider>
+    <TagDigest.Concise {...args} />
+  </MockedProvider>
+)
+
+export const Default = Template.bind({})
+Default.args = {
+  tag: MOCK_TAG as any,
+  showArticlesNum: true,
+}
