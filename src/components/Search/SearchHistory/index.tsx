5e0922ae1f0a251ab20e4afd3b5ff06343b7aee7
bluecloud <96812901+pitb2022@users.noreply.github.com>
2022-12-07 15:33:58 +0800

    feat(Search): add <SearchHistory>

---
+++ src/components/Search/SearchHistory/index.tsx
@@ -0,0 +1,49 @@
+import { Button, IconClose32, Menu, TextIcon, Translate } from '~/components'
+
+import { toPath } from '~/common/utils'
+
+interface SearchHistoryProps {
+  data: string[]
+  removeSearchHistoryItem: (value: string) => void
+}
+import styles from './styles.css'
+
+export const SearchHistory = ({
+  data,
+  removeSearchHistoryItem,
+}: SearchHistoryProps) => {
+  return (
+    <section>
+      <section className="title">
+        <TextIcon size="xm" color="grey" weight="md">
+          <Translate zh_hans="最近搜索" zh_hant="最近搜尋" en="History" />
+        </TextIcon>
+      </section>
+      <Menu>
+        {data.map((value, i) => (
+          <Menu.Item
+            key={i}
+            {...toPath({
+              page: 'search',
+              q: value,
+            })}
+            spacing={['base', 'base']}
+          >
+            <section className="item">
+              <span className="key">{value}</span>
+              <Button
+                onClick={(e) => {
+                  e?.stopPropagation()
+                  removeSearchHistoryItem(value)
+                }}
+              >
+                <IconClose32 color="grey" />
+              </Button>
+            </section>
+          </Menu.Item>
+        ))}
+      </Menu>
+      <style jsx>{styles}</style>
+    </section>
+  )
+}
