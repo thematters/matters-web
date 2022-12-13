a18bc26ad79c618836a378341051fa4a64a5b88b
bluecloud <96812901+pitb2022@users.noreply.github.com>
2022-12-13 17:13:31 +0800

    feat(NavBar): Replace url when page already in search page

--- src/components/Layout/NavBar/index.tsx
+++ src/components/Layout/NavBar/index.tsx
@@ -11,6 +11,7 @@ import {
 } from '~/components'
 
 import { PATHS, TEXT } from '~/common/enums'
+import { toPath } from '~/common/utils'
 
 import UnreadIcon from '../UnreadIcon'
 import NavListItem from './NavListItem'
@@ -18,7 +19,7 @@ import styles from './styles.css'
 
 const NavBar = () => {
   const viewer = useContext(ViewerContext)
-  const { isInPath } = useRoute()
+  const { router, isInPath } = useRoute()
   const isInHome = isInPath('HOME')
   const isInFollow = isInPath('FOLLOW')
   const isInNotification = isInPath('ME_NOTIFICATIONS')
@@ -60,7 +61,17 @@ const NavBar = () => {
           icon={<IconNavSearch24 size="md" />}
           activeIcon={<IconNavSearch24 size="md" color="green" />}
           active={isInSearch}
-          href={PATHS.SEARCH}
+          onClick={() => {
+            const path = toPath({
+              page: 'search',
+            })
+
+            if (isInSearch) {
+              router.replace(path.href)
+            } else {
+              router.push(path.href)
+            }
+          }}
         />
 
         {viewer.isAuthed && (
