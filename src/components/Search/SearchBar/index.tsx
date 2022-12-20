aa6bedda780dcd6aa9c77cd80c2144563d821d04
bluecloud <96812901+pitb2022@users.noreply.github.com>
2022-12-20 14:43:21 +0800

    fix(Search): fix input value equal empty error

--- src/components/Search/SearchBar/index.tsx
+++ src/components/Search/SearchBar/index.tsx
@@ -163,6 +163,8 @@ export const SearchBar: React.FC<SearchBarProps> = ({
           type,
         })
 
+        if (values.q.length <= 0) return
+
         if (isSearch) {
           router.replace(path.href)
         } else {
