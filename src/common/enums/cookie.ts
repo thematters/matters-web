c3a4ee67635a922b6f205f6ac5614d2c7f4bac1a
robertu <4065233+robertu7@users.noreply.github.com>
2023-02-13 11:53:05 +0800

    fix(cookie): rename token cookie name to `__access_token`

--- src/common/enums/cookie.ts
+++ src/common/enums/cookie.ts
@@ -1,4 +1,4 @@
-export const COOKIE_TOKEN_NAME = '__token'
+export const COOKIE_TOKEN_NAME = '__access_token'
 export const COOKIE_USER_GROUP = '__user_group'
 export const COOKIE_LANGUAGE = '__language'
 
