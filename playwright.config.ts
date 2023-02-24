256c7b546b0a7ce7d947dc72fbd026d21e589294
bluecloud <96812901+pitb2022@users.noreply.github.com>
2023-02-24 21:06:02 +0800

    feat(test): add Firefox and Mobile Chrome Project

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ dotenv.config({ path: path.resolve(__dirname, './', '.env.local') })
 
 const LOCALE = 'en-US'
 const isCI = process.env.PLAYWRIGHT_RUNTIME_ENV === 'ci'
-// const isLocal = process.env.PLAYWRIGHT_RUNTIME_ENV === 'local'
+const isLocal = process.env.PLAYWRIGHT_RUNTIME_ENV === 'local'
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -39,16 +39,16 @@ const config: PlaywrightTestConfig = {
         locale: LOCALE,
       },
     },
-    // ...(isLocal
-    //   ? []
-    //   : [
-    //       {
-    //         name: 'firefox',
-    //         use: {
-    //           ...devices['Desktop Firefox'],
-    //           locale: LOCALE,
-    //         },
-    //       },
+    ...(isLocal
+      ? []
+      : [
+          {
+            name: 'firefox',
+            use: {
+              ...devices['Desktop Firefox'],
+              locale: LOCALE,
+            },
+          },
           // {
           //   name: 'webkit',
           //   use: {
@@ -56,16 +56,16 @@ const config: PlaywrightTestConfig = {
           //     locale: LOCALE,
           //   },
           // },
-        // ]),
+        ]),
 
     /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: {
-    //     ...devices['Pixel 5'],
-    //     locale: LOCALE,
-    //   },
-    // },
+    {
+      name: 'Mobile Chrome',
+      use: {
+        ...devices['Pixel 5'],
+        locale: LOCALE,
+      },
+    },
     // {
     //   name: 'Mobile Safari',
     //   use: {
