e57db79a99e32250c58c4ac144722d97af771357
bluecloud <96812901+pitb2022@users.noreply.github.com>
2023-02-24 14:41:48 +0800

    fix(test): add sleep time

--- tests/helpers/poms/userProfile.ts
+++ tests/helpers/poms/userProfile.ts
@@ -1,5 +1,6 @@
 import { Locator, Page } from '@playwright/test'
 
+import { sleep } from '@/src/common/utils/time'
 import { TEST_ID } from '~/common/enums'
 
 import { waitForAPIResponse } from '../api'
@@ -68,6 +69,7 @@ export class UserProfilePage {
     // go to homepage
     await this.page.goto('/')
 
+    await sleep(5 * 1000)
     // click "My Page" button
     await this.page.getByRole('button', { name: 'My Page' }).click()
 
