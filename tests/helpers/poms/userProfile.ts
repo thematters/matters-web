23f82912ede23d21dcd992934a70e91dadb572c4
bluecloud <96812901+pitb2022@users.noreply.github.com>
2023-02-27 11:10:06 +0800

    feat(test): revise commentArticle test

--- tests/helpers/poms/userProfile.ts
+++ tests/helpers/poms/userProfile.ts
@@ -76,10 +76,6 @@ export class UserProfilePage {
     // click "Profile" link
     await this.page.getByRole('link', { name: 'Profile' }).click()
     await this.page.waitForLoadState('networkidle')
-
-    // confirm User Profile Page
-    // await expect(this.page.getByTestId(TEST_ID.USER_PROFILE)).toBeVisible()
-    // await expect(this.feedArticles.first()).toBeVisible()
   }
 
   async goto(userName: string) {
