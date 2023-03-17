e5a965fd792b4d21250b265a3da9453240223182
Aka jazzy <mingchenliang954@gmail.com>
2023-03-17 10:36:56 +0800

    fix(i18n): revise circle analytics read counts copy

--- src/components/Dialogs/HelpDialog/index.tsx
+++ src/components/Dialogs/HelpDialog/index.tsx
@@ -1,8 +1,10 @@
+import { FormattedMessage } from 'react-intl'
+
 import {
   Dialog,
+  IconRead16,
   IconReadTimeTotal16,
   TextIcon,
-  Translate,
   useDialogSwitch,
 } from '~/components'
 
@@ -17,35 +19,15 @@ type Props = {
   children: ({ openDialog }: { openDialog: () => void }) => React.ReactNode
 } & HelpDetailProps
 
-const time = {
-  zh_hant:
-    '累計閱讀時數代表了你的作品被登入用戶閱讀的總時數，每次閱讀過程超過一定時間後才會計入。' +
-    '此外，同一個用戶的多次閱讀也會進行累計。',
-  zh_hans:
-    '累计阅读时長代表了你的作品被登入用户阅读的总时数，每次阅读过程超过一定时间后才会计入。' +
-    '此外，同一个用户的多次阅读也会进行累计。',
-  en: 'Accumulated read time indicates the total time length that registered users read.',
-}
-
-const count = {
-  zh_hant:
-    '閱讀次數代表了你的作品被登入用戶閱讀的總次數，每次閱讀過程超過一定時間後才會計入。' +
-    '此外，同一個用戶的多次閱讀也會進行累計。',
-  zh_hans:
-    '阅读次数代表了你的作品被登入用户阅读的总次数，每次阅读过程超过一定时间后才会计入' +
-    '此外，同一个用户的多次阅读也会进行累计。',
-  en: 'Read counts indicates how many registered users read.',
-}
-
 const ReadTime = () => (
   <>
     <h2 className="title">
       <TextIcon icon={<IconReadTimeTotal16 size="md" />} weight="md">
-        <Translate id="readTime" />
+        <FormattedMessage defaultMessage="Accumulated Read Time" description="src/components/Dialogs/HelpDialog/index.tsx" />
       </TextIcon>
     </h2>
     <p className="description">
-      <Translate zh_hant={time.zh_hant} zh_hans={time.zh_hans} en={time.en} />
+      <FormattedMessage defaultMessage="Accumulated read time indicates the total time length that registered users read." description="src/components/Dialogs/HelpDialog/index.tsx" />
     </p>
     <style jsx>{styles}</style>
   </>
@@ -54,16 +36,12 @@ const ReadTime = () => (
 const ReadCount = () => (
   <>
     <h2 className="title">
-      <TextIcon icon={<IconReadTimeTotal16 size="md" />} weight="md">
-        <Translate id="readCount" />
+      <TextIcon icon={<IconRead16 size="md" />} weight="md">
+        <FormattedMessage defaultMessage="Read Counts" description="" />
       </TextIcon>
     </h2>
     <p className="description">
-      <Translate
-        zh_hant={count.zh_hant}
-        zh_hans={count.zh_hans}
-        en={count.en}
-      />
+      <FormattedMessage defaultMessage="Read counts indicates how many registered users read." description="src/components/Dialogs/HelpDialog/index.tsx" />
     </p>
     <style jsx>{styles}</style>
   </>
@@ -78,7 +56,7 @@ const BaseHelpDialog = ({ children, hasCount, hasTime }: Props) => {
 
       <Dialog size="sm" isOpen={show} onDismiss={closeDialog}>
         <Dialog.Header
-          title={<Translate id="help" />}
+          title={<FormattedMessage defaultMessage="Help" description="" />}
           closeDialog={closeDialog}
         />
 
@@ -93,7 +71,7 @@ const BaseHelpDialog = ({ children, hasCount, hasTime }: Props) => {
             bgColor="green"
             onClick={closeDialog}
           >
-            <Translate id="understood" />
+            <FormattedMessage defaultMessage="I see" description="" />
           </Dialog.Footer.Button>
         </Dialog.Footer>
       </Dialog>
