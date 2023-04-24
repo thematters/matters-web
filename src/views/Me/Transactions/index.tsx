a2f64436c08e5461957d76eaf431e228a65f5696
bluecloud <96812901+pitb2022@users.noreply.github.com>
2023-04-24 17:52:20 +0800

    feat(Transactions): revise space

--- src/views/Me/Transactions/index.tsx
+++ src/views/Me/Transactions/index.tsx
@@ -12,6 +12,7 @@ import {
   InfiniteScroll,
   Layout,
   List,
+  ResponsiveWrapper,
   Spinner,
   Tabs,
   Transaction,
@@ -129,16 +130,18 @@ const BaseTransactions = ({ currency, purpose }: BaseTransactionsProps) => {
   }
 
   return (
-    <InfiniteScroll hasNextPage={pageInfo.hasNextPage} loadMore={loadMore}>
-      <List>
-        {edges.map(({ node, cursor }) => (
-          <List.Item key={cursor}>
-            <Transaction tx={node} />
-          </List.Item>
-        ))}
-      </List>
-      <style jsx>{styles}</style>
-    </InfiniteScroll>
+    <ResponsiveWrapper>
+      <InfiniteScroll hasNextPage={pageInfo.hasNextPage} loadMore={loadMore}>
+        <List>
+          {edges.map(({ node, cursor }) => (
+            <List.Item key={cursor}>
+              <Transaction tx={node} />
+            </List.Item>
+          ))}
+        </List>
+        <style jsx>{styles}</style>
+      </InfiniteScroll>
+    </ResponsiveWrapper>
   )
 }
 
