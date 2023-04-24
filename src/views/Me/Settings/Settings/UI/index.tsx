f1b493c4e8a8c5b6c5cc93aded29515d1e777c74
bluecloud <96812901+pitb2022@users.noreply.github.com>
2023-04-24 17:47:06 +0800

    feat(Settings.UI): revise border line

--- src/views/Me/Settings/Settings/UI/index.tsx
+++ src/views/Me/Settings/Settings/UI/index.tsx
@@ -6,20 +6,23 @@ import SwitchLanguage from './SwitchLanguage'
 
 const UISettings = () => {
   return (
-    <Form.List groupName={<Translate id="settingsUI" />} spacingX={0}>
-      <SwitchLanguage />
-      <CurrencyConvertor />
-      <section className="rate-hint">
-        <TextIcon icon={<IconInfo16 size="xs" />} color="grey" size="xs">
-          <Translate
-            zh_hans="加密货币汇率由 CoinGecko 提供"
-            zh_hant="加密貨幣匯率由 CoinGecko 提供"
-            en="Crypto rates provided by CoinGecko"
-          />
-        </TextIcon>
-      </section>
+    <section className="container">
+      <Form.List groupName={<Translate id="settingsUI" />} spacingX={0}>
+        <SwitchLanguage />
+        <hr className="dashed-line" />
+        <CurrencyConvertor />
+        <section className="rate-hint">
+          <TextIcon icon={<IconInfo16 size="xs" />} color="grey" size="xs">
+            <Translate
+              zh_hans="加密货币汇率由 CoinGecko 提供"
+              zh_hant="加密貨幣匯率由 CoinGecko 提供"
+              en="Crypto rates provided by CoinGecko"
+            />
+          </TextIcon>
+        </section>
+      </Form.List>
       <style jsx>{styles}</style>
-    </Form.List>
+    </section>
   )
 }
 
