0e9f84e4f176dde57f2ca2ded6faf5483ba4b9c4
bluecloud <96812901+pitb2022@users.noreply.github.com>
2023-04-26 18:18:48 +0800

    fix(Circle): revise layout & style

--- src/views/Circle/Works/Works.tsx
+++ src/views/Circle/Works/Works.tsx
@@ -150,6 +150,7 @@ const CircleDetailWorks = () => {
                 <ArticleDigestFeed
                   article={node}
                   hasCircle={false}
+                  hasAuthor={false}
                   onClick={() =>
                     analytics.trackEvent('click_feed', {
                       type: 'circle_detail',
@@ -158,14 +159,6 @@ const CircleDetailWorks = () => {
                       id: node.id,
                     })
                   }
-                  onClickAuthor={() => {
-                    analytics.trackEvent('click_feed', {
-                      type: 'circle_detail',
-                      contentType: 'user',
-                      location: i,
-                      id: node.author.id,
-                    })
-                  }}
                 />
               </List.Item>
             ))}
