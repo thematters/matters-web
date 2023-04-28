2132663ab7b385cb53bec8bb5381f2013bfe8e93
bluecloud <96812901+pitb2022@users.noreply.github.com>
2023-04-28 15:01:59 +0800

    fix(Follow): fix space

--- src/views/Follow/Feed/RecommendUserActivity/index.tsx
+++ src/views/Follow/Feed/RecommendUserActivity/index.tsx
@@ -17,27 +17,29 @@ const RecommendUserActivity = ({ users }: Props) => {
   }
 
   return (
-    <Slides header={<FollowingRecommendHead type="user" />}>
-      {users.map((user, index) => (
-        <Slides.Item
-          size="md"
-          key={index}
-          onClick={() => {
-            analytics.trackEvent('click_feed', {
-              type: 'following',
-              contentType: 'UserRecommendationActivity',
-              location: `${location}.${index}`,
-              id: user.id,
-            })
-          }}
-        >
-          <section className="item">
-            <FollowingRecommendUser user={user} />
-          </section>
-        </Slides.Item>
-      ))}
+    <section className="container">
+      <Slides header={<FollowingRecommendHead type="user" />}>
+        {users.map((user, index) => (
+          <Slides.Item
+            size="md"
+            key={index}
+            onClick={() => {
+              analytics.trackEvent('click_feed', {
+                type: 'following',
+                contentType: 'UserRecommendationActivity',
+                location: `${location}.${index}`,
+                id: user.id,
+              })
+            }}
+          >
+            <section className="item">
+              <FollowingRecommendUser user={user} />
+            </section>
+          </Slides.Item>
+        ))}
+      </Slides>
       <style jsx>{styles}</style>
-    </Slides>
+    </section>
   )
 }
 
