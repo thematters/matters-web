b8544176dfcbcd1c17e2c7600a9bf94c7151f12a
bluecloud <96812901+pitb2022@users.noreply.github.com>
2023-05-09 15:55:51 +0800

    feat(e2e): set english lang when test launch

--- tests/globalSetup.ts
+++ tests/globalSetup.ts
@@ -22,6 +22,28 @@ const prepareUserStorageState = async (baseURL: string, user: User) => {
   await browser.close()
 }
 
+const setupEnglish = async (baseURL: string, user: User) => {
+  const browser = await chromium.launch()
+
+  const context = await browser.newContext({
+    storageState: `test-results/storageState-${user.email}.json`,
+  })
+  const request = context.request
+  await request.post(process.env.NEXT_PUBLIC_API_URL as string, {
+    data: {
+      variables: { input: { language: 'en' } },
+      query: `mutation UpdateLanguage($input: UpdateUserInfoInput!) {
+        updateUserInfo(input: $input) {
+          id
+          settings {
+            language
+          }
+        }
+      }`,
+    },
+  })
+}
+
 async function globalSetup(config: FullConfig) {
   // read `baseURL` from `playwright.config.ts`
   const { baseURL } = config.projects[0].use
@@ -29,6 +51,9 @@ async function globalSetup(config: FullConfig) {
   // prepare users storages
   await prepareUserStorageState(baseURL as string, users.alice)
   await prepareUserStorageState(baseURL as string, users.bob)
+
+  await setupEnglish(baseURL as string, users.alice)
+  await setupEnglish(baseURL as string, users.bob)
 }
 
 export default globalSetup
