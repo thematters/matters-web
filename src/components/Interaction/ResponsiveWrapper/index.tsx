d2f99949511575dc5936bd3a1b7c36a923040d54
bluecloud <96812901+pitb2022@users.noreply.github.com>
2023-05-10 16:09:00 +0800

    feat(WalletAuthForm): revise space

--- src/components/Interaction/ResponsiveWrapper/index.tsx
+++ src/components/Interaction/ResponsiveWrapper/index.tsx
@@ -1,12 +1,17 @@
+import classNames from 'classnames'
 import React from 'react'
 
 import styles from './styles.css'
 
-export const ResponsiveWrapper: React.FC<React.PropsWithChildren<{}>> = ({
-  children,
-}) => {
+export const ResponsiveWrapper: React.FC<
+  React.PropsWithChildren<{ isInDialog?: boolean }>
+> = ({ isInDialog, children }) => {
+  const containerClasses = classNames({
+    container: true,
+    isInDialog: !!isInDialog,
+  })
   return (
-    <section className="container">
+    <section className={containerClasses}>
       {children}
       <style jsx>{styles}</style>
     </section>
