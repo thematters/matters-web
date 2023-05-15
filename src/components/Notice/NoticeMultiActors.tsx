a43c5858aec7b8858b8ce3981853577ba753ed7d
bluecloud <96812901+pitb2022@users.noreply.github.com>
2023-05-15 17:19:01 +0800

    feat(NoticeMultiActors): revise code

--- src/components/Notice/NoticeMultiActors.tsx
+++ src/components/Notice/NoticeMultiActors.tsx
@@ -12,29 +12,31 @@ const NoticeMultiActors = ({ actors, size }: NoticeMultiActorsProps) => {
   const actorsCount = actors.length
   const showAll = actorsCount <= 8
 
-  return (
-    <>
-      {showAll &&
-        actors.map((actor, index) => (
+  if (showAll) {
+    return (
+      <>
+        {actors.map((actor, index) => (
           <NoticeActorAvatar key={index} user={actor} size={size} />
         ))}
-      {!showAll && (
-        <>
-          {actors.slice(0, 7).map((actor, index) => (
-            <NoticeActorAvatar key={index} user={actor} size={size} />
-          ))}
-          <span className="more-actors">
-            <Button
-              bgColor="grey-lighter"
-              bgActiveColor="grey-lighter"
-              borderRadius="5rem"
-            >
-              <IconMore32 color="grey" size="lg" />
-            </Button>
-            <style jsx>{styles}</style>
-          </span>
-        </>
-      )}
+      </>
+    )
+  }
+
+  return (
+    <>
+      {actors.slice(0, 7).map((actor, index) => (
+        <NoticeActorAvatar key={index} user={actor} size={size} />
+      ))}
+      <span className="more-actors">
+        <Button
+          bgColor="grey-lighter"
+          bgActiveColor="grey-lighter"
+          borderRadius="5rem"
+        >
+          <IconMore32 color="grey" size="lg" />
+        </Button>
+        <style jsx>{styles}</style>
+      </span>
     </>
   )
 }
