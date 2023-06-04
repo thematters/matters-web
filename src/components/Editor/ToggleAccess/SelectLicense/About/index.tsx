f0b34b61a8f877aeb99f62ab056a84dd27ff00d4
xdj <13580441+gary02@users.noreply.github.com>
2023-06-04 20:57:36 +0800

    feat(license): add icon

--- src/components/Editor/ToggleAccess/SelectLicense/About/index.tsx
+++ src/components/Editor/ToggleAccess/SelectLicense/About/index.tsx
@@ -1,3 +1,5 @@
+import { IconExternalLink16 } from '~/components'
+
 import styles from './styles.css'
 
 const About = ({ desc, url }: { desc: string; url: string }) => {
@@ -6,6 +8,7 @@ const About = ({ desc, url }: { desc: string; url: string }) => {
       <a href={url} target="_blank">
         {desc}
       </a>
+      <IconExternalLink16 color="grey" size="xs" />
       <style jsx>{styles}</style>
     </>
   )
