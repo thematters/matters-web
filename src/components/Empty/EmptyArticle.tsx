6b4aeb7996881216e221d7751300562a8ff5418e
bluecloud <96812901+pitb2022@users.noreply.github.com>
2023-06-20 17:01:38 +0800

    feat(EmptyArticle): update copy

--- src/components/Empty/EmptyArticle.tsx
+++ src/components/Empty/EmptyArticle.tsx
@@ -1,9 +1,18 @@
-import { Empty, IconFile88, Translate } from '~/components'
+import { useIntl } from 'react-intl'
 
-export const EmptyArticle = () => (
-  <Empty
-    spacingY="xxloose"
-    icon={<IconFile88 size="xxxlM" />}
-    description={<Translate id="noArticlesYet" />}
-  />
-)
+import { Empty, IconFile88 } from '~/components'
+
+export const EmptyArticle = () => {
+  const intl = useIntl()
+
+  return (
+    <Empty
+      spacingY="xxloose"
+      icon={<IconFile88 size="xxxlM" />}
+      description={intl.formatMessage({
+        defaultMessage: 'No published articles yet',
+        description: 'src/components/Empty/EmptyArticle.tsx',
+      })}
+    />
+  )
+}
