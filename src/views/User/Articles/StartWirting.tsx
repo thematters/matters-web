49fa7988e4cce2c1b22d3869a72a41f49c855d52
robertu <4065233+robertu7@users.noreply.github.com>
2023-07-19 22:04:24 +0800

    fix(user): show setup likerId dialog on click writing button

--- src/views/User/Articles/StartWirting.tsx
+++ src/views/User/Articles/StartWirting.tsx
@@ -2,14 +2,21 @@ import { useRouter } from 'next/router'
 import { useContext } from 'react'
 import { FormattedMessage } from 'react-intl'
 
+import { OPEN_LIKE_COIN_DIALOG } from '~/common/enums'
 import { toPath, translate } from '~/common/utils'
-import { Button, LanguageContext, useMutation } from '~/components'
+import {
+  Button,
+  LanguageContext,
+  useMutation,
+  ViewerContext,
+} from '~/components'
 import CREATE_DRAFT from '~/components/GQL/mutations/createDraft'
 import { CreateDraftMutation } from '~/gql/graphql'
 
 import styles from './styles.module.css'
 
 const StartWriting = () => {
+  const viewer = useContext(ViewerContext)
   const router = useRouter()
   const { lang } = useContext(LanguageContext)
   const [putDraft] = useMutation<CreateDraftMutation>(CREATE_DRAFT, {
@@ -27,6 +34,11 @@ const StartWriting = () => {
         textColor="green"
         textActiveColor="green"
         onClick={async () => {
+          if (viewer.shouldSetupLikerID) {
+            window.dispatchEvent(new CustomEvent(OPEN_LIKE_COIN_DIALOG, {}))
+            return
+          }
+
           const result = await putDraft()
           const { slug, id } = result?.data?.putDraft || {}
 
