f1b8dae581a49437bf36e82781a810191366360b
robertu <4065233+robertu7@users.noreply.github.com>
2023-08-08 10:36:12 +0800

    fix(links): fix i18n links

--- src/views/Guide/content_ZhHans.ts
+++ src/views/Guide/content_ZhHans.ts
@@ -5,22 +5,22 @@ const zh_hans = `
 
 如果你初来乍到，还在熟悉网站的基本功能，以下有两份超详尽的基础功能指南，你可以在这两份指南中，了解到如何使用编辑器发文、什么是 IPFS、如何管理自己的创作收入、如何加入更多写作社群等。
 
-*   [Matters 读者工具箱 | 新手必看](https://matters.town/zh-Hans/@hi176/387122)
-*   [Matters 作者工具箱 | 新手必看](https://matters.town/zh-Hans/@hi176/387126)
+*   [Matters 读者工具箱 | 新手必看](https://matters.town/@hi176/387122?locale=zh-Hans)
+*   [Matters 作者工具箱 | 新手必看](https://matters.town/@hi176/387126?locale=zh-Hans)
 
 Matters 目前尚未推出 App，如果你想要在手机上使用 ${process.env.NEXT_PUBLIC_SITE_BRAND_NAME}，非常推荐你使用 PWA，功能跟 App 几乎完全一样。
 
-*   [Matters 应用下载（PWA）指南](https://matters.town/zh-Hans/@hi176/387115)
+*   [Matters 应用下载（PWA）指南](https://matters.town/@hi176/387115?locale=zh-Hans)
 
 ## 在 ${process.env.NEXT_PUBLIC_SITE_BRAND_NAME} 探索 Web3 的世界
 
 ${process.env.NEXT_PUBLIC_SITE_BRAND_NAME} 致力于探索 Web3 与创作者经济，我们提供加密钱包注册与登入的功能。绑定钱包后，也可以使用支付与提现等功能。
 
-*   [Matters 加密钱包指南](https://matters.town/zh-Hans/@hi176/387112)
+*   [Matters 加密钱包指南](https://matters.town/@hi176/387112?locale=zh-Hans)
 
 ${process.env.NEXT_PUBLIC_SITE_BRAND_NAME} 的经济系统支援法币（港币）及 USDT、LikeCoin 等虚拟货币，欢迎你使用加密货币支持优质作者，也可以将获得的报酬提领出来。
 
-*   [Matters 支付与提现指南](https://matters.town/zh-Hans/@hi176/387119)
+*   [Matters 支付与提现指南](https://matters.town/@hi176/387119?locale=zh-Hans)
 
 如果你想用手机进行登入、绑定钱包及进行 USDT-Polygon 支付，请参考我们的手机版专属指南，可以为你解决绝大部分的疑惑。
 
@@ -28,6 +28,6 @@ ${process.env.NEXT_PUBLIC_SITE_BRAND_NAME} 的经济系统支援法币（港币�
 
 好文章不遗漏，我们也支援 RSS 、JSON 阅读器或 IPNS 兼容的 Planet 阅读器来订阅作者。
 
-*   [Matters 内容订阅使用指南](https://matters.town/zh-Hans/@hi176/387116)
+*   [Matters 内容订阅使用指南](https://matters.town/@hi176/387116?locale=zh-Hans)
 `
 export default zh_hans
