69c1215e9fa3f789cfac4f72c4b5a12749b8a280
49659410+tx0c <>
2023-10-08 16:27:07 +0000

    feat(ga-event): Emit GA event: image upload time
    resolve #3850
    
    image_upload with delay_msecs

--- src/components/Hook/useDirectImageUpload.tsx
+++ src/components/Hook/useDirectImageUpload.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 
+import { analytics } from '~/common/utils'
+
 export const useDirectImageUpload = () => {
   const [uploading, setUploading] = useState(false)
 
@@ -13,9 +15,18 @@ export const useDirectImageUpload = () => {
     setUploading(true)
 
     try {
+      const started = window?.performance.now() ?? -1
       const formData = new FormData()
       formData.append('file', file)
       await fetch(uploadURL, { method: 'POST', body: formData })
+
+      analytics.trackEvent('image_upload', {
+        uploadURL,
+        type: file.type,
+        size: file.size,
+        // performance.now() = Date.now() - performance.timing.navigationStart
+        delay_msecs: (window?.performance.now() ?? -1) - started,
+      })
       setUploading(false)
     } catch (error) {
       setUploading(false)
