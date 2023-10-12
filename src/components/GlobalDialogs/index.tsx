6dfb3c114a676b787e8180dd0c2345af369c6050
bluecloud <96812901+pitb2022@users.noreply.github.com>
2023-10-12 17:12:06 +0800

    fix(GlobalDialogs): disable SetUserNameDialog in callback page
    ref:
    202309-product--2#ZN-0038

--- src/components/GlobalDialogs/index.tsx
+++ src/components/GlobalDialogs/index.tsx
@@ -1,22 +1,31 @@
 import { useContext, useEffect } from 'react'
 
 import { OPEN_SET_USER_NAME_DIALOG } from '~/common/enums'
-import { LikeCoinDialog, SetUserNameDialog, ViewerContext } from '~/components'
+import {
+  LikeCoinDialog,
+  SetUserNameDialog,
+  useRoute,
+  ViewerContext,
+} from '~/components'
 
 import UniversalAuthDialog from './UniversalAuthDialog'
 
 const GlobalDialogs = () => {
   const viewer = useContext(ViewerContext)
+  const { router, isInPath } = useRoute()
 
   useEffect(() => {
     if (!viewer.isAuthed) {
       return
     }
 
-    if (!viewer.userName || viewer.info.userNameEditable) {
+    if (
+      (!viewer.userName || viewer.info.userNameEditable) &&
+      !isInPath('CALLBACK_PROVIDER')
+    ) {
       window.dispatchEvent(new CustomEvent(OPEN_SET_USER_NAME_DIALOG))
     }
-  }, [])
+  }, [router.pathname])
 
   return (
     <>
