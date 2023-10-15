bc42669d2d0c16be033f4e4d8d319a21e93530ff
robertu <4065233+robertu7@users.noreply.github.com>
2023-10-15 21:33:03 +0700

    feat(test): add tests for <SegmentedTabs>

---
+++ src/components/SegmentedTabs/SegmentedTabs.test.tsx
@@ -0,0 +1,89 @@
+import { render, screen } from '@testing-library/react'
+import { useState } from 'react'
+import { describe, expect, it } from 'vitest'
+
+import { SegmentedTabs } from '~/components'
+
+type FeedType = 'alice' | 'bob'
+const AnyTabs = () => {
+  const [tab, setTab] = useState<FeedType>('alice')
+
+  return (
+    <SegmentedTabs>
+      <SegmentedTabs.Tab
+        onClick={() => setTab('alice')}
+        selected={tab === 'alice'}
+      >
+        alice
+      </SegmentedTabs.Tab>
+
+      <SegmentedTabs.Tab onClick={() => setTab('bob')} selected={tab === 'bob'}>
+        bob
+      </SegmentedTabs.Tab>
+    </SegmentedTabs>
+  )
+}
+
+describe('SegmentedTabs', () => {
+  it('should allow to select a tab', async () => {
+    render(<AnyTabs />)
+
+    const $tabs = screen.getAllByRole('tab')
+    const $firstTab = $tabs[0]
+    const $secondTab = $tabs[1]
+
+    // initial selected tab
+    expect(screen.getByRole('tab', { selected: true }).innerText).toBe(
+      $firstTab.innerText
+    )
+
+    // switch to second tab
+    $secondTab.click()
+    expect(screen.getByRole('tab', { selected: true }).innerText).toBe(
+      $secondTab.innerText
+    )
+
+    // switch to first tab
+    $firstTab.click()
+    expect(screen.getByRole('tab', { selected: true }).innerText).toBe(
+      $firstTab.innerText
+    )
+  })
+
+  it('should allow to be disabled', async () => {
+    render(
+      <SegmentedTabs>
+        <SegmentedTabs.Tab disabled selected>
+          alice
+        </SegmentedTabs.Tab>
+        <SegmentedTabs.Tab disabled>bob</SegmentedTabs.Tab>
+      </SegmentedTabs>
+    )
+
+    const $tabs = screen.getAllByRole('tab')
+    const $firstTab = $tabs[0]
+    const $secondTab = $tabs[1]
+
+    // initial selected tab
+    expect(screen.getByRole('tab', { selected: true }).innerText).toBe(
+      $firstTab.innerText
+    )
+
+    // switch to second tab
+    $secondTab.click()
+    expect(screen.getByRole('tab', { selected: true }).innerText).toBe(
+      $firstTab.innerText
+    )
+  })
+
+  it('should allow to have a side', async () => {
+    render(
+      <SegmentedTabs side={<div>side</div>}>
+        <SegmentedTabs.Tab selected>alice</SegmentedTabs.Tab>
+        <SegmentedTabs.Tab>bob</SegmentedTabs.Tab>
+      </SegmentedTabs>
+    )
+
+    expect(screen.getByText('side')).toBeInTheDocument()
+  })
+})
