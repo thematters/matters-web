bb4ba69a176b9911512c73d87db883712b63eda0
robertu <4065233+robertu7@users.noreply.github.com>
2023-10-17 21:54:15 +0700

    feat(test): add tests for <TableView>

---
+++ src/components/TableView/TableView.test.tsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import { describe, expect, it, vi } from 'vitest'
+
+import { render, screen } from '~/common/utils/test'
+import { TableView } from '~/components'
+
+describe('<TableView>', () => {
+  it('should render tableview and cells', () => {
+    render(
+      <TableView groupName="Group 1">
+        <TableView.Cell title="Cell 1" />
+        <TableView.Cell title="Cell 2" />
+      </TableView>
+    )
+
+    expect(screen.getByRole('list')).toBeInTheDocument()
+    expect(screen.getAllByRole('listitem')).toHaveLength(2)
+  })
+
+  it('should render tableview and cells with subtitle', () => {
+    render(
+      <TableView groupName="Group 1">
+        <TableView.Cell title="Cell 1" subtitle="Subtitle 1" />
+        <TableView.Cell title="Cell 2" subtitle="Subtitle 2" />
+      </TableView>
+    )
+
+    expect(screen.getByRole('list')).toBeInTheDocument()
+    expect(screen.getAllByRole('listitem')).toHaveLength(2)
+
+    expect(screen.getByText('Cell 1')).toBeInTheDocument()
+    expect(screen.getByText('Subtitle 1')).toBeInTheDocument()
+    expect(screen.getByText('Cell 2')).toBeInTheDocument()
+    expect(screen.getByText('Subtitle 2')).toBeInTheDocument()
+  })
+
+  it('should render tableview and cells with custom right node', () => {
+    render(
+      <TableView groupName="Group 1">
+        <TableView.Cell title="Cell 1" right={<span>Right 1</span>} />
+        <TableView.Cell title="Cell 2" right={<span>Right 2</span>} />
+      </TableView>
+    )
+
+    expect(screen.getByRole('list')).toBeInTheDocument()
+    expect(screen.getAllByRole('listitem')).toHaveLength(2)
+
+    expect(screen.getByText('Cell 1')).toBeInTheDocument()
+    expect(screen.getByText('Right 1')).toBeInTheDocument()
+    expect(screen.getByText('Cell 2')).toBeInTheDocument()
+    expect(screen.getByText('Right 2')).toBeInTheDocument()
+  })
+
+  it('should render tableview and cells with custom onClick', () => {
+    const onClick = vi.fn()
+
+    render(
+      <TableView groupName="Group 1">
+        <TableView.Cell title="Cell 1" onClick={onClick} />
+        <TableView.Cell title="Cell 2" onClick={onClick} />
+      </TableView>
+    )
+
+    expect(screen.getByRole('list')).toBeInTheDocument()
+    expect(screen.getAllByRole('listitem')).toHaveLength(2)
+
+    expect(screen.getByText('Cell 1')).toBeInTheDocument()
+    expect(screen.getByText('Cell 2')).toBeInTheDocument()
+
+    screen.getByText('Cell 1').click()
+    screen.getByText('Cell 2').click()
+
+    expect(onClick).toHaveBeenCalledTimes(2)
+  })
+})
