20d0c47e244989b98cac45193f3823c3edd02874
robertu <4065233+robertu7@users.noreply.github.com>
2023-10-17 21:45:15 +0700

    feat(test): add tests for <Menu>

---
+++ src/components/Menu/Menu.test.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { describe, expect, it, vi } from 'vitest'
+
+import { fireEvent, render, screen } from '~/common/utils/test'
+import { Menu } from '~/components'
+
+describe('<Menu>', () => {
+  it('should render Menu with items', () => {
+    const handleMenuAClick = vi.fn()
+    const handleMenuBClick = vi.fn()
+
+    render(
+      <Menu>
+        <Menu.Item text="Item A" onClick={handleMenuAClick} />
+        <Menu.Item text="Item B" onClick={handleMenuBClick} />
+      </Menu>
+    )
+
+    const $itemA = screen.getByText('Item A')
+    expect($itemA).toBeInTheDocument()
+    const $itemB = screen.getByText('Item B')
+    expect($itemB).toBeInTheDocument()
+
+    fireEvent.click($itemA)
+    expect(handleMenuAClick).toHaveBeenCalled()
+    fireEvent.click($itemB)
+    expect(handleMenuBClick).toHaveBeenCalled()
+  })
+})
