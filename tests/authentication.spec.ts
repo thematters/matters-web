db4c85c3935680bed2929c375d520197a6ca7e80
bluecloud <96812901+pitb2022@users.noreply.github.com>
2023-10-19 17:19:01 +0800

    fix(e2e): add email login unhappy path test

--- tests/authentication.spec.ts
+++ tests/authentication.spec.ts
@@ -3,6 +3,7 @@ import { expect, test } from '@playwright/test'
 import { TEST_ID } from '~/common/enums'
 
 import { authedTest, login, logout, pageGoto } from './helpers'
+import { PASSWORDOR_CODE } from './helpers/enum'
 
 test.describe.configure({ mode: 'serial' })
 
@@ -41,6 +42,38 @@ test.describe('Authentication', () => {
 
     // Expect homepage has "Notification" button on the left side
     await expect(page.getByTestId(TEST_ID.SIDE_NAV_NOTIFICATIONS)).toBeVisible()
+  })
+
+  test('Login with email and OTP', async ({ page }) => {
+    await pageGoto(page, '/login')
+
+    // Login with email & password
+    await page.getByRole('button', { name: 'Email', exact: true }).click()
+
+    const email = `e2etest${Date.now()}@matters.town`
+    // Fill the form
+    await page.getByPlaceholder('Email').fill(email)
+
+    // OTP CODE_EXPIRED error
+    await page.getByPlaceholder('Password').fill(PASSWORDOR_CODE.CODE_EXPIRED)
+    await page.getByRole('button', { name: 'Sign in' }).click()
+    await expect(
+      page.getByText('This login code has expired, please try to resend')
+    ).toBeVisible()
+
+    // USER_PASSWORD_INVALID error
+    await page
+      .getByPlaceholder('Password')
+      .fill(PASSWORDOR_CODE.USER_PASSWORD_INVALID)
+    await page.getByRole('button', { name: 'Sign in' }).click()
+    await expect(page.getByText('Incorrect email or password')).toBeVisible()
+
+    // UNKNOWN_ERROR error
+    await page.getByPlaceholder('Password').fill(PASSWORDOR_CODE.UNKNOWN_ERROR)
+    await page.getByRole('button', { name: 'Sign in' }).click()
+    await expect(
+      page.getByText('Unknown error. Please try again later.')
+    ).toBeVisible()
   })
 
   authedTest(
