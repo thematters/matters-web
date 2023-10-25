b231afd0eb57db0ee88ef528ef4bd63081443a24
robertu <4065233+robertu7@users.noreply.github.com>
2023-10-26 00:31:12 +0800

    feat(test): add tests for <ArticleDigest.Notice> and fix its nested <a> issue

---
+++ src/components/ArticleDigest/Notice/Notice.test.tsx
@@ -0,0 +1,25 @@
+import mockRouter from 'next-router-mock'
+import { describe, expect, it } from 'vitest'
+
+import { TEST_ID } from '~/common/enums'
+import { render, screen } from '~/common/utils/test'
+import { ArticleDigestNotice } from '~/components'
+import { MOCK_ARTILCE } from '~/stories/mocks'
+
+describe('<ArticleDigest.Notice>', () => {
+  it('should render an ArticleDigest.Notice', () => {
+    render(<ArticleDigestNotice article={MOCK_ARTILCE} />)
+
+    const $digest = screen.getByTestId(TEST_ID.DIGEST_ARTICLE_NOTICE)
+    expect($digest).toBeInTheDocument()
+
+    const $title = screen.getByRole('heading', { name: MOCK_ARTILCE.title })
+    expect($title).toBeInTheDocument()
+
+    const $summary = screen.getByText(MOCK_ARTILCE.summary)
+    expect($summary).toBeInTheDocument()
+
+    $digest.click()
+    expect(mockRouter.asPath).toContain(MOCK_ARTILCE.slug)
+  })
+})
