38658e61a7342946ec8092ab2aae2472892981be
bluecloud <96812901+pitb2022@users.noreply.github.com>
2023-10-25 17:35:12 +0800

    feat(WorksTabs): add draft count

--- src/views/Me/Works/WorksTabs/index.tsx
+++ src/views/Me/Works/WorksTabs/index.tsx
@@ -1,18 +1,39 @@
+import { useQuery } from '@apollo/react-hooks'
+import gql from 'graphql-tag'
 import { FormattedMessage } from 'react-intl'
 
 import { PATHS } from '~/common/enums'
 import { Tabs, useRoute } from '~/components'
+import { MeWorksTabsQuery } from '~/gql/graphql'
+
+export const ME_WORKS_TABS = gql`
+  query MeWorksTabs {
+    viewer {
+      id
+      drafts(input: { first: 0 }) @connection(key: "viewerDrafts") {
+        totalCount
+      }
+    }
+  }
+`
 
 const WorksTabs: React.FC = () => {
   const { isInPath } = useRoute()
+  const { data } = useQuery<MeWorksTabsQuery>(ME_WORKS_TABS)
 
   const isDrafts = isInPath('ME_WORKS')
   const isPublished = isInPath('ME_WORKS_PUBLISHED')
   const isArchived = isInPath('ME_WORKS_ARCHIVED')
 
+  const draftsCount = data?.viewer?.drafts.totalCount || 0
+
   return (
     <Tabs>
-      <Tabs.Tab href={PATHS.ME_WORKS} selected={isDrafts}>
+      <Tabs.Tab
+        href={PATHS.ME_WORKS}
+        selected={isDrafts}
+        count={draftsCount > 0 ? draftsCount : undefined}
+      >
         <FormattedMessage
           defaultMessage="Drafts"
           id="VeEfHm"
