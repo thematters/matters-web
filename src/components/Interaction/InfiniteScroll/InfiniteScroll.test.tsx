7d030de69e97def46af67ba7508d129613e731fe
robertu <4065233+robertu7@users.noreply.github.com>
2023-10-30 22:50:09 +0800

    feat(test): add tests for interaction components

---
+++ src/components/Interaction/InfiniteScroll/InfiniteScroll.test.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { describe, expect, it } from 'vitest'
+
+import { render, screen } from '~/common/utils/test'
+import { InfiniteScroll } from '~/components'
+
+describe('<InfiniteScroll>', () => {
+  it('should render the children and loader', () => {
+    render(
+      <InfiniteScroll
+        hasNextPage
+        loadMore={async () => {}}
+        loader={<span>Loading</span>}
+        eof="No more results"
+      >
+        <div>Child 1</div>
+        <div>Child 2</div>
+      </InfiniteScroll>
+    )
+
+    expect(screen.getByText('Child 1')).toBeInTheDocument()
+    expect(screen.getByText('Child 2')).toBeInTheDocument()
+    expect(screen.getByText('Loading')).toBeInTheDocument()
+    expect(screen.queryByText('No more results')).not.toBeInTheDocument()
+  })
+
+  it('should not render the loader when there are no more pages', () => {
+    render(
+      <InfiniteScroll
+        hasNextPage={false}
+        loadMore={async () => {}}
+        loader={<span>Loading</span>}
+        eof="No more results"
+      >
+        <div>Child 1</div>
+        <div>Child 2</div>
+      </InfiniteScroll>
+    )
+
+    expect(screen.queryByText('Loading')).not.toBeInTheDocument()
+    expect(screen.getByText('No more results')).toBeInTheDocument()
+  })
+
+  // FIXME: not support clipboard in jsdom yet
+  // it('should call the loadMore function when the waypoint is reached', () => {
+  //   const loadMoreMock = vi.fn()
+
+  //    render(
+  //     <InfiniteScroll hasNextPage={true} loadMore={loadMoreMock} eof={null}>
+  //       <div>Child 1</div>
+  //       <div>Child 2</div>
+  //     </InfiniteScroll>
+  //   )
+  // })
+})
