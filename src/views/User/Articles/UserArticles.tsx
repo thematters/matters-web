861b0e42f175340d5b6a6875b9f746c3841f1647
bluecloud <96812901+pitb2022@users.noreply.github.com>
2023-10-31 15:32:48 +0800

    feat(Articles): remove archived article

--- src/views/User/Articles/UserArticles.tsx
+++ src/views/User/Articles/UserArticles.tsx
@@ -4,7 +4,6 @@ import ICON_AVATAR_DEFAULT from '@/public/static/icons/72px/avatar-default.svg'
 import PROFILE_COVER_DEFAULT from '@/public/static/images/profile-cover.png'
 import { analytics, mergeConnections, stripSpaces } from '~/common/utils'
 import {
-  ArticleDigestArchived,
   ArticleDigestFeed,
   Empty,
   EmptyArticle,
@@ -172,10 +171,6 @@ const UserArticles = () => {
     )
   }
 
-  const articleEdges = edges.filter(
-    ({ node }) => node.articleState === 'active' || viewer.id === node.author.id
-  )
-
   return (
     <>
       <CustomHead />
@@ -190,28 +185,24 @@ const UserArticles = () => {
           eof
         >
           <List>
-            {articleEdges.map(({ node, cursor }, i) => (
+            {edges.map(({ node, cursor }, i) => (
               <List.Item key={cursor}>
-                {node.articleState !== 'active' ? (
-                  <ArticleDigestArchived article={node} />
-                ) : (
-                  <ArticleDigestFeed
-                    article={node}
-                    inUserArticles
-                    hasAuthor={false}
-                    hasEdit={true}
-                    hasAddCollection={true}
-                    hasArchive={true}
-                    onClick={() =>
-                      analytics.trackEvent('click_feed', {
-                        type: 'user_article',
-                        contentType: 'article',
-                        location: i,
-                        id: node.id,
-                      })
-                    }
-                  />
-                )}
+                <ArticleDigestFeed
+                  article={node}
+                  inUserArticles
+                  hasAuthor={false}
+                  hasEdit={true}
+                  hasAddCollection={true}
+                  hasArchive={true}
+                  onClick={() =>
+                    analytics.trackEvent('click_feed', {
+                      type: 'user_article',
+                      contentType: 'article',
+                      location: i,
+                      id: node.id,
+                    })
+                  }
+                />
               </List.Item>
             ))}
           </List>
