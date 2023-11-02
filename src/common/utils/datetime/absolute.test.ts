a8cef313799899bd944ef8244a36186c4042fc16
robertu <4065233+robertu7@users.noreply.github.com>
2023-11-02 20:58:54 +0800

    feat(test): add tests for utils/datetime

---
+++ src/common/utils/datetime/absolute.test.ts
@@ -0,0 +1,71 @@
+import { describe, expect, it } from 'vitest'
+
+import toAbsoluteDate from './absolute'
+
+describe('datetime/absolute', () => {
+  it('should parse a string date', () => {
+    const date = '2022-01-01'
+    const result = toAbsoluteDate(date, 'en')
+    expect(typeof result).toBe('string')
+  })
+
+  it("should format today's date correctly", () => {
+    const date = new Date()
+    const result = toAbsoluteDate(date, 'en')
+    expect(result).toContain('Today')
+  })
+
+  it("should format yesterday's date correctly", () => {
+    const date = new Date()
+    date.setDate(date.getDate() - 1)
+    const result = toAbsoluteDate(date, 'en')
+    expect(result).toContain('Yesterday')
+  })
+
+  it("should format this year's date correctly", () => {
+    // two days ago
+    const date = new Date()
+    date.setDate(date.getDate() - 2)
+
+    const thisYear = new Date().getFullYear()
+    const thisYearDate = new Date(`${thisYear}-01-01`)
+
+    // skip test
+    if (thisYearDate > date) {
+      return
+    }
+
+    const result = toAbsoluteDate(thisYearDate, 'en')
+    expect(result).toBe('Jan 1')
+  })
+
+  it('should format a date in the past correctly', () => {
+    const date = new Date('2021-01-01')
+    const result = toAbsoluteDate(date, 'en')
+    expect(result).toBe('Jan 1, 2021')
+  })
+
+  it('should format a date in the future correctly', () => {
+    const now = new Date()
+    const date = new Date(now.getTime() + 1000 * 60 * 60 * 24 * 365)
+    const result = toAbsoluteDate(date, 'en')
+    expect(result).toContain(date.getFullYear())
+  })
+
+  it('should format different types of dates correctly', () => {
+    // string
+    const stringDate = '2021-01-01'
+    const stringResult = toAbsoluteDate(stringDate, 'en')
+    expect(stringResult).toBe('Jan 1, 2021')
+
+    // number
+    const numberDate = 1609459200000
+    const numberResult = toAbsoluteDate(numberDate, 'en')
+    expect(numberResult).toBe('Jan 1, 2021')
+
+    // Date
+    const date = new Date('2021-01-01')
+    const dateResult = toAbsoluteDate(date, 'en')
+    expect(dateResult).toBe('Jan 1, 2021')
+  })
+})
---
+++ src/common/utils/datetime/relative.test.ts
@@ -0,0 +1,61 @@
+import { describe, expect, it } from 'vitest'
+
+import toRelativeDate from './relative'
+
+describe('datetime/relative', () => {
+  it('should parse a string date', () => {
+    const date = '2022-01-01'
+    const result = toRelativeDate(date, 'en')
+    expect(typeof result).toBe('string')
+  })
+
+  it('should format a date within this minute correctly', () => {
+    const date = new Date()
+    const result = toRelativeDate(date, 'en')
+    expect(result).toBe('just now')
+  })
+
+  it('should format a date within this hour but not this minute correctly', () => {
+    const now = new Date()
+    const date = new Date(now.getTime() - 1000 * 60)
+    const result = toRelativeDate(date, 'en')
+    expect(result).toBe('1 minute ago')
+  })
+
+  it('should format a date within today but not this hour correctly', () => {
+    const now = new Date()
+    const date = new Date(now.getTime() - 1000 * 60 * 60)
+    const result = toRelativeDate(date, 'en')
+    expect(result).toBe('1 hour ago')
+  })
+
+  it('should format a date within this week but not today correctly', () => {
+    const now = new Date()
+    const date = new Date(now.getTime() - 1000 * 60 * 60 * 24)
+    const result = toRelativeDate(date, 'en')
+    expect(result).toBe('1 day ago')
+  })
+
+  it('should format a date within this year but not this week correctly', () => {
+    const date = new Date('2021-01-01')
+    const result = toRelativeDate(date, 'en')
+    expect(result).toBe('Jan 1, 2021')
+  })
+
+  it('should format different types of dates correctly', () => {
+    // string
+    const stringDate = '2021-01-01'
+    const stringResult = toRelativeDate(stringDate, 'en')
+    expect(stringResult).toBe('Jan 1, 2021')
+
+    // number
+    const numberDate = 1609459200000
+    const numberResult = toRelativeDate(numberDate, 'en')
+    expect(numberResult).toBe('Jan 1, 2021')
+
+    // Date
+    const date = new Date('2021-01-01')
+    const dateResult = toRelativeDate(date, 'en')
+    expect(dateResult).toBe('Jan 1, 2021')
+  })
+})
