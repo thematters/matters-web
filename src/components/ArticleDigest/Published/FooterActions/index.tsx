ae8d9d2a57a1442c68dee7fb2873e7cd2d8f435f
bluecloud <96812901+pitb2022@users.noreply.github.com>
2023-11-02 14:16:44 +0800

    feat(ArticleDigest): revise published footer

--- src/components/ArticleDigest/Published/FooterActions/index.tsx
+++ src/components/ArticleDigest/Published/FooterActions/index.tsx
@@ -1,3 +1,11 @@
+import { numAbbr } from '~/common/utils'
+import { TextIcon } from '~/components'
+import {
+  IconClap2V16,
+  IconComment2V16,
+  IconMoney16,
+  IconUser2V16,
+} from '~/components/Icon'
 import { FooterActionsPublishedArticlePublicFragment } from '~/gql/graphql'
 
 import DropdownActions, { DropdownActionsControls } from '../../DropdownActions'
@@ -11,12 +19,34 @@ export type FooterActionsProps = {
 } & FooterActionsControls
 
 const FooterActions = ({ article, ...controls }: FooterActionsProps) => {
-  const {} = article
-  console.log({ article })
+  const {
+    readerCount,
+    appreciationsReceivedTotal,
+    commentCount,
+    donationCount,
+  } = article
 
   return (
     <footer className={styles.footer}>
-      <section className={styles.left}></section>
+      <section className={styles.left}>
+        <TextIcon icon={<IconUser2V16 />} spacing="basexxtight" size="sm">
+          <span className={styles.count}>{numAbbr(readerCount)}</span>
+        </TextIcon>
+
+        <TextIcon icon={<IconClap2V16 />} spacing="basexxtight" size="sm">
+          <span className={styles.count}>
+            {numAbbr(appreciationsReceivedTotal)}
+          </span>
+        </TextIcon>
+
+        <TextIcon icon={<IconComment2V16 />} spacing="basexxtight" size="sm">
+          <span className={styles.count}>{numAbbr(commentCount)}</span>
+        </TextIcon>
+
+        <TextIcon icon={<IconMoney16 />} spacing="basexxtight" size="sm">
+          <span className={styles.count}>{numAbbr(donationCount)}</span>
+        </TextIcon>
+      </section>
 
       <section className={styles.right}>
         <DropdownActions
---
+++ src/components/Icon/IconComment2V16.tsx
@@ -0,0 +1,5 @@
+import { ReactComponent as Icon } from '@/public/static/icons/16px/comment2.svg'
+
+import { withIcon } from './withIcon'
+
+export const IconComment2V16 = withIcon(Icon)
