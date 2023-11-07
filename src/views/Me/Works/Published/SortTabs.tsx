7cfca2932faba3599b3ffea86302f86a63b6f507
bluecloud <96812901+pitb2022@users.noreply.github.com>
2023-11-07 19:13:10 +0800

    fix(Works): revise SortTabs

--- src/views/Me/Works/Published/SortTabs.tsx
+++ src/views/Me/Works/Published/SortTabs.tsx
@@ -1,6 +1,8 @@
 import classNames from 'classnames'
-import { useIntl } from 'react-intl'
+import { useState } from 'react'
+import { FormattedMessage, useIntl } from 'react-intl'
 
+import { Button, IconClose20, IconUser2V16 } from '~/components'
 import { UserArticlesSort } from '~/gql/graphql'
 
 import styles from './styles.module.css'
@@ -38,54 +40,78 @@ export const SortTabs = ({
   setSort: React.Dispatch<React.SetStateAction<UserArticlesSort>>
 }) => {
   const intl = useIntl()
+  const [showHint, setShowHint] = useState(true)
   return (
-    <ul className={styles.tabList}>
-      <TabItem
-        active={sort === UserArticlesSort.Newest}
-        onClick={() => setSort(UserArticlesSort.Newest)}
-        title={intl.formatMessage({
-          defaultMessage: 'Latest',
-          id: 'SPTRiT',
-          description: 'src/views/Me/Works/Published/SortTabs.tsx',
-        })}
-      />
-      <TabItem
-        active={sort === UserArticlesSort.MostReaders}
-        onClick={() => setSort(UserArticlesSort.MostReaders)}
-        title={intl.formatMessage({
-          defaultMessage: 'Most readers',
-          id: 'rHi+cL',
-          description: 'src/views/Me/Works/Published/SortTabs.tsx',
-        })}
-      />
-      <TabItem
-        active={sort === UserArticlesSort.MostAppreciations}
-        onClick={() => setSort(UserArticlesSort.MostAppreciations)}
-        title={intl.formatMessage({
-          defaultMessage: 'Most claps',
-          id: 'AA5PkU',
-          description: 'src/views/Me/Works/Published/SortTabs.tsx',
-        })}
-      />
-      <TabItem
-        active={sort === UserArticlesSort.MostComments}
-        onClick={() => setSort(UserArticlesSort.MostComments)}
-        title={intl.formatMessage({
-          defaultMessage: 'Most comments',
-          id: 'aOFCqL',
-          description: 'src/views/Me/Works/Published/SortTabs.tsx',
-        })}
-      />
+    <>
+      <ul className={styles.tabList}>
+        <TabItem
+          active={sort === UserArticlesSort.Newest}
+          onClick={() => setSort(UserArticlesSort.Newest)}
+          title={intl.formatMessage({
+            defaultMessage: 'Latest',
+            id: 'SPTRiT',
+            description: 'src/views/Me/Works/Published/SortTabs.tsx',
+          })}
+        />
+        <TabItem
+          active={sort === UserArticlesSort.MostReaders}
+          onClick={() => setSort(UserArticlesSort.MostReaders)}
+          title={intl.formatMessage({
+            defaultMessage: 'Most readers',
+            id: 'rHi+cL',
+            description: 'src/views/Me/Works/Published/SortTabs.tsx',
+          })}
+        />
+        <TabItem
+          active={sort === UserArticlesSort.MostAppreciations}
+          onClick={() => setSort(UserArticlesSort.MostAppreciations)}
+          title={intl.formatMessage({
+            defaultMessage: 'Most claps',
+            id: 'AA5PkU',
+            description: 'src/views/Me/Works/Published/SortTabs.tsx',
+          })}
+        />
+        <TabItem
+          active={sort === UserArticlesSort.MostComments}
+          onClick={() => setSort(UserArticlesSort.MostComments)}
+          title={intl.formatMessage({
+            defaultMessage: 'Most comments',
+            id: 'aOFCqL',
+            description: 'src/views/Me/Works/Published/SortTabs.tsx',
+          })}
+        />
 
-      <TabItem
-        active={sort === UserArticlesSort.MostDonations}
-        onClick={() => setSort(UserArticlesSort.MostDonations)}
-        title={intl.formatMessage({
-          defaultMessage: 'Most Supporters',
-          id: 'n/w6lW',
-          description: 'src/views/Me/Works/Published/SortTabs.tsx',
-        })}
-      />
-    </ul>
+        <TabItem
+          active={sort === UserArticlesSort.MostDonations}
+          onClick={() => setSort(UserArticlesSort.MostDonations)}
+          title={intl.formatMessage({
+            defaultMessage: 'Most Supporters',
+            id: 'n/w6lW',
+            description: 'src/views/Me/Works/Published/SortTabs.tsx',
+          })}
+        />
+      </ul>
+      {showHint && (
+        <section className={styles.hint}>
+          <p className={styles.left}>
+            <IconUser2V16 color="grey" />
+            <FormattedMessage
+              defaultMessage="Number of readers: unique registered users plus number of anonymous IP addresses visited the article"
+              description="src/views/Me/Works/Published/index.tsx"
+              id="jaTUgx"
+            />
+          </p>
+          <section className={styles.right}>
+            <Button
+              textColor="greyDarker"
+              textActiveColor="black"
+              onClick={() => setShowHint(false)}
+            >
+              <IconClose20 size="mdS" />
+            </Button>
+          </section>
+        </section>
+      )}
+    </>
   )
 }
--- src/views/Me/Works/Published/index.tsx
+++ src/views/Me/Works/Published/index.tsx
@@ -5,11 +5,8 @@ import { FormattedMessage, useIntl } from 'react-intl'
 import { mergeConnections } from '~/common/utils'
 import {
   ArticleDigestPublished,
-  Button,
   EmptyArticle,
   Head,
-  IconClose20,
-  IconUser2V16,
   InfiniteScroll,
   Layout,
   List,
@@ -21,30 +18,31 @@ import Placeholder from '../Placeholder'
 import WorksTabs from '../WorksTabs'
 import { ME_WORKS_PUBLISHED_FEED } from './gql'
 import { SortTabs } from './SortTabs'
-import styles from './styles.module.css'
 
-export const BaseMeWorksPublished = ({
-  sort,
-  setShowSort,
-}: {
-  sort: UserArticlesSort
-  setShowSort: (state: boolean) => void
-}) => {
+export const BaseMeWorksPublished = () => {
+  const [sort, setSort] = useState<UserArticlesSort>(UserArticlesSort.Newest)
   const { data, loading, error, fetchMore } =
     useQuery<MeWorksPublishedFeedQuery>(ME_WORKS_PUBLISHED_FEED, {
       variables: { sort },
     })
 
+  const connectionPath = 'viewer.articles'
+  const { edges, pageInfo } = data?.viewer?.articles || {}
+
+  const isEmptyArticle = !edges || edges.length <= 0 || !pageInfo
+
   if (loading) {
-    return <Placeholder />
+    return (
+      <>
+        {!isEmptyArticle && <SortTabs sort={sort} setSort={setSort} />}
+        <Placeholder />
+      </>
+    )
   }
 
   if (error) {
     return <QueryError error={error} />
   }
-
-  const connectionPath = 'viewer.articles'
-  const { edges, pageInfo } = data?.viewer?.articles || {}
 
   if (!edges || edges.length <= 0 || !pageInfo) {
     return <EmptyArticle isMe />
@@ -65,24 +63,26 @@ export const BaseMeWorksPublished = ({
         }),
     })
 
-  setShowSort(true)
   return (
-    <Layout.Main.Spacing>
-      <InfiniteScroll
-        hasNextPage={pageInfo.hasNextPage}
-        loadMore={loadMore}
-        loader={<Placeholder />}
-        eof
-      >
-        <List>
-          {articleEdges.map(({ node, cursor }) => (
-            <List.Item key={cursor}>
-              <ArticleDigestPublished article={node} />
-            </List.Item>
-          ))}
-        </List>
-      </InfiniteScroll>
-    </Layout.Main.Spacing>
+    <>
+      <SortTabs sort={sort} setSort={setSort} />
+      <Layout.Main.Spacing>
+        <InfiniteScroll
+          hasNextPage={pageInfo.hasNextPage}
+          loadMore={loadMore}
+          loader={<Placeholder />}
+          eof
+        >
+          <List>
+            {articleEdges.map(({ node, cursor }) => (
+              <List.Item key={cursor}>
+                <ArticleDigestPublished article={node} />
+              </List.Item>
+            ))}
+          </List>
+        </InfiniteScroll>
+      </Layout.Main.Spacing>
+    </>
   )
 }
 
@@ -93,10 +93,6 @@ const MeWorksPublished = () => {
     description: 'src/views/Me/Works/Published/index.tsx',
     id: 'yBCdku',
   })
-
-  const [sort, setSort] = useState<UserArticlesSort>(UserArticlesSort.Newest)
-  const [showHint, setShowHint] = useState(true)
-  const [showSort, setShowSort] = useState(false)
 
   return (
     <Layout.Main>
@@ -112,39 +108,7 @@ const MeWorksPublished = () => {
 
       <WorksTabs />
 
-      {showSort && (
-        <>
-          <SortTabs sort={sort} setSort={setSort} />
-          {showHint && (
-            <section className={styles.hint}>
-              <p className={styles.left}>
-                <IconUser2V16 color="grey" />
-                <FormattedMessage
-                  defaultMessage="Number of readers: unique registered users plus number of anonymous IP addresses visited the article"
-                  description="src/views/Me/Works/Published/index.tsx"
-                  id="jaTUgx"
-                />
-              </p>
-              <section className={styles.right}>
-                <Button
-                  textColor="greyDarker"
-                  textActiveColor="black"
-                  onClick={() => setShowHint(false)}
-                >
-                  <IconClose20 size="mdS" />
-                </Button>
-              </section>
-            </section>
-          )}
-        </>
-      )}
-
-      <BaseMeWorksPublished
-        sort={sort}
-        setShowSort={(state) => {
-          setShowSort(state)
-        }}
-      />
+      <BaseMeWorksPublished />
     </Layout.Main>
   )
 }
