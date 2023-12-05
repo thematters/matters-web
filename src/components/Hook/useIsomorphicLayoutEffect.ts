d31ceb46dedc8dcb39c4d247fe70400de83f46be
bluecloud <96812901+pitb2022@users.noreply.github.com>
2023-12-05 16:00:47 +0800

    fix(Hook): add note

--- src/components/Hook/useIsomorphicLayoutEffect.ts
+++ src/components/Hook/useIsomorphicLayoutEffect.ts
@@ -1,4 +1,6 @@
 import { useEffect, useLayoutEffect } from 'react'
 
+// Beware of flickering problems when using useLayoutEffect on the server.
+// ref: https://gist.github.com/gaearon/e7d97cdf38a2907924ea12e4ebdf3c85?permalink_comment_id=4248531#gistcomment-4248531
 export const useIsomorphicLayoutEffect =
   typeof window !== undefined ? useLayoutEffect : useEffect
