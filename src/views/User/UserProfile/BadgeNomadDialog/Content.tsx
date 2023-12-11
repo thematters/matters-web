ee60166888fad7ed89cf4a35a6b129fd8a68fad1
49659410+tx0c <>
2023-12-11 04:32:10 +0000

    feat(nomad-badges): remove `LV?` prefix
    Implementation of Digital Nomad program
    
    for thematters/developer-resource#326

--- src/views/User/UserProfile/BadgeNomadDialog/Content.tsx
+++ src/views/User/UserProfile/BadgeNomadDialog/Content.tsx
@@ -71,21 +71,15 @@ const BadgeNomadDialogContent = ({
             <Dialog.Content.Message align="center" smUpAlign="center">
               <h1 className={styles.title}>
                 {nomadBadgeLevel === 4 ? (
-                  <FormattedMessage defaultMessage="LV4 Firebolt" id="FuU2MU" />
+                  <FormattedMessage defaultMessage="Firebolt" id="Rc4Oij" />
                 ) : nomadBadgeLevel === 3 ? (
-                  <FormattedMessage
-                    defaultMessage="LV3 Nimbus Ferry"
-                    id="sLiIAz"
-                  />
+                  <FormattedMessage defaultMessage="Nimbus Ferry" id="8MeJ4b" />
                 ) : nomadBadgeLevel === 2 ? (
-                  <FormattedMessage
-                    defaultMessage="LV2 Meteor Canoe"
-                    id="7ZXP9S"
-                  />
+                  <FormattedMessage defaultMessage="Meteor Canoe" id="TKsfIS" />
                 ) : (
                   <FormattedMessage
-                    defaultMessage="LV1 Moonlight Dream"
-                    id="+56XIp"
+                    defaultMessage="Moonlight Dream"
+                    id="76yoL6"
                   />
                 )}
               </h1>
@@ -94,45 +88,44 @@ const BadgeNomadDialogContent = ({
                   <FormattedMessage
                     defaultMessage="Breaking through history! The torch you've ignited will be the most dazzling support on the Nomad's journey. Congratulations on reaching the final destination of the Nomad Matters with 20 friends and earning the highest-level Firebolt badge."
                     id="p0oPHP"
-                    values={{
-                      br: <br />,
-                      // b: (chunks) => <b>{chunks}</b>,
-                    }}
                     description="src/views/User/UserProfile/BadgeNomadLabel/index.tsx"
                   />
                 ) : nomadBadgeLevel === 3 ? (
                   <FormattedMessage
                     defaultMessage="Wings have sprouted on the badge, and the halo of dreams is beginning to spin. You have gathered 10 companions on the Nomad's path. Invite another 10 fellow travelers to move towards the final destination together, where the highest level of honor awaits you."
                     id="DdVBFV"
-                    values={{
-                      br: <br />,
-                      // b: (chunks) => <b>{chunks}</b>,
-                    }}
                     description="src/views/User/UserProfile/BadgeNomadLabel/index.tsx"
                   />
                 ) : nomadBadgeLevel === 2 ? (
                   <FormattedMessage
                     defaultMessage="The dazzling light of a meteor shower is there to illuminate the night sky. Congratulations on having invited 5 companions to participate in Nomad Matters. Invite 5 more to level up, the more the merrier!"
                     id="qTjjF0"
-                    values={{
-                      br: <br />,
-                      // b: (chunks) => <b>{chunks}</b>,
-                    }}
                     description="src/views/User/UserProfile/BadgeNomadLabel/index.tsx"
                   />
                 ) : (
                   <FormattedMessage
-                    defaultMessage="Under the moonlight, your dreams are about to come true. The Moonlight Dream badge glorifies your initial participation in the Nomad Matters. To reach the next level, invite 5 friends to join the journey with you. <a>Click here in the last paragraph to find out how to invite new companions</a>"
-                    id="Z31Fz+"
-                    values={{
-                      br: <br />,
-                      a: (chunks) => (
-                        <a href={'/@hi176/464035-nomad-matters'}>{chunks}</a>
-                      ),
-                    }}
+                    defaultMessage="Under the moonlight, your dreams are about to come true. The Moonlight Dream badge glorifies your initial participation in the Nomad Matters. To reach the next level, invite 5 friends to join the journey with you."
+                    id="l+lNX/"
                     description="src/views/User/UserProfile/BadgeNomadLabel/index.tsx"
                   />
                 )}
+              </p>
+              <p>
+                <FormattedMessage
+                  defaultMessage="<a>Click here for the Nomad Matters and Badge Introduction.</a>"
+                  id="KXOkR3"
+                  values={{
+                    a: (chunks) => (
+                      <a
+                        href={
+                          process.env.NEXT_PUBLIC_NOMAD_MATTERS_CAMPAIGN_LINK
+                        }
+                      >
+                        {chunks}
+                      </a>
+                    ),
+                  }}
+                />
               </p>
             </Dialog.Content.Message>
           </section>
