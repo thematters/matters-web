ca7ef579dbc0cfb052327e499de7d91e973d22f5
bluecloud <96812901+pitb2022@users.noreply.github.com>
2024-01-05 16:01:56 +0800

    feat(AuthorSidebar): add <Tabs />

---
+++ src/views/ArticleDetail/AuthorSidebar/Tabs/index.tsx
@@ -0,0 +1,80 @@
+import classNames from 'classnames'
+import { useIntl } from 'react-intl'
+
+import { useRoute } from '~/components'
+import { ArticleDetailPublicQuery } from '~/gql/graphql'
+
+import styles from './styles.module.css'
+
+export type TABS = 'Collection' | 'Author' | 'Recommendation'
+
+type TabsProps = {
+  article: NonNullable<ArticleDetailPublicQuery['article']>
+  tab: TABS
+  setTab: React.Dispatch<React.SetStateAction<TABS>>
+}
+
+const TabItem = ({
+  active,
+  onClick,
+  title,
+}: {
+  title: string
+  active: boolean
+  onClick: () => void
+}) => {
+  const liClasses = classNames({
+    [styles.tabItem]: true,
+    [styles.active]: active,
+  })
+  return (
+    <li
+      className={liClasses}
+      role="button"
+      onClick={onClick}
+      data-title={title}
+    >
+      {title}
+    </li>
+  )
+}
+
+export const Tabs = ({ article, tab, setTab }: TabsProps) => {
+  const intl = useIntl()
+  const { getQuery } = useRoute()
+  const cid = getQuery('cid')
+
+  return (
+    <ul className={styles.tabList}>
+      {!!cid && (
+        <TabItem
+          active={tab === 'Collection'}
+          onClick={() => setTab('Collection')}
+          title={intl.formatMessage({
+            defaultMessage: 'Collection',
+            id: 'Hpmiif',
+            description: 'src/views/ArticleDetail/AuthorSidebar/Tabs/index.tsx',
+          })}
+        />
+      )}
+      <TabItem
+        active={tab === 'Author'}
+        onClick={() => setTab('Author')}
+        title={intl.formatMessage({
+          defaultMessage: 'Author',
+          id: 'RM17b4',
+          description: 'src/views/ArticleDetail/AuthorSidebar/Tabs/index.tsx',
+        })}
+      />
+      <TabItem
+        active={tab === 'Recommendation'}
+        onClick={() => setTab('Recommendation')}
+        title={intl.formatMessage({
+          defaultMessage: 'More',
+          id: 'VqdOGQ',
+          description: 'src/views/ArticleDetail/AuthorSidebar/Tabs/index.tsx',
+        })}
+      />
+    </ul>
+  )
+}
--- src/views/ArticleDetail/AuthorSidebar/index.tsx
+++ src/views/ArticleDetail/AuthorSidebar/index.tsx
@@ -1,17 +1,26 @@
+import { useState } from 'react'
+
+import { useRoute } from '~/components'
 import { ArticleDetailPublicQuery } from '~/gql/graphql'
 
 import Author from './Author'
 import { fragments } from './gql'
 import { Placeholder } from './Placeholder'
+import { TABS, Tabs } from './Tabs'
 
 type AuthorSidebarProps = {
   article: NonNullable<ArticleDetailPublicQuery['article']>
 }
 
 export const AuthorSidebar = ({ article }: AuthorSidebarProps) => {
+  const { getQuery } = useRoute()
+  const cid = getQuery('cid')
+  const [tab, setTab] = useState<TABS>(!!cid ? 'Collection' : 'Author')
+
   return (
     <>
       <Author article={article} />
+      <Tabs article={article} tab={tab} setTab={setTab} />
     </>
   )
 }
