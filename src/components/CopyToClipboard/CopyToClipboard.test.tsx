f0650934c3abc3e258827be4218844d99617e4cb
bluecloud <96812901+pitb2022@users.noreply.github.com>
2024-02-29 15:30:48 +0800

    fix(CopyToClipboard): setup unit test

--- src/components/CopyToClipboard/CopyToClipboard.test.tsx
+++ src/components/CopyToClipboard/CopyToClipboard.test.tsx
@@ -1,12 +1,21 @@
+import userEvent from '@testing-library/user-event'
 import { describe, expect, it } from 'vitest'
 
 import { render, screen } from '~/common/utils/test'
 import { CopyToClipboard } from '~/components'
 
+// setup function
+function setup(ui: React.ReactElement) {
+  return {
+    user: userEvent.setup(),
+    ...render(ui),
+  }
+}
+
 describe('<CopyToClipboard>', () => {
   it('should render CopyToClipboard', async () => {
     const textToCopy = 'text to copy'
-    render(
+    const { user } = setup(
       <CopyToClipboard text={textToCopy}>
         {({ copyToClipboard }) => (
           <button type="button" onClick={copyToClipboard}>
@@ -19,7 +28,7 @@ describe('<CopyToClipboard>', () => {
     const $button = screen.getByRole('button', { name: 'Copy' })
     expect($button).toBeInTheDocument()
 
-    $button.click()
+    await user.click($button)
 
     // FIXME: not support clipboard in jsdom yet
     const $toast = screen.getByRole('alert')
