8cdefc62072b3617d89082cd6cdad84b4bcc558e
bluecloud <96812901+pitb2022@users.noreply.github.com>
2024-03-12 19:59:07 +0800

    fix(Drawer): fix unit test

--- src/components/Drawer/Drawer.test.tsx
+++ src/components/Drawer/Drawer.test.tsx
@@ -6,9 +6,10 @@ import { Drawer, useDialogSwitch } from '~/components'
 type AnyDrawerProps = {
   children: React.ReactNode
   defaultOpen: boolean
+  title: string
 }
 
-const AnyDrawer = ({ defaultOpen, children }: AnyDrawerProps) => {
+const AnyDrawer = ({ defaultOpen, title, children }: AnyDrawerProps) => {
   const { show, openDialog, closeDialog } = useDialogSwitch(defaultOpen)
 
   return (
@@ -17,6 +18,7 @@ const AnyDrawer = ({ defaultOpen, children }: AnyDrawerProps) => {
         Open
       </button>
       <Drawer isOpen={show} onClose={closeDialog}>
+        <Drawer.Header title={title} closeDrawer={closeDialog} />
         {children}
       </Drawer>
     </>
@@ -25,25 +27,26 @@ const AnyDrawer = ({ defaultOpen, children }: AnyDrawerProps) => {
 
 describe('<Drawer>', () => {
   it('should render, open and close a Drawer', async () => {
+    const title = 'drawer title'
+
     render(
-      <AnyDrawer defaultOpen={false}>
-        <Drawer.Header title="drawer title" />
+      <AnyDrawer defaultOpen={false} title={title}>
         <Drawer.Content>
           <section>hello drawer</section>
         </Drawer.Content>
       </AnyDrawer>
     )
 
-    expect(screen.queryByText('drawer title')).not.toBeInTheDocument()
+    expect(screen.queryByText(title)).not.toBeInTheDocument()
 
     // open drawer
     fireEvent.click(screen.getByRole('button', { name: 'Open' }))
-    expect(screen.getByText('drawer title')).toBeInTheDocument()
+    expect(screen.getByText(title)).toBeInTheDocument()
 
     // close drawer
     fireEvent.click(screen.getByRole('button', { name: 'Close' }))
     await waitFor(() => {
-      expect(screen.queryByText('drawer title')).not.toBeInTheDocument()
+      expect(screen.queryByText(title)).not.toBeInTheDocument()
     })
   })
 })
