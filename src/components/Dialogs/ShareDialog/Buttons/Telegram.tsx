5edfd90216ce3b3d4ffd0d71d892f1cef2197510
bluecloud <96812901+pitb2022@users.noreply.github.com>
2024-03-22 15:54:51 +0800

    feat(ShareDialog): revise share buttons
    ref:
    2024-03-product#2-PL-015

--- src/components/Dialogs/ShareDialog/Buttons/Telegram.tsx
+++ src/components/Dialogs/ShareDialog/Buttons/Telegram.tsx
@@ -1,7 +1,7 @@
 import { useContext } from 'react'
 
 import { ReactComponent as IconShareTelegram } from '@/public/static/icons/16px/share-telegram.svg'
-import { ReactComponent as IconShareTelegramCircle } from '@/public/static/icons/40px/share-telegram-circle.svg'
+import { ReactComponent as IconShareTelegramCircle } from '@/public/static/icons/24px/share-telegram-circle.svg'
 import { REFERRAL_QUERY_REFERRAL_KEY } from '~/common/enums'
 import { analytics } from '~/common/utils'
 import { TextIcon, ViewerContext, withIcon } from '~/components'
@@ -40,7 +40,16 @@ const Telegram = ({
         return window.open(shareUrl, 'Share to Telegram')
       }}
     >
-      {circle && withIcon(IconShareTelegramCircle)({ size: 'xlM' })}
+      {circle && (
+        <TextIcon
+          icon={withIcon(IconShareTelegramCircle)({ size: 'md' })}
+          spacing="base"
+          size="md"
+          color="black"
+        >
+          Telegram
+        </TextIcon>
+      )}
 
       {!circle && (
         <TextIcon icon={withIcon(IconShareTelegram)({})} spacing="base">
