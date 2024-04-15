cccfa9ced197c4befa3a5e576ab27f81be7948aa
bluecloud <96812901+pitb2022@users.noreply.github.com>
2024-04-15 16:22:23 +0800

    fix(ArticleDetail): fix copy
    ref:
    2024-03-product#2-ZN-022

--- src/views/ArticleDetail/License/index.tsx
+++ src/views/ArticleDetail/License/index.tsx
@@ -18,32 +18,32 @@ const License: React.FC<LicenseProps> = ({ license }) => {
     <section className={styles.license} data-test-id={TEST_ID.ARTICLE_LICENSE}>
       {isCC0 && (
         <Translate
-          zh_hant="NO RIGHTS RESERVED 授權聲明"
-          zh_hans="NO RIGHTS RESERVED 授权声明"
+          zh_hant="NO RIGHTS RESERVED 授權"
+          zh_hans="NO RIGHTS RESERVED 授权"
           en="NO RIGHTS RESERVED"
         />
       )}
 
       {isARR && (
         <Translate
-          zh_hant="ALL RIGHTS RESERVED 授權聲明"
-          zh_hans="ALL RIGHTS RESERVED 授权声明"
+          zh_hant="ALL RIGHTS RESERVED 授權"
+          zh_hans="ALL RIGHTS RESERVED 授权"
           en="ALL RIGHTS RESERVED"
         />
       )}
 
       {isCCBYNCND2 && (
         <Translate
-          zh_hant="CC BY-NC-ND 2.0 授權聲明"
-          zh_hans="CC BY-NC-ND 2.0 授权声明"
+          zh_hant="CC BY-NC-ND 2.0 授權"
+          zh_hans="CC BY-NC-ND 2.0 授权"
           en="CC BY-NC-ND 2.0"
         />
       )}
 
       {isCCBYNCND4 && (
         <Translate
-          zh_hant="CC BY-NC-ND 4.0 授權聲明"
-          zh_hans="CC BY-NC-ND 4.0 授权声明"
+          zh_hant="CC BY-NC-ND 4.0 授權"
+          zh_hans="CC BY-NC-ND 4.0 授权"
           en="CC BY-NC-ND 4.0"
         />
       )}
