da733462902cb12a3097b2a1bea5380ab2330f8e
robertu <4065233+robertu7@users.noreply.github.com>
2024-04-22 17:11:18 +0800

    feat(home): add <IcymiCreatedFeed> to featured feed

--- src/views/Home/Feed/IcymiCuratedFeed/index.tsx
+++ src/views/Home/Feed/IcymiCuratedFeed/index.tsx
@@ -1,6 +1,11 @@
+import React from 'react'
+
+import { analytics } from '~/common/utils'
+import { ArticleDigestCurated, Media } from '~/components'
 import { IcymiCuratedFeedRecommendationFragment } from '~/gql/graphql'
 
 import { fragments } from './gql'
+import styles from './styles.module.css'
 
 type IcymiCuratedFeed = {
   recommendation: IcymiCuratedFeedRecommendationFragment
@@ -11,13 +16,63 @@ export const IcymiCuratedFeed = ({ recommendation }: IcymiCuratedFeed) => {
     return null
   }
 
-  // const { title, articles, pinAmount, note } = recommendation.icymiTopic
+  const { articles, pinAmount, note } = recommendation.icymiTopic
+
+  const cardArticles = articles.slice(0, pinAmount)
+  // const listArticles = articles.slice(pinAmount)
 
   return (
-    <section>
-      {/* note */}
-      {/* article cards */}
-      {/* article feed */}
+    <section className={styles.container}>
+      {note && (
+        <section className={styles.description}>
+          <span aria-hidden>/</span>
+          <p>{note}</p>
+          <span aria-hidden>/</span>
+        </section>
+      )}
+
+      <section className={styles.cards}>
+        {cardArticles.map((article, i) => {
+          const cardProps = {
+            article,
+            onClick: () => {
+              analytics.trackEvent('click_feed', {
+                type: 'icymi_curated',
+                contentType: 'article',
+                location: i,
+                id: article.id,
+              })
+            },
+            onClickAuthor: () => {
+              analytics.trackEvent('click_feed', {
+                type: 'icymi_curated',
+                contentType: 'user',
+                location: i,
+                id: article.author.id,
+              })
+            },
+          }
+
+          return (
+            <React.Fragment key={article.id}>
+              <Media at="sm">
+                <ArticleDigestCurated {...cardProps} titleLineClamp={3} />
+              </Media>
+              <Media greaterThan="sm">
+                <ArticleDigestCurated {...cardProps} titleLineClamp={2} />
+              </Media>
+            </React.Fragment>
+          )
+        })}
+      </section>
+
+      {/* {listArticles.length > 0 && (
+        <section className={styles.list}>
+          {listArticles.map((article) => (
+            <ArticleDigestFeed article={article} key={article.id} />
+          ))}
+        </section>
+      )} */}
     </section>
   )
 }
