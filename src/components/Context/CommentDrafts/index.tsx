9dbb6b88875fe2e83ba6d8ef196646d7c2b40fb0
bluecloud <96812901+pitb2022@users.noreply.github.com>
2024-04-28 17:00:08 +0800

    feat(CommentDrafts): Update CommentDrafts
    1. add beforeunload event
    2. revise handleRouteChange function

--- src/components/Context/CommentDrafts/index.tsx
+++ src/components/Context/CommentDrafts/index.tsx
@@ -1,5 +1,6 @@
 import { useApolloClient } from '@apollo/react-hooks'
 import { createContext, ReactNode, useEffect, useState } from 'react'
+import { useIntl } from 'react-intl'
 
 import COMMENT_DRAFT from '~/components/GQL/queries/commentDraft'
 import { useRoute } from '~/components/Hook'
@@ -19,6 +20,7 @@ export const CommentDraftsProvider = ({
 }: {
   children: ReactNode
 }) => {
+  const intl = useIntl()
   const [drafts] = useState<draft[]>([])
   const addDraft = (draft: draft) => {
     if (drafts.indexOf(draft) !== -1) {
@@ -37,51 +39,92 @@ export const CommentDraftsProvider = ({
   const { router } = useRoute()
   const client = useApolloClient()
 
+  const hint = intl.formatMessage({
+    // FIXME: fix i18n
+    defaultMessage: '評論尚未發布，確定放棄內容離開此頁嗎？',
+    id: '3rqn2V',
+  })
+
   useEffect(() => {
-    const confirm = (url: string) => {
-      router.events.off('beforeHistoryChange', handleRouteChange)
+    const addEventListeners = () => {
+      window.addEventListener('beforeunload', handleBeforeUnload)
+      router.events.on('routeChangeStart', handleRouteChange)
+    }
+
+    const removeEventListeners = () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload)
+      router.events.off('routeChangeStart', handleRouteChange)
+    }
+
+    const navigateTo = (url: string) => {
+      removeEventListeners()
       router.push(url)
     }
 
-    const confirmLeave = (url: string) => {
-      if (window.confirm('評論尚未發布，確定放棄內容離開此頁嗎？')) {
-        confirm(url)
-        return true
+    const confirmNavigation = async (url: string) => {
+      if (window.confirm(hint)) {
+        navigateTo(url)
+      } else {
+        router.events.emit('routeChangeError')
       }
-      router.events.emit('routeChangeError')
+    }
+
+    const isDraftEmpty = (content: string) => {
+      return content === '' || content === '<p></p>'
+    }
+
+    const queryAllDrafts = async () => {
+      return await Promise.all(
+        drafts.map((draft) => {
+          return client.query({
+            query: COMMENT_DRAFT,
+            variables: { id: draft },
+          })
+        })
+      )
+    }
+
+    const checkAllDraftsEmpty = async () => {
+      const datas = await queryAllDrafts()
+      return datas.every(({ data }) => isDraftEmpty(data?.commentDraft.content))
+    }
+
+    const handleBeforeUnload = async (event: BeforeUnloadEvent) => {
+      if (drafts.length === 0) {
+        return
+      }
+
+      const allEmptyDraft = await checkAllDraftsEmpty()
+      if (!allEmptyDraft) {
+        event.returnValue = hint
+        return hint
+      }
     }
 
     const handleRouteChange = (url: string) => {
-      const fn = async (index: number) => {
-        if (index >= drafts.length) {
-          confirm(url)
+      const processRouteChange = async () => {
+        if (drafts.length === 0) {
+          navigateTo(url)
           return
         }
 
-        const { data } = await client.query({
-          query: COMMENT_DRAFT,
-          variables: { id: drafts[index] },
-        })
-
-        console.log({ data })
-        if (
-          data?.commentDraft.content !== '' &&
-          data?.commentDraft.content !== '<p></p>'
-        ) {
-          return confirmLeave(url)
+        const allEmptyDraft = await checkAllDraftsEmpty()
+        if (allEmptyDraft) {
+          navigateTo(url)
+        } else {
+          confirmNavigation(url)
         }
-        fn(index + 1)
       }
-      fn(0)
+
+      processRouteChange()
+      // Immediately throw an error to abort the route change process in synchronous function
       throw 'Abort route change. Please ignore this error.'
     }
 
-    router.events.on('beforeHistoryChange', handleRouteChange)
+    addEventListeners()
 
-    return () => {
-      router.events.off('beforeHistoryChange', handleRouteChange)
-    }
-  }, [])
+    return removeEventListeners
+  }, [drafts])
 
   return (
     <CommentDraftsContext.Provider value={{ drafts, addDraft, removeDraft }}>
