717be57db4524d2a45d159c5455f011f040b43bc
robertu <4065233+robertu7@users.noreply.github.com>
2024-05-03 13:04:02 +0800

    feat: reuse <Label>
    refs:
    - 2024-05-dev-1#RB-003

--- src/components/CommentBeta/RoleLabel/index.tsx
+++ src/components/CommentBeta/RoleLabel/index.tsx
@@ -1,9 +1,8 @@
 import gql from 'graphql-tag'
 import { FormattedMessage } from 'react-intl'
 
+import { Label } from '~/components/Label'
 import { RoleLabelCommentFragment } from '~/gql/graphql'
-
-import styles from './styles.module.css'
 
 const fragments = {
   comment: gql`
@@ -25,33 +24,31 @@ const fragments = {
   `,
 }
 
-// TODO: reuse <Label> component
-// https://github.com/thematters/matters-web/pull/4192#discussion_r1495549659
 const RoleLabel = ({ comment }: { comment: RoleLabelCommentFragment }) => {
   if (
     comment.node.__typename === 'Article' &&
     comment.author.id === comment.node.author.id
   ) {
     return (
-      <span className={`${styles.label} ${styles.authorLabel}`}>
+      <Label color="green">
         <FormattedMessage
           defaultMessage="Author"
           id="78qsg6"
           description="src/components/Comment/RoleLabel/index.tsx"
         />
-      </span>
+      </Label>
     )
   }
 
   if (comment.fromDonator) {
     return (
-      <span className={`${styles.label} ${styles.supporterLabel}`}>
+      <Label color="gold">
         <FormattedMessage
           defaultMessage="Supporter"
           id="jc3mW/"
           description="src/components/Comment/RoleLabel/index.tsx"
         />
-      </span>
+      </Label>
     )
   }
 
