8260c8fe4864ba9bb42ddead290da20085a5ff4c
bluecloud <96812901+pitb2022@users.noreply.github.com>
2024-05-09 14:37:50 +0800

    fix(ArticleDetail): retire Translate
    ref:
    production-pre-test#PR-008

--- src/views/ArticleDetail/License/index.tsx
+++ src/views/ArticleDetail/License/index.tsx
@@ -1,5 +1,6 @@
+import { FormattedMessage } from 'react-intl'
+
 import { TEST_ID } from '~/common/enums'
-import { Translate } from '~/components'
 import { ArticleLicenseType } from '~/gql/graphql'
 
 import styles from './styles.module.css'
@@ -16,36 +17,16 @@ const License: React.FC<LicenseProps> = ({ license }) => {
 
   return (
     <section className={styles.license} data-test-id={TEST_ID.ARTICLE_LICENSE}>
-      {isCC0 && (
-        <Translate
-          zh_hant="CC0 公眾領域貢獻宣告"
-          zh_hans="CC0 公众领域贡献宣告"
-          en="CC0 License"
-        />
-      )}
+      {isCC0 && <FormattedMessage defaultMessage="CC0 License" id="rE7sE3" />}
 
-      {isARR && (
-        <Translate
-          zh_hant="作者保留所有權利"
-          zh_hans="作者保留所有權利"
-          en="No Copyright"
-        />
-      )}
+      {isARR && <FormattedMessage defaultMessage="No Copyright" id="pKRF+E" />}
 
       {isCCBYNCND2 && (
-        <Translate
-          zh_hant="CC BY-NC-ND 2.0 授權"
-          zh_hans="CC BY-NC-ND 2.0 授权"
-          en="CC BY-NC-ND 2.0"
-        />
+        <FormattedMessage defaultMessage="CC BY-NC-ND 2.0" id="A0MNSc" />
       )}
 
       {isCCBYNCND4 && (
-        <Translate
-          zh_hant="CC BY-NC-ND 4.0 授權"
-          zh_hans="CC BY-NC-ND 4.0 授权"
-          en="CC BY-NC-ND 4.0"
-        />
+        <FormattedMessage defaultMessage="CC BY-NC-ND 4.0" id="TPEQMa" />
       )}
     </section>
   )
