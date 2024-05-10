219c135f086e1a26346cdc330ab5c4011d5e7b13
robertu <4065233+robertu7@users.noreply.github.com>
2024-05-10 12:27:01 +0800

    fix(article): revise info header on article history page
    refs:
    - 2024-05-prod-1#ZN-002
    - PR-049

--- src/views/ArticleDetail/History/Versions/VersionsSidebar/index.tsx
+++ src/views/ArticleDetail/History/Versions/VersionsSidebar/index.tsx
@@ -15,7 +15,7 @@ const VersionsSidebar = ({ article }: { article: VersionsArticleFragment }) => {
   const versions = article.versions.edges.map((edge) => edge?.node!)
   const currVersion = getQuery('v') || versions[0]?.id
 
-  if (versions.length < 1) {
+  if (versions.length <= 1) {
     return null
   }
 
