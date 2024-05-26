81a91249c40e7c94c9da549e1eab46290cc6d886
robertu <4065233+robertu7@users.noreply.github.com>
2024-05-26 15:05:52 +0800

    fix(ipfs): skip pinging ipfs gateways on SSR and remove pinata gateway

--- src/common/utils/resolvers/gatewayUrls.ts
+++ src/common/utils/resolvers/gatewayUrls.ts
@@ -2,7 +2,6 @@ const TEST_HASH = 'Qmaisz6NMhDB51cCvNWa1GMS7LU1pAxdF4Ld6Ft9kZEP2a'
 const PUBLIC_GATEWAYS: string[] = [
   'https://gateway.ipfs.io/ipfs/:hash',
   'https://cloudflare-ipfs.com/ipfs/:hash',
-  'https://gateway.pinata.cloud/ipfs/:hash',
   'https://ipfs.io/ipfs/:hash',
   'https://cf-ipfs.com/ipfs/:hash',
   'https://4everland.io/ipfs/:hash',
