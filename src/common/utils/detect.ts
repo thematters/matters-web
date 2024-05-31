962c542fdc19ea8ee2aaf6fad318bdff1611edec
bluecloud <96812901+pitb2022@users.noreply.github.com>
2024-05-31 15:29:59 +0800

    fix(detect): revise RegExp to match Safari

--- src/common/utils/detect.ts
+++ src/common/utils/detect.ts
@@ -14,14 +14,19 @@ export const isMobile = () => {
   )
 }
 
+const getSafariVersion = (userAgent: string): string | null => {
+  const userAgentRegex =
+    /^Mozilla\/5\.0 \(Macintosh; (Intel|Apple) Mac OS X \d+_\d+(?:_\d+)?\) AppleWebKit\/\d+(?:\.\d+)* \(KHTML, like Gecko\) Version\/(\d+\.\d+(?:\.\d+)*) Safari\/\d+(?:\.\d+)*$/
+  const match = userAgent.match(userAgentRegex)
+  return match ? match[2] : null // The capturing group for the version number
+}
+
 export const checkIsSafariVersionLessThan17 = () => {
   const userAgent = navigator.userAgent
-  const safariMatch = userAgent.match(/Version\/(\d+\.\d+)/)
-
-  if (safariMatch && safariMatch[1]) {
-    const safariVersion = parseFloat(safariMatch[1])
-    return safariVersion < 17
+  const version = getSafariVersion(userAgent)
+  if (version) {
+    const majorVersion = parseInt(version.split('.')[0], 10)
+    return majorVersion < 17
   }
-
   return false
 }
