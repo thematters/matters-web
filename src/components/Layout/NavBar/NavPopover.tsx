7a9c90323e27036021cbc49b8a7a5d2a3c6c77c7
byhow <by.how@hotmail.com>
2024-06-11 01:03:30 -0700

    feat: revise to the latest designs

--- src/components/Layout/NavBar/NavPopover.tsx
+++ src/components/Layout/NavBar/NavPopover.tsx
@@ -1,7 +1,7 @@
 import { FormattedMessage } from 'react-intl'
 
+import { ReactComponent as IconComment } from '@/public/static/icons/24px/comment.svg'
 import { ReactComponent as IconEdit } from '@/public/static/icons/24px/edit.svg'
-import { ReactComponent as IconStory } from '@/public/static/icons/24px/story.svg'
 import { PATHS } from '~/common/enums'
 import { Card } from '~/components'
 import { Icon } from '~/components/Icon'
@@ -15,42 +15,42 @@ const NavPopover: React.FC = () => {
       <ul className={styles.list}>
         <li key="new-journal">
           <Card
-            spacing={[8, 16]}
-            textColor="greyDarker"
+            spacing={[12, 24]}
+            textColor="black"
             textActiveColor="black"
             bgColor="transparent"
-            borderRadius="xxtight"
+            borderRadius="base"
             role="menuitem"
           >
             <TextIcon
-              icon={<Icon icon={IconStory} size={32} />}
-              size={12}
+              icon={<Icon icon={IconComment} size={32} />}
+              size={14}
               spacing={12}
               weight="normal"
               placement="bottom"
             >
-              <FormattedMessage defaultMessage="New Journal" id="subQNF" />
+              <FormattedMessage defaultMessage="發動態" id="pcuiJf" />
             </TextIcon>
           </Card>
         </li>
         <li key="write-articles">
           <Card
-            spacing={[8, 16]}
-            textColor="greyDarker"
+            spacing={[12, 24]}
+            textColor="black"
             textActiveColor="black"
             bgColor="transparent"
-            borderRadius="xxtight"
+            borderRadius="base"
             role="menuitem"
             href={PATHS.ME_DRAFT_NEW}
           >
             <TextIcon
               icon={<Icon icon={IconEdit} size={32} />}
-              size={12}
+              size={14}
               spacing={12}
               weight="normal"
               placement="bottom"
             >
-              <FormattedMessage defaultMessage="Write Articles" id="Xj0yv9" />
+              <FormattedMessage defaultMessage="寫文章" id="aIs0hF" />
             </TextIcon>
           </Card>
         </li>
