82e1b43c6cec32859de0269b104fe6ef49eda334
bluecloud <96812901+pitb2022@users.noreply.github.com>
2024-06-13 19:37:57 +0800

    use editor.view.dom.focus()

--- src/views/JournalDetail/Edit/index.tsx
+++ src/views/JournalDetail/Edit/index.tsx
@@ -6,29 +6,29 @@ import JournalEditor from '~/components/Editor/Journal'
 
 import styles from './styles.module.css'
 
-function triggerTouchEvent(element: HTMLElement, eventType: string) {
-  const touchObj = new Touch({
-    identifier: Date.now(),
-    target: element,
-    clientX: 230, // 设置触摸点的x坐标
-    clientY: 230, // 设置触摸点的y坐标
-    radiusX: 2.5,
-    radiusY: 2.5,
-    rotationAngle: 10,
-    force: 1,
-  })
+// function triggerTouchEvent(element: HTMLElement, eventType: string) {
+//   const touchObj = new Touch({
+//     identifier: Date.now(),
+//     target: element,
+//     clientX: 230, // 设置触摸点的x坐标
+//     clientY: 230, // 设置触摸点的y坐标
+//     radiusX: 2.5,
+//     radiusY: 2.5,
+//     rotationAngle: 10,
+//     force: 1,
+//   })
 
-  const touchEvent = new TouchEvent(eventType, {
-    cancelable: true,
-    bubbles: true,
-    touches: [touchObj],
-    targetTouches: [],
-    changedTouches: [touchObj],
-    shiftKey: true, // 其他事件属性可以根据需要设置
-  })
+//   const touchEvent = new TouchEvent(eventType, {
+//     cancelable: true,
+//     bubbles: true,
+//     touches: [touchObj],
+//     targetTouches: [],
+//     changedTouches: [touchObj],
+//     shiftKey: true, // 其他事件属性可以根据需要设置
+//   })
 
-  element.dispatchEvent(touchEvent)
-}
+//   element.dispatchEvent(touchEvent)
+// }
 
 const Edit = () => {
   const intl = useIntl()
@@ -45,16 +45,17 @@ const Edit = () => {
   useEffect(() => {
     if (editor) {
       setTimeout(() => {
-        editor.chain().focus().insertContent('test touch event').run()
-        editor.commands.focus()
+        // editor.chain().focus().insertContent('test touch event').run()
+        // editor.commands.focus()
 
-        console.log({ editor })
-        const editorElement = editor.view.dom as HTMLElement
-        editorElement.focus()
-        console.log({ editorElement })
-        triggerTouchEvent(editorElement, 'touchstart')
-        triggerTouchEvent(editorElement, 'touchend')
-      }, 5 * 1000)
+        // console.log({ editor })
+        // const editorElement = editor.view.dom as HTMLElement
+        // editorElement.focus()
+        // console.log({ editorElement })
+        // triggerTouchEvent(editorElement, 'touchstart')
+        // triggerTouchEvent(editorElement, 'touchend')
+        editor.view.dom.focus()
+      }, 1 * 1000)
     }
   }, [editor])
 
