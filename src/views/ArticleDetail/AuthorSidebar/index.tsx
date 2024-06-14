aef7908d8734e10e0fc80fd6434bcedb9261c99f
robertu <4065233+robertu7@users.noreply.github.com>
2024-06-14 10:19:36 +0800

    feat(article): lazy load sidebar related articles & collection

--- src/views/ArticleDetail/AuthorSidebar/index.tsx
+++ src/views/ArticleDetail/AuthorSidebar/index.tsx
@@ -1,10 +1,10 @@
+import dynamic from 'next/dynamic'
 import { useState } from 'react'
 
-import { useRoute } from '~/components'
+import { SpinnerBlock, useRoute } from '~/components'
 import { ArticleDetailPublicQuery } from '~/gql/graphql'
 
 import Author from './Author'
-import { Collection } from './Collection'
 import { FromAuthor } from './FromAuthor'
 import { fragments } from './gql'
 import { Placeholder } from './Placeholder'
@@ -16,16 +16,21 @@ type AuthorSidebarProps = {
   article: NonNullable<ArticleDetailPublicQuery['article']>
 }
 
+const DynamicCollection = dynamic(() => import('./Collection'), {
+  ssr: false,
+  loading: () => <SpinnerBlock />,
+})
+
 export const AuthorSidebar = ({ article }: AuthorSidebarProps) => {
   const { getQuery } = useRoute()
-  const cid = getQuery('collection')
+  const collectionId = getQuery('collection')
   const latestWorks = article.author?.latestWorks.filter((work) => {
     return work.id !== article.id
   })
   const hasFromAuthor = latestWorks && latestWorks.length > 0
   const hasRecommendation = article.relatedArticles?.totalCount > 0
   const [tab, setTab] = useState<TABS>(
-    !!cid
+    !!collectionId
       ? 'Collection'
       : hasFromAuthor
       ? 'Author'
@@ -41,8 +46,11 @@ export const AuthorSidebar = ({ article }: AuthorSidebarProps) => {
         <>
           <Tabs article={article} tab={tab} setTab={setTab} />
           <section className={styles.list}>
-            {!!cid && tab === 'Collection' && (
-              <Collection article={article} collectionId={cid} />
+            {!!collectionId && tab === 'Collection' && (
+              <DynamicCollection
+                article={article}
+                collectionId={collectionId}
+              />
             )}
             {tab === 'Author' && <FromAuthor article={article} />}
             {tab === 'Recommendation' && <RelatedArticles article={article} />}
