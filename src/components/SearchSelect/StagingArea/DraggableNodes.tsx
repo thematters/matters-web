dfe4eaa640d94da0edb98e8e7e2af15dee80e603
byhow <by.how@hotmail.com>
2024-06-15 13:58:09 -0700

    refactor(types): additional component related typing

--- src/components/SearchSelect/StagingArea/DraggableNodes.tsx
+++ src/components/SearchSelect/StagingArea/DraggableNodes.tsx
@@ -24,7 +24,7 @@ interface DraggableNodesProps {
   toggleSelectNode: (node: SelectNode) => void
 }
 
-const reorder = (list: any[], startIndex: number, endIndex: number) => {
+const reorder = (list: StagingNode[], startIndex: number, endIndex: number) => {
   const result = Array.from(list)
   const [removed] = result.splice(startIndex, 1)
   result.splice(endIndex, 0, removed)
