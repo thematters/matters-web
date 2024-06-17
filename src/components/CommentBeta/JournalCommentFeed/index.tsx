2029ec8128c514ef96a7abac4d9e091bf46c3217
bluecloud <96812901+pitb2022@users.noreply.github.com>
2024-06-17 22:58:39 +0800

    fix(JournalCommentFeed): fix build error

--- src/components/CommentBeta/JournalCommentFeed/index.tsx
+++ src/components/CommentBeta/JournalCommentFeed/index.tsx
@@ -9,57 +9,53 @@ import {
   CommentFormType,
   DateTime,
   LinkWrapper,
-  ThreadCommentType,
 } from '~/components'
 import {
-  FeedCommentBetaPrivateFragment,
-  FeedCommentBetaPublicFragment,
+  JournalCommentFeedCommentPrivateFragment,
+  JournalCommentFeedCommentPublicFragment,
 } from '~/gql/graphql'
 
 import Content from '../Content'
 import DropdownActions, { DropdownActionsControls } from '../DropdownActions'
-import FooterActions, { FooterActionsControls } from '../FooterActions'
-import PinnedLabel from '../PinnedLabel'
+import { FooterActionsControls } from '../FooterActions'
 import RoleLabel from '../RoleLabel'
-import { fragments } from './gql'
+// import { fragments } from './gql'
 import styles from './styles.module.css'
 
-export type CommentControls = {
+export type JournalCommentControls = {
   avatarSize?: AvatarSize
   hasUserName?: boolean
 } & FooterActionsControls &
   DropdownActionsControls
 
-export type CommentProps = {
-  comment: FeedCommentBetaPublicFragment &
-    Partial<FeedCommentBetaPrivateFragment>
-  pinnedComment?: ThreadCommentType
+export type JournalCommentProps = {
+  comment: JournalCommentFeedCommentPublicFragment &
+    Partial<JournalCommentFeedCommentPrivateFragment>
   type: CommentFormType
-} & CommentControls
+} & JournalCommentControls
 
-export const BaseJournalCommentFeed = ({
+const BaseJournalCommentFeed = ({
   comment,
-  pinnedComment,
   type,
   avatarSize = 32,
   hasUserName,
   replySubmitCallback,
   ...actionControls
-}: CommentProps) => {
+}: JournalCommentProps) => {
   const { id, author, parentComment } = comment
   const article =
     comment.node.__typename === 'Article' ? comment.node : undefined
   const nodeId = parentComment ? `${parentComment.id}-${id}` : id
 
-  const submitCallback = () => {
-    if (replySubmitCallback) {
-      replySubmitCallback()
-    }
-  }
+  // const submitCallback = () => {
+  //   if (replySubmitCallback) {
+  //     replySubmitCallback()
+  //   }
+  // }
 
   const contentClasses = classNames({
     [styles.contentContainer]: true,
-    [styles.descendant]: !!parentComment,
+    // [styles.descendant]: !!parentComment,
   })
 
   const userProfilePath = toPath({
@@ -101,10 +97,8 @@ export const BaseJournalCommentFeed = ({
           </section>
         </section>
         <section className={styles.right}>
-          <PinnedLabel comment={comment} />
           <DropdownActions
             comment={comment}
-            pinnedComment={pinnedComment}
             type={type}
             hasPin={false}
             inCard={actionControls.inCard}
@@ -115,12 +109,12 @@ export const BaseJournalCommentFeed = ({
       <section className={contentClasses}>
         <Content comment={comment} type={type} size={15} limit={5} />
 
-        <FooterActions
+        {/* <FooterActions
           comment={comment}
           type={type}
           replySubmitCallback={submitCallback}
           {...actionControls}
-        />
+        /> */}
       </section>
     </article>
   )
@@ -129,33 +123,27 @@ export const BaseJournalCommentFeed = ({
 /**
  * Memoizing
  */
-type MemoizedCommentFeed = React.MemoExoticComponent<React.FC<CommentProps>> & {
-  fragments: typeof fragments
-}
+type MemoizedJournalCommentFeed = React.MemoExoticComponent<
+  React.FC<JournalCommentProps>
+>
+// & {
+//   fragments: typeof fragments
+// }
 
 const JournalCommentFeed = React.memo(
   BaseJournalCommentFeed,
-  (
-    {
-      comment: prevComment,
-      pinnedComment: prevPinnedComment,
-      disabled: prevDisabled,
-    },
-    { comment, pinnedComment, disabled }
-  ) => {
+  ({ comment: prevComment, disabled: prevDisabled }, { comment, disabled }) => {
     return (
       prevComment.content === comment.content &&
       prevComment.upvotes === comment.upvotes &&
       prevComment.myVote === comment.myVote &&
       prevComment.state === comment.state &&
-      prevComment.pinned === comment.pinned &&
       prevComment.author.isBlocked === comment.author.isBlocked &&
-      prevDisabled === disabled &&
-      prevPinnedComment?.id === pinnedComment?.id
+      prevDisabled === disabled
     )
   }
-) as MemoizedCommentFeed
+) as MemoizedJournalCommentFeed
 
-JournalCommentFeed.fragments = fragments
+// JournalCommentFeed.fragments = fragments
 
 export default JournalCommentFeed
