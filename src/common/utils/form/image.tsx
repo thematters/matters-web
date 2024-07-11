277563d8a2ef5fab0025777afcb718e147d12a0f
bluecloud <96812901+pitb2022@users.noreply.github.com>
2024-07-11 14:55:48 +0800

    feat(MomentDigest): add image lightbox

--- src/common/utils/form/image.tsx
+++ src/common/utils/form/image.tsx
@@ -86,3 +86,19 @@ export const validateImage = (image: File, isAvatar: boolean = false) =>
       }
     })
   })
+
+interface Dimensions {
+  width: number
+  height: number
+}
+export const getImageDimensions = (src: string): Promise<Dimensions> => {
+  return new Promise((resolve, reject) => {
+    const img = new Image()
+    img.onload = () => {
+      resolve({ width: img.width, height: img.height })
+    }
+    img.onerror = reject
+
+    img.src = src
+  })
+}
--- src/components/MomentDigest/Assets/index.tsx
+++ src/components/MomentDigest/Assets/index.tsx
@@ -1,6 +1,12 @@
+import 'photoswipe/dist/photoswipe.css'
+
 import gql from 'graphql-tag'
+import { useEffect, useState } from 'react'
+import { Gallery, Item } from 'react-photoswipe-gallery'
 
 import { TEST_ID } from '~/common/enums'
+import { toSizedImageURL } from '~/common/utils'
+import { getImageDimensions } from '~/common/utils/form'
 import { ResponsiveImage } from '~/components'
 import { MomentDigestAssetsMomentFragment } from '~/gql/graphql'
 
@@ -17,19 +23,77 @@ const fragments = {
   `,
 }
 
+type LoadedAsset = {
+  __typename?: 'Asset' | undefined
+  id: string
+  path: string
+  width: number
+  height: number
+}
+
+const ImagePlaceholder = () => <div className={styles.imagePlaceholder} />
+
 const Assets = ({ moment }: { moment: MomentDigestAssetsMomentFragment }) => {
   const { assets } = moment
+  const [loadedAssets, setLoadedAssets] = useState<LoadedAsset[] | null>(null)
+  useEffect(() => {
+    const loadAssets = async () => {
+      const newAssets = await Promise.all(
+        assets.map(async (asset) => {
+          const { width, height } = await getImageDimensions(asset.path)
+          return {
+            ...asset,
+            width,
+            height,
+          }
+        })
+      )
+      setLoadedAssets(newAssets)
+    }
+    loadAssets()
+  }, [assets])
+  if (!loadedAssets) {
+    return (
+      <section className={styles.assets}>
+        {assets.map((asset) => (
+          <ImagePlaceholder key={asset.id} />
+        ))}
+      </section>
+    )
+  }
+
   return (
     <section
       className={styles.assets}
       data-test-id={TEST_ID.MOMENT_DIGEST_ASSETS}
     >
-      {assets.map((asset) => (
-        // TODO: add lightbox
-        <div className={styles.item} key={asset.id} role="button">
-          <ResponsiveImage url={asset.path} width={106} height={106} />
-        </div>
-      ))}
+      <Gallery options={{}}>
+        {loadedAssets.map((asset) => (
+          <Item
+            key={asset.id}
+            original={asset.path}
+            thumbnail={toSizedImageURL({
+              url: asset.path,
+              width: 106,
+              disableAnimation: true,
+            })}
+            width={asset.width}
+            height={asset.height}
+          >
+            {({ ref, open }) => (
+              <div
+                ref={ref}
+                onClick={open}
+                className={styles.item}
+                key={asset.id}
+                role="button"
+              >
+                <ResponsiveImage url={asset.path} width={106} height={106} />
+              </div>
+            )}
+          </Item>
+        ))}
+      </Gallery>
     </section>
   )
 }
