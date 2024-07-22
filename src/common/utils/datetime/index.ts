6dbcadda4d5337377c4e4f36a053edb29d719766
robertu <4065233+robertu7@users.noreply.github.com>
2024-07-22 08:32:52 +0700

    fix(campaign): show timezone if current is not UTC+8; revise info area line breaks;

--- src/common/utils/datetime/index.ts
+++ src/common/utils/datetime/index.ts
@@ -5,3 +5,9 @@ export const datetimeFormat = {
   absolute,
   relative,
 }
+
+export const isUTC8 = () => {
+  const timezoneOffset = new Date().getTimezoneOffset()
+  const utc8Offset = -480
+  return timezoneOffset === utc8Offset
+}
