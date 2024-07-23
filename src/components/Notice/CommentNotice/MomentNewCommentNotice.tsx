c6ebe81cc9164cd83eceb8ea94cb940b32e5b443
byhow <by.how@hotmail.com>
2024-07-22 22:25:10 -0700

    refactor(moment): add title components for truncation

--- src/components/Notice/CommentNotice/MomentNewCommentNotice.tsx
+++ src/components/Notice/CommentNotice/MomentNewCommentNotice.tsx
@@ -1,7 +1,11 @@
 import gql from 'graphql-tag'
-import { FormattedMessage } from 'react-intl'
+import { useContext } from 'react'
+import { FormattedMessage, useIntl } from 'react-intl'
 
 import { TEST_ID } from '~/common/enums'
+import { stripHtml } from '~/common/utils'
+import { truncateTitle } from '~/common/utils/text/moment'
+import { LanguageContext } from '~/components/Context'
 import { MomentNewCommentNoticeFragment } from '~/gql/graphql'
 
 import NoticeActorAvatar from '../NoticeActorAvatar'
@@ -9,15 +13,22 @@ import NoticeComment from '../NoticeComment'
 import NoticeDate from '../NoticeDate'
 import NoticeDigest from '../NoticeDigest'
 import NoticeHeadActors from '../NoticeHeadActors'
+import NoticeMomentTitle from '../NoticeMomentTitle'
 
 const MomentNewCommentNotice = ({
   notice,
 }: {
   notice: MomentNewCommentNoticeFragment
 }) => {
+  const { lang } = useContext(LanguageContext)
+  const intl = useIntl()
+
   if (!notice.actors) {
     return null
   }
+
+  const commentMoment =
+    notice.comment?.node.__typename === 'Moment' ? notice.comment.node : null
 
   return (
     <NoticeDigest
@@ -27,8 +38,30 @@ const MomentNewCommentNotice = ({
           defaultMessage="commented in your moment"
           id="qCotH5"
           description="src/components/Notice/CommentNotice/MomentNewCommentNotice.tsx"
+          values={{
+            commentMoment: (
+              <NoticeMomentTitle
+                moment={commentMoment}
+                title={`${truncateTitle(
+                  stripHtml(commentMoment?.content || ''),
+                  10,
+                  lang
+                )} ${
+                  commentMoment?.assets?.length
+                    ? intl
+                        .formatMessage({
+                          defaultMessage: `[image]`,
+                          id: 'W3tqQO',
+                        })
+                        .repeat(Math.min(3, commentMoment.assets.length))
+                    : ''
+                }`}
+              />
+            ),
+          }}
         />
       }
+      content={<NoticeComment comment={notice.comment} />}
       title={notice.comment?.content || 'FIXME title'}
       testId={TEST_ID.NOTICE_ARTICLE_NEW_COMMENT}
     />
@@ -46,12 +79,18 @@ MomentNewCommentNotice.fragments = {
       }
       comment: target {
         ...NoticeComment
+        node {
+          ... on Moment {
+            ...NoticeMomentTitle
+          }
+        }
       }
     }
     ${NoticeActorAvatar.fragments.user}
     ${NoticeHeadActors.fragments.user}
     ${NoticeComment.fragments.comment}
     ${NoticeDate.fragments.notice}
+    ${NoticeMomentTitle.fragments.moment}
   `,
 }
 
