6484e6e9e1c71001a5bec703eba1ea00e4bae869
bluecloud <96812901+pitb2022@users.noreply.github.com>
2024-07-29 10:58:32 +0800

    refactor(NavBar): optimize NavCreate

--- src/components/Layout/NavBar/index.tsx
+++ src/components/Layout/NavBar/index.tsx
@@ -1,35 +1,22 @@
 import { useContext } from 'react'
 import { FormattedMessage, useIntl } from 'react-intl'
 
-import { ReactComponent as IconCircleTimes } from '@/public/static/icons/24px/circle-times.svg'
-import { ReactComponent as IconNavCreate } from '@/public/static/icons/24px/nav-create.svg'
 import { ReactComponent as IconNavHome } from '@/public/static/icons/24px/nav-home.svg'
 import { ReactComponent as IconNavHomeActive } from '@/public/static/icons/24px/nav-home-active.svg'
 import { ReactComponent as IconNavSearch } from '@/public/static/icons/24px/nav-search.svg'
 import { ReactComponent as IconNavSearchActive } from '@/public/static/icons/24px/nav-search-active.svg'
+import { PATHS } from '~/common/enums'
+import { toPath } from '~/common/utils'
 import {
-  ERROR_CODES,
-  ERROR_MESSAGES,
-  HAS_SHOW_MOMENT_BANNER,
-  PATHS,
-} from '~/common/enums'
-import { storage, toPath } from '~/common/utils'
-import {
-  Dropdown,
-  hidePopperOnClick,
   Icon,
-  toast,
   UniversalAuthButton,
-  useDialogSwitch,
   useRoute,
   ViewerContext,
 } from '~/components'
 
-import SideNavNavListItem from '../SideNav/NavListItem'
 import UnreadIcon from '../UnreadIcon'
-import NavBanner from './NavBanner'
+import { NavCreate } from './NavCreate'
 import NavListItem from './NavListItem'
-import NavPopover from './NavPopover'
 import styles from './styles.module.css'
 
 const NavBar = () => {
@@ -40,23 +27,6 @@ const NavBar = () => {
   const isInFollow = isInPath('FOLLOW')
   const isInNotification = isInPath('ME_NOTIFICATIONS')
   const isInSearch = isInPath('SEARCH')
-
-  const { show: showMomentBanner, closeDialog: _closeMomentBanner } =
-    useDialogSwitch(!storage.get(HAS_SHOW_MOMENT_BANNER))
-
-  const closeMomentBanner = () => {
-    _closeMomentBanner()
-    storage.set(HAS_SHOW_MOMENT_BANNER, true)
-  }
-
-  const {
-    show: showWriteDropdown,
-    openDialog: openWriteDropdown,
-    closeDialog: closeWriteDropdown,
-  } = useDialogSwitch(false)
-
-  const toggleWriteDropdown = () =>
-    showWriteDropdown ? closeWriteDropdown() : openWriteDropdown()
 
   if (!viewer.isAuthed) {
     return (
@@ -114,68 +84,7 @@ const NavBar = () => {
           active={isInFollow}
           href={PATHS.FOLLOW}
         />
-        <Dropdown
-          arrow={true}
-          onHidden={closeMomentBanner}
-          onClickOutside={closeMomentBanner}
-          visible={showMomentBanner}
-          content={<NavBanner />}
-          placement="top"
-          onShown={hidePopperOnClick}
-          offset={[0, 12]} // 16px - 4px (default tippy padding)
-          theme="banner"
-        >
-          {({ ref: bannerRef }) => (
-            <Dropdown
-              arrow={true}
-              onHidden={closeWriteDropdown}
-              onClickOutside={closeWriteDropdown}
-              visible={showWriteDropdown}
-              content={<NavPopover />}
-              placement="top"
-              onShown={hidePopperOnClick}
-              offset={[0, 12]} // 16px - 4px (default tippy padding)
-              theme="mobile"
-            >
-              {({ ref: navRef }) => (
-                <span ref={bannerRef}>
-                  <SideNavNavListItem
-                    onClick={() => {
-                      if (viewer.isInactive) {
-                        toast.error({
-                          message: (
-                            <FormattedMessage
-                              {...ERROR_MESSAGES[
-                                ERROR_CODES.FORBIDDEN_BY_STATE
-                              ]}
-                            />
-                          ),
-                        })
-
-                        return
-                      }
-
-                      if (showMomentBanner) {
-                        closeMomentBanner()
-                      }
-
-                      toggleWriteDropdown()
-                    }}
-                    name={
-                      <FormattedMessage defaultMessage="Create" id="VzzYJk" />
-                    }
-                    icon={<Icon icon={IconNavCreate} size={32} />}
-                    activeIcon={<Icon icon={IconCircleTimes} size={32} />}
-                    active={showWriteDropdown}
-                    aria-haspopup="menu"
-                    canScrollTop={false}
-                    ref={navRef}
-                  />
-                </span>
-              )}
-            </Dropdown>
-          )}
-        </Dropdown>
+        <NavCreate />
 
         <NavListItem
           name={<FormattedMessage defaultMessage="Search" id="xmcVZ0" />}
