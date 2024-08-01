e7428e708399d63f29efb981bb017d229381ceda
robertu <4065233+robertu7@users.noreply.github.com>
2024-08-01 15:55:10 +0700

    feat(editor): use figureImageUploader in UploadImageButton

--- src/components/Editor/Article/FloatingMenu/UploadImageButton.tsx
+++ src/components/Editor/Article/FloatingMenu/UploadImageButton.tsx
@@ -1,13 +1,10 @@
 import { Editor } from '@matters/matters-editor'
 import { VisuallyHidden } from '@reach/visually-hidden'
-import classNames from 'classnames'
-import { useState } from 'react'
-import { FormattedMessage, useIntl } from 'react-intl'
+import { useIntl } from 'react-intl'
 
 import { ReactComponent as IconEditorImage } from '@/public/static/icons/editor-image.svg'
 import { ACCEPTED_UPLOAD_IMAGE_TYPES, ASSET_TYPE } from '~/common/enums'
-import { getFileType, validateImage } from '~/common/utils'
-import { Icon, toast } from '~/components'
+import { Icon } from '~/components'
 
 import styles from './styles.module.css'
 
@@ -29,7 +26,6 @@ const UploadImageButton: React.FC<UploadImageButtonProps> = ({
   upload,
 }) => {
   const intl = useIntl()
-  const [uploading, setUploading] = useState(false)
 
   const acceptTypes = ACCEPTED_UPLOAD_IMAGE_TYPES.join(',')
   const fieldId = 'editor-image-upload-form'
@@ -37,59 +33,26 @@ const UploadImageButton: React.FC<UploadImageButtonProps> = ({
   const handleChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
     event.stopPropagation()
 
-    if (!upload || !event.target || !event.target.files) {
+    const files = event.target?.files
+
+    if (!upload || !files) {
       return
     }
 
-    const files = event.target.files
-
-    const hasInvalidImage = await Promise.all(
-      Array.from(files).map((file) => validateImage(file))
-    ).then((results) => results.some((result) => !result))
-
-    if (hasInvalidImage) {
-      event.target.value = ''
-      return
-    }
-
-    try {
-      setUploading(true)
-
-      for (const file of files) {
-        const mime = (await getFileType(file))!.mime
-        const { path } = await upload({ file, type: ASSET_TYPE.embed, mime })
-
-        editor.chain().focus().setFigureImage({ src: path }).run()
-
-        toast.success({
-          message: (
-            <FormattedMessage defaultMessage="Image uploaded" id="TcTp+J" />
-          ),
-        })
-      }
-    } catch (e) {
-      toast.error({
-        message: (
-          <FormattedMessage
-            defaultMessage="Failed to upload, please try again."
-            id="qfi4cg"
-          />
-        ),
+    Array.from(files).forEach((file) => {
+      editor.commands.addFigureImageUploader({
+        previewSrc: URL.createObjectURL(file),
+        file,
+        upload,
       })
-    }
+    })
 
     event.target.value = ''
-    setUploading(false)
   }
-
-  const labelClasses = classNames({
-    [styles.uploadButton]: true,
-    'u-area-disable': uploading,
-  })
 
   return (
     <label
-      className={labelClasses}
+      className={styles.uploadButton}
       htmlFor={fieldId}
       title={intl.formatMessage({
         defaultMessage: 'Insert image',
@@ -107,7 +70,6 @@ const UploadImageButton: React.FC<UploadImageButtonProps> = ({
             defaultMessage: 'Insert image',
             id: 'Pv2PlK',
           })}
-          disabled={uploading}
           accept={acceptTypes}
           multiple={true}
           onChange={handleChange}
