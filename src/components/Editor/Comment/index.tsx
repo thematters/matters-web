acdf940cc5f1436b41ded98d63301608036605a7
robertu <4065233+robertu7@users.noreply.github.com>
2024-08-20 14:39:33 +0800

    feat(editor): support mod+enter to submit comment/moment

--- src/components/Editor/Comment/index.tsx
+++ src/components/Editor/Comment/index.tsx
@@ -13,13 +13,18 @@ import { useIntl } from 'react-intl'
 import { BYPASS_SCROLL_LOCK, ENBABLE_SCROLL_LOCK } from '~/common/enums'
 import { useCommentEditorContext } from '~/components/Context'
 
-import { makeMentionSuggestion, SmartLink } from '../Article/extensions'
+import {
+  CustomShortcuts,
+  makeMentionSuggestion,
+  SmartLink,
+} from '../Article/extensions'
 import { makeSmartLinkOptions } from '../Article/extensions/smartLink/utils'
 import styles from './styles.module.css'
 
 interface Props {
   content: string
   update: (params: { content: string }) => void
+  onSubmit: () => any
   placeholder?: string
   setEditor?: (editor: Editor | null) => void
   onFocused?: () => void
@@ -30,6 +35,7 @@ interface Props {
 const CommentEditor: React.FC<Props> = ({
   content,
   update,
+  onSubmit,
   placeholder,
   setEditor,
   onFocused,
@@ -63,6 +69,9 @@ const CommentEditor: React.FC<Props> = ({
       lockScroll && window.dispatchEvent(new CustomEvent(ENBABLE_SCROLL_LOCK))
     },
     extensions: [
+      CustomShortcuts.configure({
+        onModEnter: () => onSubmit(),
+      }),
       Placeholder.configure({
         placeholder,
       }),
