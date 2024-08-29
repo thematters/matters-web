a29ef99ad68d6a2d29bb4f27f60c99211fe78246
bluecloud <96812901+pitb2022@users.noreply.github.com>
2024-08-29 14:34:12 +0800

    fix(Announcements): remove unused field

--- src/views/Home/Announcements/gql.ts
+++ src/views/Home/Announcements/gql.ts
@@ -2,9 +2,6 @@ import gql from 'graphql-tag'
 
 export const VISIBLE_ANNOUNCEMENTS = gql`
   query VisibleAnnouncements($input: AnnouncementsInput!) {
-    viewer {
-      id
-    }
     official {
       announcements(input: $input) {
         id
