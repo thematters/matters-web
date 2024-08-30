5811d2c1c163a23f0514383c53db174968ee3e4e
devformatters2 <177856586+devformatters2@users.noreply.github.com>
2024-08-30 13:28:03 +0800

    fix(notice): fix incorrect word counting of `truncateNoticeTitle`

--- src/common/utils/text/notice.ts
+++ src/common/utils/text/notice.ts
@@ -1,182 +1,62 @@
-import { UserLanguage } from '~/gql/graphql'
-
-type TruncateNoticeTitleOptions = {
-  locale?: UserLanguage
-  maxLength?: number
-  includeAtSign?: boolean
-}
-
 /**
  * Truncates a title to a specified maximum length, while preserving tagged users.
  *
  * @param title - The title to truncate.
  * @param maxLength - The maximum length of the truncated title.
- * @param locale - The locale to determine the truncation rules. Defaults to 'en'.
+ *   - Each CJK character is counted as 1 unit.
+ *   - Each latin word is counted as 1 unit.
+ *   - Each tagged user is counted as 1 unit.
+ *   - Ignoer spaces and punctuations.
+ *
  * @returns The truncated title with preserved tagged users.
  */
-export const truncateNoticeTitle = (
-  title: string,
-  options: TruncateNoticeTitleOptions = {}
-) => {
-  const DEFAULTS = {
-    locale: UserLanguage.En,
-    includeAtSign: false,
-    maxLength: 10,
-  }
-  let localOptions = { ...DEFAULTS, ...options }
+const REGEXP_CJK =
+  '[\u3040-\u30ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff\uff66-\uff9f]'
 
-  if (/^zh/.test(localOptions.locale)) {
-    return localOptions.includeAtSign
-      ? truncateTitleForChineseWithAtSign(title, localOptions)
-      : truncateTitleForChinese(title, localOptions)
-  } else {
-    return localOptions.includeAtSign
-      ? truncateTitleForEnglishWithAtSign(title, localOptions)
-      : truncateTitleForEnglish(title, localOptions)
+function countUnits(word: string) {
+  // Latin word
+  if (/^@\w+/.test(word) || new RegExp(`^@${REGEXP_CJK}+`).test(word)) {
+    return 1
   }
+  // CJK
+  else if (new RegExp(REGEXP_CJK, 'g').test(word)) {
+    return 1
+  }
+  // Latin
+  else if (/^\w+/.test(word)) {
+    return 1
+  }
+
+  // Ignore spaces and punctuations
+  return 0
 }
 
-/**
- * Truncates a title to a specified maximum length for Chinese (Simplified or traditional) text.
- *
- * @param text - The title to truncate.
- * @param maxWords - The maximum number of words in the truncated title. Defaults to 10.
- * @returns The truncated title.
- */
-export function truncateTitleForChinese(
-  text: string,
-  {
-    maxLength,
-  }: { maxLength: NonNullable<TruncateNoticeTitleOptions['maxLength']> }
-): string {
-  const chineseRegex = /[\u4e00-\u9fa5]/g
-  const chineseWords = text.match(chineseRegex)
-  if (chineseWords && chineseWords.length > maxLength) {
-    return chineseWords.slice(0, maxLength).join('') + '...'
-  }
-  return text
+function trimSpacesAndPunctuations(str: string) {
+  return str.replace(/^[\s\p{P}]+|[\s\p{P}]+$/gu, '')
 }
 
-/**
- * Truncates a title to a specified maximum length for English text.
- *
- * @param text - The title to truncate.
- * @param maxLength - The maximum length of the truncated title. Defaults to 50.
- * @returns The truncated title.
- */
-export function truncateTitleForEnglish(
-  text: string,
-  {
-    maxLength,
-  }: { maxLength: NonNullable<TruncateNoticeTitleOptions['maxLength']> }
-): string {
-  if (text.length > maxLength) {
-    const words = text.split(' ')
-    let truncatedText = ''
-    let count = 0
-    for (const word of words) {
-      if (count + word.length <= maxLength) {
-        truncatedText += word + ' '
-        count += word.length + 1
-      } else {
-        break
+export const truncateNoticeTitle = (title: string, maxLength: number = 10) => {
+  const components =
+    title.match(
+      new RegExp(`(@\\w+|@${REGEXP_CJK}+|\\w+|${REGEXP_CJK}|[^\w\s])`, 'g')
+    ) || []
+
+  let truncatedTitle = ''
+  let currentLength = 0
+
+  for (const [index, component] of components.entries()) {
+    const componentUnits = countUnits(component)
+
+    if (currentLength + componentUnits > maxLength) {
+      if (index < components.length - 1) {
+        truncatedTitle = trimSpacesAndPunctuations(truncatedTitle) + '...'
       }
-    }
-    return truncatedText.trim() + '...'
-  }
-  return text
-}
-
-/**
- * Truncates a title in English to a specified maximum length, while preserving tagged users.
- *
- * @param title - The title to truncate.
- * @param maxLength - The maximum length of the truncated title.
- * @returns The truncated title with preserved tagged users.
- */
-const truncateTitleForEnglishWithAtSign = (
-  title: string,
-  {
-    maxLength,
-  }: { maxLength: NonNullable<TruncateNoticeTitleOptions['maxLength']> }
-) => {
-  const words = title.split(/\s+/)
-  let hasTag = words.some((word) => word.startsWith('@'))
-  let truncated = ''
-  let count = 0
-
-  for (const word of words) {
-    if (word.startsWith('@')) {
-      truncated += `${word} `
-      continue
-    }
-    if (count + word.length + 1 > maxLength) {
       break
     }
-    truncated += `${word} `
-    count += word.length + 1
+
+    truncatedTitle += component
+    currentLength += componentUnits
   }
 
-  let base = truncated.trim() + (title.length > count ? '...' : '')
-  if (hasTag && !base.includes('@')) {
-    for (const word of words) {
-      if (word.startsWith('@')) {
-        base += `${word} `
-      }
-    }
-  }
-
-  return base.trim()
-}
-
-/**
- * Truncates a title in CJK (Chinese, Japanese, Korean) to a specified maximum length, while preserving tagged users.
- *
- * @param title - The title to truncate.
- * @param maxLength - The maximum length of the truncated title.
- * @returns The truncated title with preserved tagged users.
- */
-const truncateTitleForChineseWithAtSign = (
-  title: string,
-  {
-    maxLength,
-  }: { maxLength: NonNullable<TruncateNoticeTitleOptions['maxLength']> }
-) => {
-  const pattern = /(@\w+|[^\x00-\x7F]|\s)/gu
-  const phrases = title.match(pattern)?.filter((s) => s !== ' ') || []
-  let hasTag = phrases.some((p) => p.startsWith('@'))
-  let count = 0
-  let truncated = ''
-
-  for (const [idx, p] of phrases.entries()) {
-    if (p.startsWith('@')) {
-      if (idx + 1 == phrases.length) {
-        truncated += ` ${p}`
-        count += 1
-      } else if (idx === 0) {
-        truncated += `${p} `
-        count += 1
-      } else {
-        truncated += ` ${p} `
-        count += 2
-      }
-      continue
-    }
-    if (count + 1 > maxLength) {
-      break
-    }
-    truncated += p
-    count++
-  }
-
-  let base = truncated.trim() + (title.length > count ? '...' : '')
-  if (hasTag && !base.includes('@')) {
-    for (const p of phrases) {
-      if (p.startsWith('@')) {
-        base += `${p} `
-      }
-    }
-  }
-
-  return base.trim()
+  return truncatedTitle
 }
