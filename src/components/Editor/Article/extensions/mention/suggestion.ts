aa783b4e76e21c52bc8ce58828113a93dcf6cf14
j <13580441+gary02@users.noreply.github.com>
2024-08-31 16:33:16 +0800

    feat(search-user): allow blockees to search and mention blockers

--- src/components/Editor/Article/extensions/mention/suggestion.ts
+++ src/components/Editor/Article/extensions/mention/suggestion.ts
@@ -20,7 +20,7 @@ export const makeMentionSuggestion = ({
   items: async ({ query }: { query: string }) => {
     const { data } = await client.query<SearchUsersQuery>({
       query: SEARCH_USERS,
-      variables: { search: `@${query}`, exclude: 'blocked' },
+      variables: { search: `@${query}`},
     })
     const mentionUsers = (data?.search.edges || []).map(({ node }: any) => node)
 
