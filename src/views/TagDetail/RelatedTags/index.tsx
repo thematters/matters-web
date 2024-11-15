6db96f9f2bf035efb36416cb881a06b725568096
Kechicode <186776112+Kechicode@users.noreply.github.com>
2024-11-15 19:04:10 +0800

    feat(TagDetail): revise RelatedTags

--- src/views/TagDetail/RelatedTags/index.tsx
+++ src/views/TagDetail/RelatedTags/index.tsx
@@ -5,18 +5,8 @@ import _get from 'lodash/get'
 import _random from 'lodash/random'
 import { FormattedMessage } from 'react-intl'
 
-import { PATHS } from '~/common/enums'
 import { analytics } from '~/common/utils'
-import {
-  List,
-  PageHeader,
-  ShuffleButton,
-  Slides,
-  TagDigest,
-  usePublicQuery,
-  ViewAllButton,
-  ViewMoreCard,
-} from '~/components'
+import { ArticleTag, usePublicQuery } from '~/components'
 import FETCH_RECORD from '~/components/GQL/queries/lastFetchRandom'
 import { LastFetchRandomQuery, TagDetailRecommendedQuery } from '~/gql/graphql'
 
@@ -28,49 +18,29 @@ interface RelatedTagsProps {
   inSidebar?: boolean
 }
 
-const RelatedTagsHeader = ({
-  hasViewAll,
-  hasShuffle,
-  onShuffle,
-}: {
-  hasViewAll?: boolean
-  hasShuffle?: boolean
-  onShuffle?: () => void
-}) => {
+const RelatedTagsHeader = () => {
   return (
-    <PageHeader
-      title={
-        <FormattedMessage
-          defaultMessage="Related Tags"
-          id="HFVDeB"
-          description="src/views/TagDetail/RelatedTags/index.tsx"
-        />
-      }
-      is="h2"
-      hasBorder={false}
-    >
-      <section className={styles.right}>
-        {hasShuffle && <ShuffleButton onClick={onShuffle} />}
-        {hasViewAll && <ViewAllButton href={PATHS.TAGS} />}
-      </section>
-    </PageHeader>
+    <section className={styles.header}>
+      <FormattedMessage
+        defaultMessage="Related Tags"
+        id="HFVDeB"
+        description="src/views/TagDetail/RelatedTags/index.tsx"
+      />
+    </section>
   )
 }
 
 const RelatedTags: React.FC<RelatedTagsProps> = ({ tagId, inSidebar }) => {
-  const { data: lastFetchRandom, client } = useQuery<LastFetchRandomQuery>(
+  const { data: lastFetchRandom } = useQuery<LastFetchRandomQuery>(
     FETCH_RECORD,
     { variables: { id: 'local' } }
   )
 
   const lastRandom = lastFetchRandom?.lastFetchRandom.feedTags
 
-  const { data, refetch } = usePublicQuery<TagDetailRecommendedQuery>(
-    RELATED_TAGS,
-    {
-      variables: { id: tagId, random: lastRandom || 0 },
-    }
-  )
+  const { data } = usePublicQuery<TagDetailRecommendedQuery>(RELATED_TAGS, {
+    variables: { id: tagId, random: lastRandom || 0 },
+  })
 
   const { edges } =
     (data?.node?.__typename === 'Tag' && data.node.recommended) || {}
@@ -87,73 +57,23 @@ const RelatedTags: React.FC<RelatedTagsProps> = ({ tagId, inSidebar }) => {
     return null
   }
 
-  const shuffle = () => {
-    const random = _random(0, 49)
-    refetch({ random })
-
-    client.writeData({
-      id: 'LastFetchRandom:local',
-      data: { feedAuthors: random },
-    })
-  }
-
   const relatedTagsClasses = classNames({
     [styles.relatedTags]: true,
     [styles.inSidebar]: inSidebar,
   })
 
-  if (!inSidebar) {
-    return (
-      <section className={relatedTagsClasses}>
-        <Slides header={<RelatedTagsHeader hasShuffle onShuffle={shuffle} />}>
-          {_chunk(edges, 5).map((chunks, edgeIndex) => (
-            <Slides.Item size="md" key={edgeIndex}>
-              <section>
-                {chunks.map(({ node, cursor }, nodeIndex) => (
-                  <TagDigest.Sidebar
-                    key={node.id}
-                    tag={node}
-                    onClick={() =>
-                      trackRelatedTags(
-                        (edgeIndex + 1) * (nodeIndex + 1) - 1,
-                        node.id
-                      )
-                    }
-                  />
-                ))}
-              </section>
-            </Slides.Item>
-          ))}
-        </Slides>
-
-        <section className={styles.backToAll}>
-          <ViewMoreCard
-            spacing={[12, 12]}
-            href={PATHS.TAGS}
-            iconProps={{ size: 14 }}
-            textIconProps={{ size: 14, weight: 'medium', spacing: 4 }}
-            textAlign="center"
-          >
-            <FormattedMessage defaultMessage="Back to All" id="o2Na0B" />
-          </ViewMoreCard>
-        </section>
-      </section>
-    )
-  }
-
   return (
     <section className={relatedTagsClasses}>
-      <RelatedTagsHeader hasViewAll hasShuffle onShuffle={shuffle} />
-      <List hasBorder={false}>
+      <RelatedTagsHeader />
+      <section className={styles.tags}>
         {edges?.map(({ node, cursor }, i) => (
-          <List.Item key={node.id}>
-            <TagDigest.Sidebar
-              tag={node}
-              onClick={() => trackRelatedTags(i, node.id)}
-            />
-          </List.Item>
+          <ArticleTag
+            key={node.id}
+            tag={node}
+            onClick={() => trackRelatedTags(i, node.id)}
+          />
         ))}
-      </List>
+      </section>
     </section>
   )
 }
