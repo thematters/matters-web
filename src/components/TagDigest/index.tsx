3567a9bfa699db0f752ee9e5eb183fe81e962fab
Kechicode <186776112+Kechicode@users.noreply.github.com>
2024-11-22 11:42:38 +0800

    refactor(TagDigest): remove TagDigest.Rich component

--- src/components/TagDigest/index.tsx
+++ src/components/TagDigest/index.tsx
@@ -1,11 +1,9 @@
 import Concise from './Concise'
 import Feed from './Feed'
-import Rich from './Rich'
 import Sidebar from './Sidebar'
 
 export const TagDigest = {
   Concise,
-  Rich,
   Sidebar,
   Feed,
 }
