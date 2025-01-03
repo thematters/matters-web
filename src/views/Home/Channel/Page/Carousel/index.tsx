015c0cd3ad79acfe5f47c8d88652fb1e69719335
Kechicode <186776112+Kechicode@users.noreply.github.com>
2025-01-03 21:44:19 +0800

    feat(Channel): Enhance Channel Carousel with Dynamic Column Count and Responsive Design

--- src/views/Home/Channel/Page/Carousel/index.tsx
+++ src/views/Home/Channel/Page/Carousel/index.tsx
@@ -12,6 +12,8 @@ import {
 import Dot from './Dot'
 import styles from './styles.module.css'
 
+type ColumnCount = '4' | '5' | '6' | '7'
+
 const ChannelCarousel = () => {
   const host = typeof window !== 'undefined' ? window.location.origin : ''
   const items = [
@@ -50,6 +52,111 @@ const ChannelCarousel = () => {
       title: 'Item 7',
       link: `${host}/#channel=7`,
     },
+    {
+      id: '8',
+      title: 'Item 8',
+      link: `${host}/#channel=8`,
+    },
+    {
+      id: '9',
+      title: 'Item 9',
+      link: `${host}/#channel=9`,
+    },
+    {
+      id: '10',
+      title: 'Item 10',
+      link: `${host}/#channel=10`,
+    },
+    {
+      id: '11',
+      title: 'Item 11',
+      link: `${host}/#channel=11`,
+    },
+    {
+      id: '12',
+      title: 'Item 12',
+      link: `${host}/#channel=12`,
+    },
+    {
+      id: '13',
+      title: 'Item 13',
+      link: `${host}/#channel=13`,
+    },
+    {
+      id: '14',
+      title: 'Item 14',
+      link: `${host}/#channel=14`,
+    },
+    {
+      id: '15',
+      title: 'Item 15',
+      link: `${host}/#channel=15`,
+    },
+    {
+      id: '16',
+      title: 'Item 16',
+      link: `${host}/#channel=16`,
+    },
+    {
+      id: '17',
+      title: 'Item 17',
+      link: `${host}/#channel=17`,
+    },
+    {
+      id: '18',
+      title: 'Item 18',
+      link: `${host}/#channel=18`,
+    },
+    {
+      id: '19',
+      title: 'Item 19',
+      link: `${host}/#channel=19`,
+    },
+    {
+      id: '20',
+      title: 'Item 20',
+      link: `${host}/#channel=20`,
+    },
+    {
+      id: '21',
+      title: 'Item 21',
+      link: `${host}/#channel=21`,
+    },
+    {
+      id: '22',
+      title: 'Item 22',
+      link: `${host}/#channel=22`,
+    },
+    {
+      id: '23',
+      title: 'Item 23',
+      link: `${host}/#channel=23`,
+    },
+    {
+      id: '24',
+      title: 'Item 24',
+      link: `${host}/#channel=24`,
+    },
+    {
+      id: '25',
+      title: 'Item 25',
+      link: `${host}/#channel=25`,
+    },
+    {
+      id: '26',
+      title: 'Item 26',
+      link: `${host}/#channel=26`,
+    },
+    {
+      id: '27',
+      title: 'Item 27',
+      link: `${host}/#channel=27`,
+    },
+    {
+      id: '28',
+      title: 'Item 28',
+      link: `${host}/#channel=28`,
+    },
   ]
   const [dot, setDot] = useState(0)
   const [, setSnaps] = useState<any[]>([])
@@ -58,24 +165,35 @@ const ChannelCarousel = () => {
     skipSnaps: false,
   })
 
+  const [columnCount, setColumnCount] = useState<ColumnCount>('4')
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      if (window.innerWidth >= 453) {
+        setColumnCount('5')
+      }
+      if (window.innerWidth >= 561) {
+        setColumnCount('6')
+      }
+      if (window.innerWidth >= 669) {
+        setColumnCount('7')
+      }
+    }
+  }, [])
+
+  const pageCount = Math.ceil(items.length / (Number(columnCount) * 2))
+  let slicedItems = []
+  for (let i = 0; i < pageCount; i++) {
+    slicedItems.push(
+      items.slice(
+        i * (Number(columnCount) * 2),
+        (i + 1) * (Number(columnCount) * 2)
+      )
+    )
+  }
+
   // state of carusel
   const scrolling = useRef(false)
-  // const settled = useRef(true)
-
-  // const autoplay = useCallback(() => {
-  //   if (!carouselApi) {
-  //     return
-  //   }
-  //   if (carouselApi.canScrollNext()) {
-  //     setDot(carouselApi.selectedScrollSnap())
-  //     carouselApi.scrollNext()
-  //   } else {
-  //     setDot(0)
-  //     carouselApi.scrollTo(0)
-  //   }
-  // }, [carouselApi])
-
-  // const { play, stop } = useCarousel(autoplay, 5000)
 
   const scroll = (index: number) => {
     if (!carouselApi) {
@@ -110,20 +228,7 @@ const ChannelCarousel = () => {
     setDot(0)
     setSnaps(carouselApi.scrollSnapList())
 
-    // carouselApi.on('pointerDown', stop)
     carouselApi.on('select', onSelect)
-    // carouselApi.on('scroll', () => {
-    //   if (!scrolling.current && settled.current) {
-    //     scrolling.current = true
-    //     settled.current = false
-    //   }
-    // })
-    // carouselApi.on('settle', () => {
-    //   scrolling.current = false
-    //   settled.current = true
-    // })
-
-    // play()
   }, [carouselApi])
 
   const [hash, setHash] = useState('')
@@ -163,91 +268,25 @@ const ChannelCarousel = () => {
         onClickCapture={onCaptureClick}
       >
         <div className={styles.container}>
-          {items?.map((item, i) => {
-            const title = item.title || ''
-
+          {slicedItems?.map((its, i) => {
             return (
-              <div key={item.id} className={styles.slide}>
+              <div key={i} className={styles.slide}>
                 <div className={styles.content}>
-                  <a
-                    href={item.link + 1}
-                    className={classnames({
-                      [styles.selectedChannel]:
-                        selectedChannel === parseInt(item.id + 1, 10),
-                    })}
-                  >
-                    {title}1
-                  </a>
-
-                  <a
-                    href={item.link + 2}
-                    className={classnames({
-                      [styles.selectedChannel]:
-                        selectedChannel === parseInt(item.id + 2, 10),
-                    })}
-                  >
-                    {title}2
-                  </a>
-
-                  <a
-                    href={item.link + 3}
-                    className={classnames({
-                      [styles.selectedChannel]:
-                        selectedChannel === parseInt(item.id + 3, 10),
-                    })}
-                  >
-                    {title}3
-                  </a>
-
-                  <a
-                    href={item.link + 4}
-                    className={classnames({
-                      [styles.selectedChannel]:
-                        selectedChannel === parseInt(item.id + 4, 10),
-                    })}
-                  >
-                    {title}4
-                  </a>
-
-                  <a
-                    href={item.link + 5}
-                    className={classnames({
-                      [styles.selectedChannel]:
-                        selectedChannel === parseInt(item.id + 5, 10),
-                    })}
-                  >
-                    {title}5
-                  </a>
-
-                  <a
-                    href={item.link + 6}
-                    className={classnames({
-                      [styles.selectedChannel]:
-                        selectedChannel === parseInt(item.id + 6, 10),
-                    })}
-                  >
-                    {title}6
-                  </a>
-
-                  <a
-                    href={item.link + 7}
-                    className={classnames({
-                      [styles.selectedChannel]:
-                        selectedChannel === parseInt(item.id + 7, 10),
-                    })}
-                  >
-                    {title}7
-                  </a>
-
-                  <a
-                    href={item.link + 8}
-                    className={classnames({
-                      [styles.selectedChannel]:
-                        selectedChannel === parseInt(item.id + 8, 10),
-                    })}
-                  >
-                    {title}8
-                  </a>
+                  {its?.map((item, i) => {
+                    const title = item.title || ''
+                    return (
+                      <a
+                        key={i}
+                        href={item.link}
+                        className={classnames({
+                          [styles.selectedChannel]:
+                            selectedChannel === parseInt(item.id, 10),
+                        })}
+                      >
+                        {title}
+                      </a>
+                    )
+                  })}
                 </div>
               </div>
             )
@@ -256,7 +295,7 @@ const ChannelCarousel = () => {
       </section>
       <footer className={styles.footer}>
         <section className={styles.dots}>
-          {items?.map((_, index) => (
+          {slicedItems?.map((_, index) => (
             <Dot
               key={index}
               index={index}
