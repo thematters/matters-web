23a8e5c1f734837e609c609e8a8784ad0eb68cc5
Kechicode <186776112+Kechicode@users.noreply.github.com>
2025-02-13 16:07:44 +0800

    feat(Search): Refactor AggregateResults tabs styling and layout

--- src/views/Search/AggregateResults/Tabs.tsx
+++ src/views/Search/AggregateResults/Tabs.tsx
@@ -2,6 +2,8 @@ import { useIntl } from 'react-intl'
 
 import { SquareTabs } from '~/components'
 
+import styles from './styles.module.css'
+
 export type TABS = 'article' | 'user' | 'tag'
 
 type TabsProps = {
@@ -13,31 +15,33 @@ export const Tabs = ({ tab, setTab }: TabsProps) => {
   const intl = useIntl()
 
   return (
-    <SquareTabs spacing="md">
-      <SquareTabs.Tab
-        selected={tab === 'article'}
-        onClick={() => setTab('article')}
-        title={intl.formatMessage({
-          defaultMessage: 'Articles',
-          id: '3KNMbJ',
-        })}
-      />
-      <SquareTabs.Tab
-        selected={tab === 'user'}
-        onClick={() => setTab('user')}
-        title={intl.formatMessage({
-          defaultMessage: 'Users',
-          id: 'YDMrKK',
-        })}
-      />
-      <SquareTabs.Tab
-        selected={tab === 'tag'}
-        onClick={() => setTab('tag')}
-        title={intl.formatMessage({
-          defaultMessage: 'Tags',
-          id: '1EYCdR',
-        })}
-      />
-    </SquareTabs>
+    <section className={styles.tabs}>
+      <SquareTabs>
+        <SquareTabs.Tab
+          selected={tab === 'article'}
+          onClick={() => setTab('article')}
+          title={intl.formatMessage({
+            defaultMessage: 'Articles',
+            id: '3KNMbJ',
+          })}
+        />
+        <SquareTabs.Tab
+          selected={tab === 'user'}
+          onClick={() => setTab('user')}
+          title={intl.formatMessage({
+            defaultMessage: 'Users',
+            id: 'YDMrKK',
+          })}
+        />
+        <SquareTabs.Tab
+          selected={tab === 'tag'}
+          onClick={() => setTab('tag')}
+          title={intl.formatMessage({
+            defaultMessage: 'Tags',
+            id: '1EYCdR',
+          })}
+        />
+      </SquareTabs>
+    </section>
   )
 }
