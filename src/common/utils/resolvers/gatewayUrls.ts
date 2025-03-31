def363e30a7c44ac613a2cb85b1f1da0e2deca19
gitwoz <177856586+gitwoz@users.noreply.github.com>
2025-03-31 18:10:14 +0700

    feat(ipfs): add w3s.link gateway

--- src/common/utils/resolvers/gatewayUrls.ts
+++ src/common/utils/resolvers/gatewayUrls.ts
@@ -1,49 +1,10 @@
 // const TEST_HASH = 'Qmaisz6NMhDB51cCvNWa1GMS7LU1pAxdF4Ld6Ft9kZEP2a'
 const PUBLIC_GATEWAYS: string[] = [
-  'https://cloudflare-ipfs.com/ipfs/:hash',
-  'https://ipfs.io/ipfs/:hash',
+  'https://:cidv1.ipfs.w3s.link',
   'https://ipfs-gateway.matters.town/ipfs/:hash',
+  'https://ipfs.io/ipfs/:hash',
+  'https://cloudflare-ipfs.com/ipfs/:hash',
 ]
-
-// check accessbility for a given hash and gateway
-// const checkGateway = async (
-//   hash: string,
-//   gatewayUrl: string
-// ): Promise<boolean> => {
-//   const testUrl = `${gatewayUrl.replace(
-//     ':hash',
-//     hash
-//   )}#x-ipfs-companion-no-redirect`
-
-//   try {
-//     const res = await fetch(testUrl)
-//     if (res && res.ok) {
-//       return true
-//     }
-//     return false
-//   } catch (err) {
-//     console.log(`Gateway not alive, skipping: ${gatewayUrl}`)
-//     return false
-//   }
-// }
-
-// const gatewayUrlsResolver = async () => {
-//   const gatwayUrls: string[] = []
-
-//   // push in faster public gateway first
-//   await Promise.all(
-//     PUBLIC_GATEWAYS.map((url) =>
-//       checkGateway(TEST_HASH, url).then(
-//         (alive: boolean) => alive && gatwayUrls.push(url)
-//       )
-//     )
-//   )
-
-//   // meson network use 302 redirect, so we have to push it in mannually
-//   gatwayUrls.concat(['https://pz-matters.meson.network/ipfs/:hash'])
-
-//   return gatwayUrls
-// }
 
 const gatewayUrlsResolver = () => PUBLIC_GATEWAYS
 
