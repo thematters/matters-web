c8a1d2247c66e14e0a328b21b9b4c6ddac2a2fc5
Kechicode <186776112+Kechicode@users.noreply.github.com>
2025-04-07 16:40:46 +0800

    src/components/Buttons/UniversalAuth/index.tsx

--- src/components/Buttons/UniversalAuth/index.tsx
+++ src/components/Buttons/UniversalAuth/index.tsx
@@ -1,69 +1,76 @@
 import React from 'react'
-import { FormattedMessage } from 'react-intl'
+import { useIntl } from 'react-intl'
 
 import {
   OPEN_UNIVERSAL_AUTH_DIALOG,
   UNIVERSAL_AUTH_TRIGGER,
 } from '~/common/enums'
 import { analytics } from '~/common/utils'
-import { Button, Media, TextIcon } from '~/components'
+import {
+  Button,
+  ButtonHeight,
+  ButtonSpacingX,
+  ButtonSpacingY,
+  ButtonWidth,
+  Media,
+  TextIcon,
+} from '~/components'
 
 type UniversalAuthButtonProps = { resideIn?: 'nav' | 'sideNav' }
 
 export const UniversalAuthButton: React.FC<UniversalAuthButtonProps> = ({
   resideIn,
 }) => {
-  const props = {
-    onClick: () => {
-      analytics.trackEvent('click_button', {
-        type: 'login/signup',
-      })
+  const intl = useIntl()
+  const enterText = intl.formatMessage({
+    defaultMessage: 'Enter',
+    id: 'qVhxp5',
+    description: 'src/components/Buttons/UniversalAuth/index.tsx',
+  })
 
-      // deprecated
-      // window.dispatchEvent(new CustomEvent(CLOSE_ACTIVE_DIALOG))
-      window.dispatchEvent(
-        new CustomEvent(OPEN_UNIVERSAL_AUTH_DIALOG, {
-          ...(resideIn
-            ? { detail: { trigger: UNIVERSAL_AUTH_TRIGGER[resideIn] } }
-            : {}),
-        })
-      )
-    },
+  const handleClick = () => {
+    analytics.trackEvent('click_button', {
+      type: 'login/signup',
+    })
+
+    // deprecated
+    // window.dispatchEvent(new CustomEvent(CLOSE_ACTIVE_DIALOG))
+    window.dispatchEvent(
+      new CustomEvent(OPEN_UNIVERSAL_AUTH_DIALOG, {
+        ...(resideIn
+          ? { detail: { trigger: UNIVERSAL_AUTH_TRIGGER[resideIn] } }
+          : {}),
+      })
+    )
   }
+
+  const renderButton = (
+    size: [ButtonWidth, ButtonHeight] | undefined,
+    spacing: [ButtonSpacingY, ButtonSpacingX],
+    textSize: 12 | 14
+  ) => (
+    <Button bgColor="black" size={size} spacing={spacing} onClick={handleClick}>
+      <TextIcon color="white" weight="medium" size={textSize}>
+        {enterText}
+      </TextIcon>
+    </Button>
+  )
 
   return (
     <>
       <Media lessThan="md">
-        <Button
-          bgColor="black"
-          size={[null, '1.625rem']}
-          spacing={[0, 12]}
-          {...props}
-        >
-          <TextIcon color="white" weight="medium" size={12}>
-            <FormattedMessage
-              defaultMessage="Enter"
-              description="src/components/Buttons/UniversalAuth/index.tsx"
-              id="qVhxp5"
-            />
-          </TextIcon>
-        </Button>
+        {renderButton(
+          [null, '1.625rem' as ButtonHeight],
+          [0 as ButtonSpacingY, 12 as ButtonSpacingX],
+          12
+        )}
       </Media>
       <Media greaterThan="sm">
-        <Button
-          bgColor="black"
-          // size={[null, '1.625rem']}
-          spacing={[8, 20]}
-          {...props}
-        >
-          <TextIcon color="white" weight="medium" size={14}>
-            <FormattedMessage
-              defaultMessage="Enter"
-              description="src/components/Buttons/UniversalAuth/index.tsx"
-              id="qVhxp5"
-            />
-          </TextIcon>
-        </Button>
+        {renderButton(
+          undefined,
+          [8 as ButtonSpacingY, 20 as ButtonSpacingX],
+          14
+        )}
       </Media>
     </>
   )
