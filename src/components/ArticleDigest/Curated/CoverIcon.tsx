a41aa60e62f50b9ba3dd9c2236fb4626ac8a7cc8
Kechicode <186776112+Kechicode@users.noreply.github.com>
2025-04-16 21:22:53 +0800

    feat(CoverIcon): add size prop for responsive design and update usage in ArticleDigestCurated

--- src/components/ArticleDigest/Curated/CoverIcon.tsx
+++ src/components/ArticleDigest/Curated/CoverIcon.tsx
@@ -1,6 +1,12 @@
 import styles from './styles.module.css'
 
-const CoverIcon = ({ title }: { title: string }) => {
+const CoverIcon = ({
+  title,
+  size = 'sm',
+}: {
+  title: string
+  size: 'sm' | 'lg'
+}) => {
   const titleLength = title.length
   const hValue = Math.round((titleLength / 100) * 360)
   const layer1Color = `hsl(${hValue}, 50%, 90%)`
@@ -9,6 +15,46 @@ const CoverIcon = ({ title }: { title: string }) => {
   const layer4Color = `hsl(${hValue}, 50%, 80%)`
   const layer5Color = `hsl(${hValue}, 50%, 85%)`
   const layer6Color = `hsl(${hValue}, 50%, 80%)`
+
+  if (size === 'sm') {
+    return (
+      <div className={styles.coverIcon}>
+        <svg
+          width="334"
+          height="167"
+          viewBox="0 0 334 167"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g clip-path="url(#clip0_4780_19260)">
+            <path d="M334 0H0V167H334V0Z" fill={layer1Color} />
+            <path
+              d="M0 14.5557L173.68 52.104L334 42.084V167H0V14.5557Z"
+              fill={layer2Color}
+            />
+            <path d="M334 70V167H0V107.5L334 70Z" fill={layer3Color} />
+            <path
+              d="M100.2 125.472C119.384 125.472 134.936 109.92 134.936 90.736C134.936 71.5518 119.384 56 100.2 56C81.0157 56 65.4639 71.5518 65.4639 90.736C65.4639 109.92 81.0157 125.472 100.2 125.472Z"
+              fill={layer4Color}
+            />
+            <path
+              d="M0 133.049L216.432 70L334 128.5V167H0L0 133.049Z"
+              fill={layer5Color}
+            />
+            <path
+              d="M216.489 70L114 167H334V103.472L216.489 70Z"
+              fill={layer6Color}
+            />
+          </g>
+          <defs>
+            <clipPath id="clip0_4780_19260">
+              <rect width="334" height="167" rx="8" fill="white" />
+            </clipPath>
+          </defs>
+        </svg>
+      </div>
+    )
+  }
 
   return (
     <div className={styles.coverIcon}>
--- src/components/ArticleDigest/Curated/index.tsx
+++ src/components/ArticleDigest/Curated/index.tsx
@@ -103,11 +103,11 @@ export const ArticleDigestCurated = ({
         >
           <Media lessThan="sm">
             {cover && <ResponsiveImage url={cover} width={334} height={167} />}
-            {!cover && <CoverIcon title={article.title} />}
+            {!cover && <CoverIcon title={article.title} size="sm" />}
           </Media>
           <Media greaterThanOrEqual="sm">
             {cover && <ResponsiveImage url={cover} width={404} height={404} />}
-            {!cover && <CoverIcon title={article.title} />}
+            {!cover && <CoverIcon title={article.title} size="lg" />}
           </Media>
 
           {pinned && (
