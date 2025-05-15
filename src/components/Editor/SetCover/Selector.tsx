ce514057aa027c23d658e7023c96751443d51c86
Kechicode <186776112+Kechicode@users.noreply.github.com>
2025-05-15 17:26:27 +0800

    feat(Selector): update layout and styles for improved cover image selection

--- src/components/Editor/SetCover/Selector.tsx
+++ src/components/Editor/SetCover/Selector.tsx
@@ -3,10 +3,8 @@ import { useIntl } from 'react-intl'
 import { ReactComponent as IconCircleCheckFill2 } from '@/public/static/icons/24px/circle-check-fill-2.svg'
 import { ASSET_TYPE } from '~/common/enums'
 import { toSizedImageURL } from '~/common/utils'
-import { Icon, Translate } from '~/components'
+import { Icon } from '~/components'
 import { AssetFragment } from '~/gql/graphql'
-
-import styles from './styles.module.css'
 
 interface SelectorProps {
   assets: AssetFragment[]
@@ -27,48 +25,38 @@ const Selector: React.FC<SelectorProps> = ({
   )
 
   return (
-    <section className={styles.selector}>
-      <h3>
-        <Translate
-          zh_hant="你也可以選擇一張已有的圖片作為封面"
-          zh_hans="你也可以选择一张已有的图片作为封面"
-          en="you can choose an existing picture as cover"
-        />
-      </h3>
-
-      <ul>
-        {imageAssets.map((asset, index) => (
-          <li
-            key={asset.id}
-            className={asset.path === selected?.path ? 'selected' : undefined}
+    <>
+      {imageAssets.map((asset, index) => (
+        <div
+          key={asset.id}
+          className={asset.path === selected?.path ? 'selected' : undefined}
+        >
+          <button
+            type="button"
+            onClick={() =>
+              setSelected(asset.id === selected?.id ? undefined : asset)
+            }
+            aria-label={intl.formatMessage({
+              defaultMessage: `Set as cover`,
+              id: 'BNupBu',
+            })}
           >
-            <button
-              type="button"
-              onClick={() =>
-                setSelected(asset.id === selected?.id ? undefined : asset)
-              }
-              aria-label={intl.formatMessage({
-                defaultMessage: `Set as cover`,
-                id: 'BNupBu',
+            <img
+              src={toSizedImageURL({
+                url: asset.path,
+                width: 101,
+                height: 101,
               })}
-            >
-              <img
-                src={toSizedImageURL({
-                  url: asset.path,
-                  width: 72,
-                  height: 72,
-                })}
-                alt="cover"
-              />
+              alt="cover"
+            />
 
-              {asset.path === selected?.path && (
-                <Icon icon={IconCircleCheckFill2} size={24} />
-              )}
-            </button>
-          </li>
-        ))}
-      </ul>
-    </section>
+            {asset.path === selected?.path && (
+              <Icon icon={IconCircleCheckFill2} size={20} />
+            )}
+          </button>
+        </div>
+      ))}
+    </>
   )
 }
 
