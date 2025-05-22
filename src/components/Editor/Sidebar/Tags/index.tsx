e14a1c2060786b3c054a10ff4093ccbd9ae46e7c
Kechicode <186776112+Kechicode@users.noreply.github.com>
2025-05-22 14:18:58 +0800

    feat(Tags): enhance tag management with editing functionality and updated UI

--- src/components/Editor/Sidebar/Tags/index.tsx
+++ src/components/Editor/Sidebar/Tags/index.tsx
@@ -1,6 +1,8 @@
+import { useState } from 'react'
 import { FormattedMessage } from 'react-intl'
 
-import { ReactComponent as IconHashtag } from '@/public/static/icons/24px/hashtag.svg'
+import { ReactComponent as IconPlus } from '@/public/static/icons/24px/plus.svg'
+import { ReactComponent as IconTimes } from '@/public/static/icons/24px/times.svg'
 import { analytics } from '~/common/utils'
 import {
   EditorSearchSelectDialog,
@@ -28,15 +30,12 @@ const SidebarTags = ({
   saving,
   disabled,
 }: SidebarTagsProps) => {
+  const [isEditing, setIsEditing] = useState(false)
+
   return (
     <EditorSearchSelectDialog
-      title={<FormattedMessage defaultMessage="Add Tag" id="GUW//c" />}
-      hint={
-        <FormattedMessage
-          defaultMessage="Adding tags helps readers find your articles. Add or create new tags."
-          id="NmhF45"
-        />
-      }
+      title={<FormattedMessage defaultMessage="Add Tags" id="WNxQX0" />}
+      hint={<FormattedMessage defaultMessage="Add up to 3 tags" id="/5yq4w" />}
       searchType="Tag"
       onSave={(nodes: SearchSelectNode[]) =>
         editTags(nodes as DigestTagFragment[])
@@ -50,13 +49,28 @@ const SidebarTags = ({
     >
       {({ openDialog }) => (
         <Box
-          icon={<Icon icon={IconHashtag} size={24} />}
-          title={<FormattedMessage defaultMessage="Add Tag" id="GUW//c" />}
+          title={<FormattedMessage defaultMessage="Add Tags" id="WNxQX0" />}
           subtitle={
-            <FormattedMessage
-              defaultMessage="Adding tags helps readers find your articles."
-              id="fPcF7H"
-            />
+            <FormattedMessage defaultMessage="Add up to 3 tags" id="/5yq4w" />
+          }
+          rightButton={
+            <>
+              {isEditing ? (
+                <button
+                  onClick={() => setIsEditing(false)}
+                  className={styles.rightButton}
+                >
+                  <Icon icon={IconTimes} size={24} color="black" />
+                </button>
+              ) : (
+                <button
+                  onClick={() => setIsEditing(true)}
+                  className={styles.rightButton}
+                >
+                  <Icon icon={IconPlus} size={24} color="black" />
+                </button>
+              )}
+            </>
           }
           onClick={openDialog}
           disabled={disabled}
