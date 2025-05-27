ff9049e8802658a08362da4bf85dbcde067741ac
Kechicode <186776112+Kechicode@users.noreply.github.com>
2025-05-27 17:24:41 +0800

    refactor(Tags): simplify SidebarTags component structure and enhance TagInput with improved search handling

--- src/components/Editor/Sidebar/Tags/TagInput/index.tsx
+++ src/components/Editor/Sidebar/Tags/TagInput/index.tsx
@@ -36,155 +36,194 @@ type TagInputProps = {
   saving?: boolean
 }
 
+type DropdownMenuProps = {
+  searchKey: string
+  debouncedSearchKey: string
+  searchResults: QuickResultQuery['tag'] | undefined
+  searchLoading: boolean
+  recentTags: DigestTagFragment[]
+  onSelectTag: (tagContent: string) => void
+}
+
+const renderRecentTags = (
+  recentTags: DigestTagFragment[],
+  onSelectTag: (tagContent: string) => void
+) => {
+  return recentTags.slice(0, ARTICLE_TAGS_MAX_COUNT).map((tag) => (
+    <Menu.Item
+      key={tag.id}
+      spacing={[8, 16]}
+      bgActiveColor="greyHover"
+      onClick={() => onSelectTag(tag.content)}
+    >
+      <ListTag tag={tag} hasCount={false} is="span" iconSize={20} />
+    </Menu.Item>
+  ))
+}
+
+const renderCreateNewTag = (
+  debouncedSearchKey: string,
+  onSelectTag: (tagContent: string) => void
+) => {
+  return (
+    <Menu.Item
+      key={debouncedSearchKey}
+      spacing={[8, 16]}
+      bgActiveColor="greyHover"
+      onClick={() => onSelectTag(debouncedSearchKey)}
+    >
+      <ListTag
+        tag={toDigestTagPlaceholder(normalizeTag(debouncedSearchKey))}
+        hasCount={false}
+        is="span"
+        icon={<Icon icon={IconCirclePlus} color="greyDarker" size={20} />}
+      />
+    </Menu.Item>
+  )
+}
+
+const renderSearchResults = (
+  searchResults: QuickResultQuery['tag'] | undefined,
+  onSelectTag: (tagContent: string) => void
+) => {
+  const { edges: tagEdges } = searchResults || {}
+
+  return tagEdges?.map(({ node }) =>
+    node.__typename === 'Tag' ? (
+      <Menu.Item
+        key={node.id}
+        spacing={[8, 16]}
+        bgActiveColor="greyHover"
+        onClick={() => onSelectTag(node.content)}
+      >
+        <ListTag tag={node} hasCount={false} is="span" iconSize={20} />
+      </Menu.Item>
+    ) : null
+  )
+}
+
+const DropdownMenu = ({
+  searchKey,
+  debouncedSearchKey,
+  searchResults,
+  searchLoading,
+  recentTags,
+  onSelectTag,
+}: DropdownMenuProps) => {
+  const { edges: tagEdges } = searchResults || {}
+  const hasSearchResults = tagEdges && tagEdges.length > 0
+  const isSearching = searchKey !== ''
+  const shouldShowCreateOption =
+    isSearching && !hasSearchResults && !searchLoading
+
+  return (
+    <Menu>
+      {!isSearching && renderRecentTags(recentTags, onSelectTag)}
+      {shouldShowCreateOption &&
+        renderCreateNewTag(debouncedSearchKey, onSelectTag)}
+      {isSearching &&
+        hasSearchResults &&
+        renderSearchResults(searchResults, onSelectTag)}
+    </Menu>
+  )
+}
+
 const TagInput = ({ tags, onAddTag, saving }: TagInputProps) => {
-  const formId = 'search-input-tag-form'
   const intl = useIntl()
+  const client = useApolloClient()
+
   const [searchKey, setSearchKey] = useState('')
   const [debouncedSearchKey] = useDebounce(searchKey, INPUT_DEBOUNCE)
-  const textAriaLabel = intl.formatMessage({
-    defaultMessage: 'Input tags',
-    id: 'xk1AW6',
-  })
-
-  const client = useApolloClient()
-  const [data, setData] = useState<QuickResultQuery>()
+  const [searchData, setSearchData] = useState<QuickResultQuery>()
   const [searchLoading, setSearchLoading] = useState(false)
-  const clearData = () => setData(undefined)
 
-  const { edges: tagEdges } = data?.tag || {}
-  const hasTags = tagEdges && tagEdges.length > 0
-
-  useEffect(() => {
-    ;(async () => {
-      clearData()
-      setSearchLoading(true)
-      // Why not useLazyQuery 👇🔗
-      // https://github.com/apollographql/apollo-client/issues/5912
-      const response = await client.query({
-        query: QUICK_RESULT,
-        variables: {
-          key: searchKey,
-        },
-        fetchPolicy: 'no-cache',
-      })
-      setData(response.data)
-      setSearchLoading(response.loading)
-    })()
-  }, [searchKey])
-
-  const { values, handleChange, handleSubmit, setFieldValue, handleBlur } =
-    useFormik<{
-      tag: string
-    }>({
-      initialValues: { tag: '' },
-      onSubmit: (values) => {
-        const msg = validateTagName(values.tag, intl)
-        if (msg) {
-          toast.error({
-            message: msg,
-          })
-          return
-        }
-        onAddTag(values.tag)
-      },
-    })
+  const formik = useFormik<{ tag: string }>({
+    initialValues: { tag: '' },
+    onSubmit: (values) => {
+      const validationMessage = validateTagName(values.tag, intl)
+      if (validationMessage) {
+        toast.error({ message: validationMessage })
+        return
+      }
+      onAddTag(values.tag)
+    },
+  })
 
   const { data: recentTagsData, loading: recentTagsLoading } =
     useQuery<EditorRecentTagsQuery>(EDITOR_RECENT_TAGS)
 
-  // recent tags
-  const userTagsEdges = recentTagsData?.viewer?.tags.edges || []
+  useEffect(() => {
+    const searchTags = async () => {
+      setSearchData(undefined)
+      setSearchLoading(true)
 
-  let recentTags = [...userTagsEdges]?.map((edge) => edge.node)
-  // remove duplicated tags
-  recentTags = _uniqBy(recentTags, (tag) => tag.content)
-  // remove selected tags
-  recentTags = recentTags.filter(
-    (tag) => !tags.find((t) => t.content === tag.content)
+      try {
+        const response = await client.query({
+          query: QUICK_RESULT,
+          variables: { key: searchKey },
+          fetchPolicy: 'no-cache',
+        })
+        setSearchData(response.data)
+      } finally {
+        setSearchLoading(false)
+      }
+    }
+
+    if (searchKey) {
+      searchTags()
+    }
+  }, [searchKey, client])
+
+  const processedRecentTags = (() => {
+    const userTagsEdges = recentTagsData?.viewer?.tags.edges || []
+    let recentTags = userTagsEdges.map((edge) => edge.node)
+
+    recentTags = _uniqBy(recentTags, (tag) => tag.content)
+    return recentTags.filter(
+      (tag) => !tags.find((t) => t.content === tag.content)
+    )
+  })()
+
+  const handleSelectTag = (tagContent: string) => {
+    formik.setFieldValue('tag', tagContent)
+    formik.handleSubmit()
+  }
+
+  const isDropdownVisible =
+    (debouncedSearchKey === '' &&
+      !recentTagsLoading &&
+      processedRecentTags.length > 0 &&
+      !saving) ||
+    (debouncedSearchKey !== '' && !searchLoading && !saving)
+
+  const inputAriaLabel = intl.formatMessage({
+    defaultMessage: 'Input tags',
+    id: 'xk1AW6',
+  })
+
+  const dropdownContent = (
+    <DropdownMenu
+      searchKey={searchKey}
+      debouncedSearchKey={debouncedSearchKey}
+      searchResults={searchData?.tag}
+      searchLoading={searchLoading}
+      recentTags={processedRecentTags}
+      onSelectTag={handleSelectTag}
+    />
   )
 
   return (
     <Dropdown
       focusLock={false}
-      content={
-        <div>
-          <Menu>
-            {searchKey === '' &&
-              recentTags.slice(0, ARTICLE_TAGS_MAX_COUNT).map((tag) => (
-                <Menu.Item
-                  key={tag.id}
-                  spacing={[8, 16]}
-                  bgActiveColor="greyHover"
-                  onClick={() => {
-                    setFieldValue('tag', tag.content)
-                    handleSubmit()
-                  }}
-                >
-                  <ListTag tag={tag} hasCount={false} is="span" iconSize={20} />
-                </Menu.Item>
-              ))}
-            {searchKey !== '' && !hasTags && !searchLoading && (
-              <Menu.Item
-                key={debouncedSearchKey}
-                spacing={[8, 16]}
-                bgActiveColor="greyHover"
-                onClick={() => {
-                  setFieldValue('tag', debouncedSearchKey)
-                  handleSubmit()
-                }}
-              >
-                <ListTag
-                  tag={toDigestTagPlaceholder(normalizeTag(debouncedSearchKey))}
-                  hasCount={false}
-                  is="span"
-                  icon={
-                    <Icon icon={IconCirclePlus} color="greyDarker" size={20} />
-                  }
-                />
-              </Menu.Item>
-            )}
-            {searchKey !== '' &&
-              hasTags &&
-              tagEdges.map(
-                ({ node, cursor }, i) =>
-                  node.__typename === 'Tag' && (
-                    <Menu.Item
-                      key={node.id}
-                      spacing={[8, 16]}
-                      bgActiveColor="greyHover"
-                      onClick={() => {
-                        setFieldValue('tag', node.content)
-                        handleSubmit()
-                      }}
-                    >
-                      <ListTag
-                        tag={node}
-                        hasCount={false}
-                        is="span"
-                        iconSize={20}
-                      />
-                    </Menu.Item>
-                  )
-              )}
-          </Menu>
-        </div>
-      }
+      content={dropdownContent}
       placement="bottom-start"
-      visible={
-        (debouncedSearchKey === '' &&
-          !recentTagsLoading &&
-          recentTags.length > 0 &&
-          !saving) ||
-        (debouncedSearchKey !== '' && !searchLoading && !saving)
-      }
+      visible={isDropdownVisible}
     >
       {({ ref }) => (
         <form
-          id={formId}
           className={styles.form}
-          onSubmit={handleSubmit}
+          onSubmit={formik.handleSubmit}
           ref={ref}
-          action=""
           autoComplete="off"
         >
           <Icon icon={IconHashTag} color="greyDark" />
@@ -194,23 +233,20 @@ const TagInput = ({ tags, onAddTag, saving }: TagInputProps) => {
             id="search-input-tag"
             autoCorrect="off"
             autoFocus
-            value={values.tag}
+            value={formik.values.tag}
             disabled={saving}
-            aria-label={textAriaLabel}
-            placeholder={intl.formatMessage({
-              defaultMessage: 'Input tags',
-              id: 'xk1AW6',
-            })}
+            aria-label={inputAriaLabel}
+            placeholder={inputAriaLabel}
             className={styles.input}
-            onBlur={handleBlur}
+            onBlur={formik.handleBlur}
             onChange={(e) => {
               setSearchKey(e.target.value)
-              handleChange(e)
+              formik.handleChange(e)
             }}
             onKeyDown={(e) => {
               if (e.key === 'Enter') {
                 e.preventDefault()
-                handleSubmit()
+                formik.handleSubmit()
               }
             }}
           />
--- src/components/Editor/Sidebar/Tags/index.tsx
+++ src/components/Editor/Sidebar/Tags/index.tsx
@@ -5,17 +5,9 @@ import { ReactComponent as IconPlus } from '@/public/static/icons/24px/plus.svg'
 import { ReactComponent as IconTimes } from '@/public/static/icons/24px/times.svg'
 import { ARTICLE_TAGS_MAX_COUNT } from '~/common/enums/article'
 import { analytics, normalizeTag } from '~/common/utils'
-import {
-  EditorSearchSelectDialog,
-  Icon,
-  InlineTag,
-  toDigestTagPlaceholder,
-  // SearchSelectNode,
-} from '~/components'
-import { SearchSelectNode } from '~/components/Forms/SearchSelectForm'
+import { Icon, InlineTag, toDigestTagPlaceholder } from '~/components'
 import { DigestTagFragment } from '~/gql/graphql'
 
-import TagCustomStagingArea from '../../TagCustomStagingArea'
 import Box from '../Box'
 import styles from './styles.module.css'
 import TagInput from './TagInput'
@@ -41,77 +33,59 @@ const SidebarTags = ({
   }
 
   return (
-    <EditorSearchSelectDialog
+    <Box
       title={<FormattedMessage defaultMessage="Add Tags" id="WNxQX0" />}
-      hint={<FormattedMessage defaultMessage="Add up to 3 tags" id="/5yq4w" />}
-      searchType="Tag"
-      onSave={(nodes: SearchSelectNode[]) =>
-        editTags(nodes as DigestTagFragment[])
+      subtitle={
+        <FormattedMessage defaultMessage="Add up to 3 tags" id="/5yq4w" />
       }
-      nodes={tags}
-      saving={saving}
-      createTag
-      CustomStagingArea={TagCustomStagingArea}
-      dismissOnClickOutside={false}
-      dismissOnESC={false}
+      rightButton={
+        <>
+          {isEditing ? (
+            <button
+              onClick={() => setIsEditing(false)}
+              className={styles.rightButton}
+            >
+              <Icon icon={IconTimes} size={24} color="black" />
+            </button>
+          ) : (
+            <button
+              onClick={() => setIsEditing(true)}
+              className={styles.rightButton}
+              disabled={tags.length >= ARTICLE_TAGS_MAX_COUNT}
+            >
+              <Icon icon={IconPlus} size={24} color="black" />
+            </button>
+          )}
+        </>
+      }
+      disabled={disabled}
     >
-      {({ openDialog }) => (
-        <Box
-          title={<FormattedMessage defaultMessage="Add Tags" id="WNxQX0" />}
-          subtitle={
-            <FormattedMessage defaultMessage="Add up to 3 tags" id="/5yq4w" />
-          }
-          rightButton={
-            <>
-              {isEditing ? (
-                <button
-                  onClick={() => setIsEditing(false)}
-                  className={styles.rightButton}
-                >
-                  <Icon icon={IconTimes} size={24} color="black" />
-                </button>
-              ) : (
-                <button
-                  onClick={() => setIsEditing(true)}
-                  className={styles.rightButton}
-                  disabled={tags.length >= ARTICLE_TAGS_MAX_COUNT}
-                >
-                  <Icon icon={IconPlus} size={24} color="black" />
-                </button>
-              )}
-            </>
-          }
-          onClick={openDialog}
-          disabled={disabled}
-        >
-          <div className={styles.content}>
-            {tags.length > 0 && (
-              <ul className={styles.list}>
-                {tags.map((tag) => (
-                  <li key={tag.id}>
-                    <InlineTag
-                      tag={tag}
-                      onRemoveTag={() => {
-                        editTags(tags.filter((t) => t.content !== tag.content))
-                        analytics.trackEvent('click_button', {
-                          type: 'remove_tag',
-                          pageType: 'edit_draft',
-                        })
-                      }}
-                    />
-                  </li>
-                ))}
-              </ul>
-            )}
-            {isEditing && (
-              <div className={styles.tagInput}>
-                <TagInput tags={tags} onAddTag={onAddTag} saving={saving} />
-              </div>
-            )}
+      <div className={styles.content}>
+        {tags.length > 0 && (
+          <ul className={styles.list}>
+            {tags.map((tag) => (
+              <li key={tag.id}>
+                <InlineTag
+                  tag={tag}
+                  onRemoveTag={() => {
+                    editTags(tags.filter((t) => t.content !== tag.content))
+                    analytics.trackEvent('click_button', {
+                      type: 'remove_tag',
+                      pageType: 'edit_draft',
+                    })
+                  }}
+                />
+              </li>
+            ))}
+          </ul>
+        )}
+        {isEditing && (
+          <div className={styles.tagInput}>
+            <TagInput tags={tags} onAddTag={onAddTag} saving={saving} />
           </div>
-        </Box>
-      )}
-    </EditorSearchSelectDialog>
+        )}
+      </div>
+    </Box>
   )
 }
 
