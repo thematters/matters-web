9e19b5392191863f60a74aa269ac84efa2b16999
Kechicode <186776112+Kechicode@users.noreply.github.com>
2025-06-19 12:13:02 +0800

    feat(Notice): Add channel feedback acceptance notification component

---
+++ src/components/Notice/ArticleNotice/TopicChannelFeedbackAcceptedNotice.tsx
@@ -0,0 +1,50 @@
+import gql from 'graphql-tag'
+import { FormattedMessage } from 'react-intl'
+
+import { TopicChannelFeedbackAcceptedNoticeFragment } from '~/gql/graphql'
+
+import NoticeArticleCard from '../NoticeArticleCard'
+import NoticeArticleTitle from '../NoticeArticleTitle'
+import NoticeDate from '../NoticeDate'
+import styles from '../styles.module.css'
+
+const TopicChannelFeedbackAcceptedNotice = ({
+  notice,
+}: {
+  notice: TopicChannelFeedbackAcceptedNoticeFragment
+}) => {
+  return (
+    <section className={styles.container}>
+      <section className={styles.noticeActorsNameAndTitleInfo}>
+        <FormattedMessage
+          defaultMessage="Your channel suggestion for {articleTitle} has been accepted by the editor. Thank you for supporting Matters!"
+          id="EnAfl8"
+          description="src/components/Notice/ArticleNotice/TopicChannelFeedbackAcceptedNotice.tsx"
+          values={{
+            articleTitle: <NoticeArticleTitle article={notice.article} />,
+          }}
+        />
+      </section>
+
+      <section className={styles.footer}>
+        <NoticeDate notice={notice} />
+      </section>
+    </section>
+  )
+}
+
+TopicChannelFeedbackAcceptedNotice.fragments = {
+  notice: gql`
+    fragment TopicChannelFeedbackAcceptedNotice on ArticleNotice {
+      id
+      ...NoticeDate
+      article: target {
+        ...NoticeArticleCard
+      }
+    }
+    ${NoticeArticleCard.fragments.article}
+    ${NoticeDate.fragments.notice}
+  `,
+}
+
+export default TopicChannelFeedbackAcceptedNotice
