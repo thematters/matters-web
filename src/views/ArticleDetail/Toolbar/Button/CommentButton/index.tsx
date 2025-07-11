63b2c264940f45cdc7def3e7785d7dee4dac6909
gitwoz <177856586+gitwoz@users.noreply.github.com>
2025-07-11 13:58:19 +0700

    fix(article): show comment button tooltip for visitor

--- src/views/ArticleDetail/Toolbar/Button/CommentButton/index.tsx
+++ src/views/ArticleDetail/Toolbar/Button/CommentButton/index.tsx
@@ -82,7 +82,7 @@ const Content = ({
 
   return (
     <Tooltip
-      disabled={!!tooltip && !isMdUp}
+      disabled={!tooltip || (!!tooltip && !isMdUp)}
       content={tooltip}
       placement="top"
       delay={[500, null]}
@@ -201,6 +201,17 @@ const CommentButton = ({
 
     return (
       <Content
+        tooltip={
+          <span className={styles.hotKeyTooltip}>
+            <FormattedMessage
+              defaultMessage="Comments"
+              id="7uYW+U"
+              description="src/views/ArticleDetail/Toolbar/FixedToolbar/index.tsx"
+            />
+            &nbsp;
+            <span className={styles.key}>(c)</span>
+          </span>
+        }
         aria-haspopup="dialog"
         article={article}
         iconSize={iconSize}
