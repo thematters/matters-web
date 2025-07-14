fd1b481347668feac4bbb81ac8a2a859447114f1
Kechicode <186776112+Kechicode@users.noreply.github.com>
2025-07-14 17:50:12 +0800

    feat(PreviewDialog): refactor Campaign and Tags components with BasePreviewItem

--- src/components/Editor/PreviewDialog/Campaign/index.tsx
+++ src/components/Editor/PreviewDialog/Campaign/index.tsx
@@ -1,13 +1,9 @@
 import gql from 'graphql-tag'
 import { FormattedMessage } from 'react-intl'
 
-import IconRight from '@/public/static/icons/24px/right.svg'
-import { BREAKPOINTS } from '~/common/enums'
-import { toPath } from '~/common/utils'
-import { Icon, useMediaQuery, useRoute } from '~/components'
 import { EditorPreviewDialogCampaignDraftFragment } from '~/gql/graphql'
 
-import styles from './styles.module.css'
+import { BasePreviewItem } from '../BasePreviewItem'
 
 const fragment = gql`
   fragment EditorPreviewDialogCampaignDraft on Draft {
@@ -27,50 +23,25 @@ export const Campaign = ({
   draft: EditorPreviewDialogCampaignDraftFragment
   closeDialog: () => void
 }) => {
-  const { getQuery, router } = useRoute()
-  const draftId = getQuery('draftId')
-  const path = toPath({
-    page: 'draftDetailOptions',
-    id: draftId,
-  })
-  const goToOptionsPage = () => {
-    router.push(path.href + '?type=campaign')
-  }
   const { campaign } = draft.campaigns[0]
-  const isSmUp = useMediaQuery(`(min-width: ${BREAKPOINTS.MD}px)`)
-  const onClick = () => {
-    closeDialog()
-    if (isSmUp) {
-      window.dispatchEvent(
-        new CustomEvent('open-drawer', {
-          detail: {
-            type: 'campaign',
-            id: campaign.id,
-            name: campaign.name,
-          },
-        })
-      )
-      return
-    }
 
-    goToOptionsPage()
-  }
   return (
-    <button className={styles.container} onClick={onClick}>
-      <section className={styles.left}>
-        <span className={styles.title}>
-          <FormattedMessage
-            defaultMessage="Event"
-            id="+SgLoK"
-            description="src/components/Editor/PreviewDialog/Campaign/index.tsx"
-          />
-        </span>
-        <div className={styles.name}>{campaign.name}</div>
-      </section>
-      <section className={styles.right}>
-        <Icon icon={IconRight} size={14} color="black" />
-      </section>
-    </button>
+    <BasePreviewItem
+      title={
+        <FormattedMessage
+          defaultMessage="Event"
+          id="+SgLoK"
+          description="src/components/Editor/PreviewDialog/Campaign/index.tsx"
+        />
+      }
+      names={[campaign.name]}
+      eventType="campaign"
+      eventDetail={{
+        id: campaign.id,
+        name: campaign.name,
+      }}
+      closeDialog={closeDialog}
+    />
   )
 }
 
--- src/components/Editor/PreviewDialog/Tags/index.tsx
+++ src/components/Editor/PreviewDialog/Tags/index.tsx
@@ -1,13 +1,9 @@
 import gql from 'graphql-tag'
 import { FormattedMessage } from 'react-intl'
 
-import IconRight from '@/public/static/icons/24px/right.svg'
-import { BREAKPOINTS } from '~/common/enums'
-import { toPath } from '~/common/utils'
-import { Icon, useMediaQuery, useRoute } from '~/components'
 import { EditorPreviewDialogTagsDraftFragment } from '~/gql/graphql'
 
-import styles from './styles.module.css'
+import { BasePreviewItem } from '../BasePreviewItem'
 
 const fragment = gql`
   fragment EditorPreviewDialogTagsDraft on Draft {
@@ -22,57 +18,25 @@ export const Tags = ({
   draft: EditorPreviewDialogTagsDraftFragment
   closeDialog: () => void
 }) => {
-  const { getQuery, router } = useRoute()
-  const draftId = getQuery('draftId')
-  const path = toPath({
-    page: 'draftDetailOptions',
-    id: draftId,
-  })
-  const goToOptionsPage = () => {
-    router.push(path.href + '?type=tags')
-  }
   const { tags } = draft
-  const isSmUp = useMediaQuery(`(min-width: ${BREAKPOINTS.MD}px)`)
-  const onClick = () => {
-    closeDialog()
-    if (isSmUp) {
-      window.dispatchEvent(
-        new CustomEvent('open-drawer', {
-          detail: {
-            type: 'tags',
-          },
-        })
-      )
-      return
-    }
 
-    goToOptionsPage()
-  }
   if (!tags) {
     return null
   }
+
   return (
-    <button className={styles.container} onClick={onClick}>
-      <section className={styles.left}>
-        <span className={styles.title}>
-          <FormattedMessage
-            defaultMessage="Tags"
-            id="gccvX4"
-            description="src/components/Editor/PreviewDialog/Tags/index.tsx"
-          />
-        </span>
-        <section className={styles.tags}>
-          {tags.map((tag) => (
-            <div className={styles.name} key={tag}>
-              {tag}
-            </div>
-          ))}
-        </section>
-      </section>
-      <section className={styles.right}>
-        <Icon icon={IconRight} size={14} color="black" />
-      </section>
-    </button>
+    <BasePreviewItem
+      title={
+        <FormattedMessage
+          defaultMessage="Tags"
+          id="gccvX4"
+          description="src/components/Editor/PreviewDialog/Tags/index.tsx"
+        />
+      }
+      names={tags}
+      eventType="tags"
+      closeDialog={closeDialog}
+    />
   )
 }
 
