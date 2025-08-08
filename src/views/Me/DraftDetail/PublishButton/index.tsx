54c95270a47a0c3d9e12dbac84462443032a64f6
Kechicode <186776112+Kechicode@users.noreply.github.com>
2025-08-08 15:24:23 +0800

    fix(DraftDetail): update button and divider styles for disabled state
    PD-2508-2-3

--- src/views/Me/DraftDetail/PublishButton/index.tsx
+++ src/views/Me/DraftDetail/PublishButton/index.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames'
 import { useState } from 'react'
 import { FormattedMessage } from 'react-intl'
 
@@ -54,13 +55,21 @@ const Buttons = ({
     openEditorPreviewDialog()
   }
 
+  const buttonsClasses = classNames(styles.buttons, {
+    [styles.disabled]: disabled,
+  })
+
+  const dividerClasses = classNames(styles.divider, {
+    [styles.disabled]: disabled,
+  })
+
   return (
-    <section className={styles.buttons}>
+    <section className={buttonsClasses}>
       <Button
         size={[null, isSmUp ? '2.375rem' : '2.125rem']}
         spacing={[0, 14]}
         borderRadius={'0.75rem'}
-        bgColor="black"
+        bgColor={disabled ? 'grey' : 'black'}
         onClick={openEditorPreviewDialog}
         disabled={disabled}
         aria-haspopup="dialog"
@@ -76,7 +85,7 @@ const Buttons = ({
         </TextIcon>
       </Button>
 
-      <span className={styles.divider} />
+      <span className={dividerClasses} />
 
       <MoreButton
         draft={draft}
