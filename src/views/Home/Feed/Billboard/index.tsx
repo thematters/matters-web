223b9b5666cfbf412ace4b13affc0d434154615f
Kechicode <186776112+Kechicode@users.noreply.github.com>
2025-08-22 11:28:55 +0800

    fix(Home): fix billboard invisible issues

--- src/views/Home/Feed/Billboard/index.tsx
+++ src/views/Home/Feed/Billboard/index.tsx
@@ -1,16 +1,23 @@
+import classNames from 'classnames'
+
 import { Billboard as BaseBillboard } from '~/components'
 
 import styles from './styles.module.css'
 
-const Billboard = () => {
+const Billboard = ({ hasBorder = true }: { hasBorder?: boolean }) => {
   const tokenId = process.env.NEXT_PUBLIC_BILLBOARD_TOKEN_ID
 
   if (isNaN(Number(tokenId))) {
     return null
   }
 
+  const containerClasses = classNames({
+    [styles.container]: true,
+    [styles.hasBorder]: hasBorder,
+  })
+
   return (
-    <section className={styles.container}>
+    <section className={containerClasses}>
       <BaseBillboard tokenId={tokenId} type="home_hottest" />
     </section>
   )
