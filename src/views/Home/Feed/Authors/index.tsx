f551bc27dfc820b88f2dee77ec67ff6d721aecfb
Kechicode <186776112+Kechicode@users.noreply.github.com>
2025-09-03 16:20:10 +0800

    feat(Home): remove logbook icon from home page's aside author recommendations

--- src/views/Home/Feed/Authors/index.tsx
+++ src/views/Home/Feed/Authors/index.tsx
@@ -83,6 +83,7 @@ const Authors = () => {
                     id: node.id,
                   })
                 }
+                showLogbook={false}
               />
             ))}
           </section>
