0d12f0c42f39c919241e32a1cd90181e28646aca
Kechicode <186776112+Kechicode@users.noreply.github.com>
2025-09-11 16:47:04 +0800

    feat(Home): revise spacing

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,5 +1,14 @@
+import { useContext } from 'react'
+
 import IMAGE_INTRO from '@/public/static/images/intro.jpg'
-import { Announcements, Head, Layout, Spacer } from '~/components'
+import {
+  Announcements,
+  Head,
+  Layout,
+  Spacer,
+  useRoute,
+  ViewerContext,
+} from '~/components'
 
 import Feed from './Feed'
 import Sidebar from './Sidebar'
@@ -9,11 +18,21 @@ const Home = ({
 }: {
   showRecommendation?: boolean
 } = {}) => {
+  const { isInPath } = useRoute()
+  const viewer = useContext(ViewerContext)
+  const isAuthed = viewer.isAuthed
+  const isInNewest = isInPath('NEWEST')
+  const isInChannel = isInPath('CHANNEL')
+  const isInFeatured = isInPath('FEATURED') || (!isAuthed && isInPath('HOME'))
+  const isInHottest = isInPath('HOTTEST')
   return (
     <Layout.Main
       aside={
         <>
-          <Spacer size="sp16" />
+          {(isInNewest || isInFeatured || isInHottest) && (
+            <Spacer size="sp44" />
+          )}
+          {isInChannel && <Spacer size="sp16" />}
           <Announcements />
           {showRecommendation && <Sidebar.Authors />}
           {showRecommendation && <Sidebar.Tags />}
